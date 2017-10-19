a4dbd4a1e8998cbda7b927bd39a706069b07096b
Alex Shyba <as@sitecore.net>
2017-10-19 06:38:37 -0700

    app folder rename

---
+++ samples/node-express-ssr/app/readme.md
@@ -0,0 +1,2 @@
+# Deploy your app here
+For AdvancedApp, need to create a folder with app name (AdvancedApp) and copy the production app bundle here from AdvancedApp's `/dist/prod` folder.
--- samples/node-express-ssr/index.js
+++ samples/node-express-ssr/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const scProxy = require('@sitecore-jss/sitecore-jss-proxy').default;
-const app = require('./dist/AdvancedApp/server.bundle');
+const app = require('./app/AdvancedApp/server.bundle');
 const config = require('./config');
 const server = express();
 const port = process.env.PORT || 3000;
