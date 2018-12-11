1548eb36455e5e2341661b2f82981df77bed9689
David Sherman <orion_treet@hotmail.com>
2018-12-11 17:38:01 +0100

    Change detection fix

--- packages/sitecore-jss-angular/src/components/placeholder.component.ts
+++ packages/sitecore-jss-angular/src/components/placeholder.component.ts
@@ -1,4 +1,5 @@
 import {
+  ChangeDetectorRef,
   Component,
   ComponentFactoryResolver,
   ContentChild,
@@ -17,10 +18,11 @@ import {
 import { ComponentRendering, HtmlElementRendering } from '@sitecore-jss/sitecore-jss';
 import { Observable } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
-import { ComponentFactoryResult, JssComponentFactoryService } from '../jss-component-factory.service';
 import {
-  PLACEHOLDER_MISSING_COMPONENT_COMPONENT
-} from './placeholder.token';
+  ComponentFactoryResult,
+  JssComponentFactoryService,
+} from '../jss-component-factory.service';
+import { PLACEHOLDER_MISSING_COMPONENT_COMPONENT } from './placeholder.token';
 import { RenderEachDirective } from './render-each.directive';
 import { RenderEmptyDirective } from './render-empty.directive';
 import { isRawRendering } from './rendering';
@@ -44,13 +46,20 @@ export class PlaceholderComponent implements OnChanges, DoCheck, OnDestroy {
   private _componentInstances: any[] = [];
   private destroyed = false;
 
-  @Input() name?: string;
-  @Input() rendering: ComponentRendering;
-  @Input() renderings?: Array<ComponentRendering | HtmlElementRendering>;
-  @Input() outputs: { [k: string]: (eventType: any) => void };
-  @ViewChild('view', { read: ViewContainerRef }) private view: ViewContainerRef;
-  @ContentChild(RenderEachDirective) renderEachTemplate: RenderEachDirective;
-  @ContentChild(RenderEmptyDirective) renderEmptyTemplate: RenderEmptyDirective;
+  @Input()
+  name?: string;
+  @Input()
+  rendering: ComponentRendering;
+  @Input()
+  renderings?: Array<ComponentRendering | HtmlElementRendering>;
+  @Input()
+  outputs: { [k: string]: (eventType: any) => void };
+  @ViewChild('view', { read: ViewContainerRef })
+  private view: ViewContainerRef;
+  @ContentChild(RenderEachDirective)
+  renderEachTemplate: RenderEachDirective;
+  @ContentChild(RenderEmptyDirective)
+  renderEmptyTemplate: RenderEmptyDirective;
 
   @Input()
   set inputs(value: { [key: string]: any }) {
@@ -64,8 +73,9 @@ export class PlaceholderComponent implements OnChanges, DoCheck, OnDestroy {
     private componentFactoryResolver: ComponentFactoryResolver,
     private differs: KeyValueDiffers,
     private componentFactory: JssComponentFactoryService,
+    private cd: ChangeDetectorRef,
     @Inject(PLACEHOLDER_MISSING_COMPONENT_COMPONENT) private missingComponentComponent: Type<any>
-  ) { }
+  ) {}
 
   ngOnDestroy() {
     this.destroyed = true;
@@ -88,24 +98,31 @@ export class PlaceholderComponent implements OnChanges, DoCheck, OnDestroy {
       return;
     }
     const updates: { [key: string]: any } = {};
-    changes.forEachRemovedItem((change) => updates[change.key] = null);
-    changes.forEachAddedItem((change) => updates[change.key] = change.currentValue);
-    changes.forEachChangedItem((change) => updates[change.key] = change.currentValue);
-    this._componentInstances.forEach((componentInstance) => this._setComponentInputs(componentInstance, updates));
+    changes.forEachRemovedItem((change) => (updates[change.key] = null));
+    changes.forEachAddedItem((change) => (updates[change.key] = change.currentValue));
+    changes.forEachChangedItem((change) => (updates[change.key] = change.currentValue));
+    this._componentInstances.forEach((componentInstance) =>
+      this._setComponentInputs(componentInstance, updates)
+    );
   }
 
   private _setComponentInputs(componentInstance: any, inputs: { [key: string]: any }) {
-    Object.keys(inputs).forEach((input) => componentInstance[input] = inputs[input]);
+    Object.keys(inputs).forEach((input) => (componentInstance[input] = inputs[input]));
   }
 
-  private _subscribeComponentOutputs(componentInstance: any, outputs: { [k: string]: (eventType: any) => void }) {
+  private _subscribeComponentOutputs(
+    componentInstance: any,
+    outputs: { [k: string]: (eventType: any) => void }
+  ) {
     Object.keys(outputs)
-      .filter((output) => componentInstance[output] && componentInstance[output] instanceof Observable)
-      .forEach((output) => (componentInstance[output] as Observable<any>)
-        .pipe(
-        takeWhile(() => !this.destroyed)
+      .filter(
+        (output) => componentInstance[output] && componentInstance[output] instanceof Observable
       )
-        .subscribe(outputs[output]));
+      .forEach((output) =>
+        (componentInstance[output] as Observable<any>)
+          .pipe(takeWhile(() => !this.destroyed))
+          .subscribe(outputs[output])
+      );
   }
 
   private _render() {
@@ -118,49 +135,58 @@ export class PlaceholderComponent implements OnChanges, DoCheck, OnDestroy {
 
     if (!this.name && !this.renderings) {
       // tslint:disable-next-line:max-line-length
-      console.warn(`Placeholder name was not specified, and explicit renderings array was not passed. Placeholder requires either name and rendering, or renderings.`);
+      console.warn(
+        `Placeholder name was not specified, and explicit renderings array was not passed. Placeholder requires either name and rendering, or renderings.`
+      );
       return;
     }
 
     const placeholder = this.renderings || getPlaceholder(this.rendering, this.name || '');
 
     if (!placeholder) {
-      console.warn(`Placeholder '${this.name}' was not found in the current rendering data`, JSON.stringify(this.rendering, null, 2));
+      console.warn(
+        `Placeholder '${this.name}' was not found in the current rendering data`,
+        JSON.stringify(this.rendering, null, 2)
+      );
       return;
     }
 
     // if the placeholder is empty (contains only raw renderings), then we may need to use the empty template if it's defined
-    const placeholderIsEmpty = placeholder.every((rendering: ComponentRendering | HtmlElementRendering) => isRawRendering(rendering));
+    const placeholderIsEmpty = placeholder.every(
+      (rendering: ComponentRendering | HtmlElementRendering) => isRawRendering(rendering)
+    );
 
     if (this.renderEmptyTemplate && placeholderIsEmpty) {
-      this.view.createEmbeddedView(this.renderEmptyTemplate.templateRef,
-        {
-          renderings: placeholder,
-        });
+      this.view.createEmbeddedView(this.renderEmptyTemplate.templateRef, {
+        renderings: placeholder,
+      });
     } else {
-      this.componentFactory.getComponents(placeholder)
-        .then((components) => components.forEach((rendering, index) => {
-          if (this.renderEachTemplate && !isRawRendering(rendering.componentDefinition)) {
-            this._renderTemplatedComponent(rendering.componentDefinition, index);
-          } else {
-            this._renderEmbeddedComponent(rendering, index);
-          }
-      }));
+      this.componentFactory
+        .getComponents(placeholder)
+        .then((components) =>
+          components.forEach((rendering, index) => {
+            if (this.renderEachTemplate && !isRawRendering(rendering.componentDefinition)) {
+              this._renderTemplatedComponent(rendering.componentDefinition, index);
+            } else {
+              this._renderEmbeddedComponent(rendering, index);
+            }
+          })
+        )
+        .then(() => this.cd.markForCheck());
     }
   }
 
   private _renderTemplatedComponent(
     rendering: ComponentRendering | HtmlElementRendering,
     index: number
-   ) {
+  ) {
     // the render-each template takes care of all component mapping etc
     // generally using <sc-render-component> which is about like _renderEmbeddedComponent()
     // as a separate component
-    this.view.createEmbeddedView(this.renderEachTemplate.templateRef,
-      {
-        rendering,
-        index,
-      });
+    this.view.createEmbeddedView(this.renderEachTemplate.templateRef, {
+      rendering,
+      index,
+    });
   }
 
   private _renderEmbeddedComponent(rendering: ComponentFactoryResult, index: number) {
@@ -178,7 +204,8 @@ export class PlaceholderComponent implements OnChanges, DoCheck, OnDestroy {
     }
 
     const componentFactory =
-      rendering.componentFactory || this.componentFactoryResolver.resolveComponentFactory(rendering.componentImplementation);
+      rendering.componentFactory ||
+      this.componentFactoryResolver.resolveComponentFactory(rendering.componentImplementation);
 
     const componentInstance = this.view.createComponent(componentFactory, index).instance;
     componentInstance.rendering = rendering.componentDefinition;
