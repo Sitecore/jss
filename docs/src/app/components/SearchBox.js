b0ce8997cf64ce0f0773837b50d54e10a6a19474
Kam Figy <kamsar@gmail.com>
2019-02-12 07:13:52 -0800

    Make Algolia resilient to failures without taking out the content

--- docs/src/app/components/SearchBox.js
+++ docs/src/app/components/SearchBox.js
@@ -1,35 +1,57 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
 
 export default class SearchBox extends React.Component {
-  componentDidMount() {
-    docsearch({
-      apiKey: 'be76fcb7f3c958d6058ed6410d997522',
-      indexName: 'jss',
-      inputSelector: '#search-input',
-      debug: false,
-    });
+  constructor(props) {
+    super(props);
+    this.state = { algoliaEnabled: true };
   }
+
+  componentDidMount() {
+    if (typeof docsearch === 'function') {
+      try {
+        // eslint-disable-next-line no-undef
+        docsearch({
+          apiKey: 'be76fcb7f3c958d6058ed6410d997522',
+          indexName: 'jss',
+          inputSelector: '#search-input',
+          debug: false,
+        });
+        this.setState({ algoliaEnabled: true });
+      } catch (e) {
+        console.log(e);
+        this.setState({ algoliaEnabled: false });
+      }
+    } else {
+      this.setState({ algoliaEnabled: false });
+    }
+  }
+
   render() {
-    return (
-      <form className="bd-search d-flex align-items-center">
-        <input
-          type="search"
-          className="form-control"
-          id="search-input"
-          placeholder="Search..."
-          aria-label="Search for..."
-          autoComplete="off"
-        />
-        <button
-          className="btn btn-link bd-search-docs-toggle d-md-none p-0 ml-3"
-          type="button"
-          data-toggle="collapse"
-          data-target="#bd-docs-nav"
-          aria-controls="bd-docs-nav"
-          aria-expanded="false"
-          aria-label="Toggle docs navigation"
-        />
-      </form>
-    );
+    if (this.state.algoliaEnabled) {
+      return (
+        <form className="bd-search d-flex align-items-center">
+          <input
+            type="search"
+            className="form-control"
+            id="search-input"
+            placeholder="Search..."
+            aria-label="Search for..."
+            autoComplete="off"
+          />
+          <button
+            className="btn btn-link bd-search-docs-toggle d-md-none p-0 ml-3"
+            type="button"
+            data-toggle="collapse"
+            data-target="#bd-docs-nav"
+            aria-controls="bd-docs-nav"
+            aria-expanded="false"
+            aria-label="Toggle docs navigation"
+          />
+        </form>
+      );
+    }
+
+    return null;
   }
 }
