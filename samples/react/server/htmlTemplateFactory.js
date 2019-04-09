350061a0bf028da6224d258d5569c3de52dd2552
Adam Weber <adw@sitecore.net>
2019-04-09 22:54:09 +0100

    Removed unnecessary console.log statements from htmlTemplateFactory.js

--- samples/react/server/htmlTemplateFactory.js
+++ samples/react/server/htmlTemplateFactory.js
@@ -1,10 +1,8 @@
 export function getHtmlTemplate(state) {
   let indexTemplate;
   if (process.env.JSS_RENDER_ENGINE && process.env.JSS_RENDER_ENGINE === 'http') {
-    console.log('using jss render host html template');
     indexTemplate = require('../build/index.html');
   } else {
-    console.log('using default html template');
     indexTemplate = require('../build/index.html');
   }
   return Promise.resolve(indexTemplate);
