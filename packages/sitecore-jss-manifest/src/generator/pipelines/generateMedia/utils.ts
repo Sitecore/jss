7e668bc426e5ff1cbf880d50e7388b4da1bb619b
Adam Weber <adamweber01@gmail.com>
2019-04-17 16:05:18 -0500

    Added method to recursively resolve fields from all inherited templates and ancestors. Allows the buildMediaOutput function to properly inspect all template fields (including inherited fields) when extracting image/file field values.

--- packages/sitecore-jss-manifest/src/generator/pipelines/generateMedia/utils.ts
+++ packages/sitecore-jss-manifest/src/generator/pipelines/generateMedia/utils.ts
@@ -1,10 +1,55 @@
 export const enhanceTemplates = (templates: any) => {
-  const newTemplates: {[k: string]: any} = [...templates];
+  const newTemplates: { [k: string]: any } = [...templates];
   newTemplates.getTemplate = function getTemplate(templateName: string) {
-    return this.find((template: any) => template.name === templateName);
+    const foundTemplate = this.find((template) => template.name === templateName);
+    if (foundTemplate) {
+      foundTemplate.allFields = getAllTemplateFields(foundTemplate, templates);
+    }
+    return foundTemplate;
   };
   return newTemplates;
 };
+
+// NOTE: this function does not attempt to do anything about field names
+// that might be duplicated in the template inheritance tree. That said, for
+// the purposes of extracting media field values, we don't need to be concerned
+// about duplicates or naming collisions. We simply need a field type and
+// a field value.
+function getAllTemplateFields(template: any, templateCollection: any) {
+  if (!template) {
+    return null;
+  }
+
+  // If the template already has an `allFields` property, assume that
+  // its inheritance tree has already been resolved.
+  if (template.allFields && Array.isArray(template.allFields)) {
+    return template.allFields;
+  }
+
+  const allFields: any[] = [];
+  
+  // If the template has its own fields, add them to the `allFields` array.
+  if (template.fields && Array.isArray(template.fields)) {
+    allFields.push(...template.fields);
+  }
+
+  // Recursively add fields from all inherited templates and any ancestors.
+  if (template.inherits && Array.isArray(template.inherits)) {
+    template.inherits.forEach((inheritedTemplateName) => {
+      const inheritedTemplate = templateCollection.find((t) => t.name === inheritedTemplateName);
+      if (!inheritedTemplate || !inheritedTemplate.fields || !Array.isArray(inheritedTemplate.fields)) {
+        return;
+      }
+      
+      const inheritedFields = getAllTemplateFields(inheritedTemplate, templateCollection);
+      if (inheritedFields) {
+        allFields.push(...inheritedFields);
+      }
+    });
+  }
+
+  return allFields;
+}
 
 function getMediaFieldValue(field: any) {
   return field.value;
@@ -18,7 +63,7 @@ function getTreelistFieldValue(field: any, templates: any) {
   }, []);
 }
 
-function getFieldValues({ field, templates }: { field: any, templates: any }) {
+function getFieldValues({ field, templates }: { field: any; templates: any }) {
   switch (field.type) {
     case 'Image':
       return [getMediaFieldValue(field)];
@@ -42,11 +87,11 @@ export function buildMediaOutput(item: any, templates: any) {
   }
 
   const fields = item.fields.reduce((result: any, field: any) => {
-    if (!template.fields) {
+    if (!template.allFields) {
       return [];
     }
 
-    const templateField = template.fields.find((f: any) => f.name === field.name);
+    const templateField = template.allFields.find((f: any) => f.name === field.name);
     if (templateField) {
       return [...result, { ...field, type: templateField.type }];
     }
