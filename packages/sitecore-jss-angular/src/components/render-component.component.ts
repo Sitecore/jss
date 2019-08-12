921f02eb321de183f12ed4da36c5256103c7702e
Jeroen van der Last <jeroen.vander.last@essent.nl>
2019-08-12 13:53:24 +0200

    Update to angular 8

--- packages/sitecore-jss-angular/src/components/render-component.component.ts
+++ packages/sitecore-jss-angular/src/components/render-component.component.ts
@@ -36,7 +36,7 @@ export class RenderComponentComponent implements OnChanges {
 
   @Input() rendering: ComponentRendering | HtmlElementRendering;
   @Input() outputs: { [k: string]: (eventType: any) => void };
-  @ViewChild('view', { read: ViewContainerRef }) private view: ViewContainerRef;
+  @ViewChild('view', { read: ViewContainerRef, static: false }) private view: ViewContainerRef;
 
   @Input()
   set inputs(value: { [key: string]: any }) {
