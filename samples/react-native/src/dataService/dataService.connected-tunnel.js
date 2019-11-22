2e3f9dbee88166d9d9870020a5671bab3959c606
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2019-11-22 09:25:33 +0200

    fix image loading in connected mode

--- samples/react-native/src/dataService/dataService.connected-tunnel.js
+++ samples/react-native/src/dataService/dataService.connected-tunnel.js
@@ -7,12 +7,14 @@ import {
 } from './dataService.connected';
 import { mapNestedJson } from './util';
 
+global.__SC_API_HOST__ = __SC_API_HOST__
+
 const processObjectProperty = (key, value) => {
   // replace 'jssreactnative' with ngrok URL.
   // urls returned from layout service will resolve to the Sitecore site host, e.g. http://jssreactnative.
   // need to replace 'http://jssreactnative' with the tunnel host so that urls resolve correctly via the tunnel.
   if (key === 'src') {
-    const src = value.replace(__SC_API_HOST__, __SC_TUNNEL_HOST__);
+    const src = value.toLowerCase().replace(__SC_API_HOST__.toLowerCase(), __SC_TUNNEL_HOST__);
     return src;
   }
   return value;
