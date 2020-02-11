f5f744aac50d894436683a2ae181f9735a3ee59b
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-02-11 12:34:08 +0200

    update locks and refactor code

--- packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
+++ packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
@@ -1,48 +1,43 @@
-import React from 'react';
-import { ComponentFactory } from './sharedTypes';
-import { expect } from 'chai';
-// const iterate = require('leakage').iterate;
-import { SitecoreContext, SitecoreContextFactory } from './SitecoreContext';
-import { withSitecoreContext } from '../enhancers/withSitecoreContext';
-const create = require('react-test-renderer').create;
+import React, { FC } from 'react';
+import { expect, use, spy } from 'chai';
+import spies from 'chai-spies';
+import { shallow } from 'enzyme';
 
-const TestNestedComponent: React.FC = (props: any) => (<div>{props.sitecoreContext && 'test'}</div>);
-const TestNestedComponentWithContext: React.FC = withSitecoreContext()(TestNestedComponent as any);
-const arrayOfComponents: React.Component[] = [];
-const mockComponentFactory: ComponentFactory = (name: string) => arrayOfComponents[name as any];
+use(spies);
+
+import { SitecoreContext, SitecoreContextFactory } from './SitecoreContext';
+import { ComponentFactory } from './sharedTypes';
+import { withSitecoreContext, ComponentConsumerProps } from '../enhancers/withSitecoreContext'
+
+const NestedComponent: FC<ComponentConsumerProps> = (props: ComponentConsumerProps) => <div>{props.sitecoreContext && 'test'}</div>;
+const NestedComponentWithContext: FC = withSitecoreContext()(NestedComponent);
+
+const components = new Map();
+const mockComponentFactory: ComponentFactory = name => components.get(name);
+
 const singleton = new SitecoreContextFactory();
 
 describe('SitecoreContext', () => {
-  // it('should not leak memory', () => {
-  //   iterate(() => {
-  //     const wrappedComponent = create(
-  //       <SitecoreContext componentFactory={mockComponentFactory} contextFactory={singleton}>
-  //         <TestNestedComponentWithContext />
-  //       </SitecoreContext>
-  //     );
-  //
-  //     singleton.setSitecoreContext({...singleton.context, myCustomProp: "asdfasdfasdfas"});
-  //
-  //     wrappedComponent.unmount();
-  //   }, {iterations:100});
-  // });
-
   it('should unsubscribe from SitecoreContextFactory on unmount', () => {
-    const wrappedComponent = create(
+    const wrappedComponent = shallow(
       <SitecoreContext componentFactory={mockComponentFactory} contextFactory={singleton}>
-        <TestNestedComponentWithContext />
+        <NestedComponentWithContext />
       </SitecoreContext>
     );
 
-    const myCustomSubscriberContext: () => Object = () => ({});
+    const spyContextListener = spy((value: any) => value);
 
-    singleton.subscribeToContext(myCustomSubscriberContext);
+    singleton.subscribeToContext(spyContextListener);
     expect(singleton.subscribers).to.have.lengthOf(2);
 
+    singleton.setSitecoreContext('mock');
+
+    expect(spyContextListener).to.have.called.with('mock');
+    expect(spyContextListener).on.have.been.called.exactly(1);
+
     wrappedComponent.unmount();
+
     expect(singleton.subscribers).to.have.lengthOf(1);
-    expect(singleton.subscribers[0]).to.eqls(myCustomSubscriberContext);
+    expect(singleton.subscribers[0]).to.eqls(spyContextListener);
   });
 });
-
-
