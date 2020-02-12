6f776b097277ada610a688990ed598ed9aaf8a6c
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-02-12 12:20:14 +0200

    samples/react add eject script

---
+++ samples/react/scripts/eject.js
@@ -0,0 +1,14 @@
+const crossSpawn = require('cross-spawn');
+const path = require('path');
+const fs = require('fs-extra');
+
+const SOURCE_PATH = path.resolve(__dirname, '../scripts')
+const COPY_PATH = path.resolve(__dirname, '../scripts-copy')
+
+fs.removeSync(SOURCE_PATH);
+
+crossSpawn.sync(`npm run eject`, {
+  stdio: 'inherit',
+});
+
+fs.moveSync(COPY_PATH, SOURCE_PATH);
