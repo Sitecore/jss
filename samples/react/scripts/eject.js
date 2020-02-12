4d8ca3a365c8437914089cab463f0dab5cd80053
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-02-12 13:01:31 +0200

    change script

--- samples/react/scripts/eject.js
+++ samples/react/scripts/eject.js
@@ -3,13 +3,11 @@ const path = require('path');
 const fs = require('fs-extra');
 
 const SOURCE_PATH = path.resolve(__dirname, '../scripts')
-const COPY_PATH = path.resolve(__dirname, '../scripts-copy')
 
 fs.removeSync(SOURCE_PATH);
 
 crossSpawn.sync('git add .');
+crossSpawn.sync('git commit -am "Save before eject"');
 crossSpawn.sync(`npm run eject`, {
   stdio: 'inherit',
 });
-
-fs.moveSync(COPY_PATH, SOURCE_PATH);
