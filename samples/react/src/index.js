ce8598bd7f188c8002658964066c9b7373389365
sc-illiakovalenko <55081915+sc-illiakovalenko@users.noreply.github.com>
2020-04-01 13:49:19 +0300

    Include polyfills for IE11 in react sample app

--- samples/react/src/index.js
+++ samples/react/src/index.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
