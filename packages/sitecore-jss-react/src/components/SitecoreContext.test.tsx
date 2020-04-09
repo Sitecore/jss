5ac4f821eacd5d66a8a3016751bf9208af700438
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-04-09 15:27:23 +0300

    [sitecore-jss-react] add UT for SitecoreContext

--- packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
+++ packages/sitecore-jss-react/src/components/SitecoreContext.test.tsx
@@ -15,29 +15,117 @@ const NestedComponentWithContext: FC = withSitecoreContext()(NestedComponent);
 const components = new Map();
 const mockComponentFactory: ComponentFactory = name => components.get(name);
 
-const singleton = new SitecoreContextFactory();
+const mockSitecoreContext = new SitecoreContextFactory();
 
 describe('SitecoreContext', () => {
   it('should unsubscribe from SitecoreContextFactory on unmount', () => {
     const wrappedComponent = shallow(
-      <SitecoreContext componentFactory={mockComponentFactory} contextFactory={singleton}>
+      <SitecoreContext componentFactory={mockComponentFactory} contextFactory={mockSitecoreContext}>
         <NestedComponentWithContext />
       </SitecoreContext>
     );
 
     const spyContextListener = spy((value: any) => value);
 
-    singleton.subscribeToContext(spyContextListener);
-    expect(singleton.subscribers).to.have.lengthOf(2);
+    mockSitecoreContext.subscribeToContext(spyContextListener);
+    expect(mockSitecoreContext.subscribers).to.have.lengthOf(2);
 
-    singleton.setSitecoreContext('mock');
+    mockSitecoreContext.setSitecoreContext('mock');
 
     expect(spyContextListener).to.have.called.with('mock');
     expect(spyContextListener).on.have.been.called.exactly(1);
 
     wrappedComponent.unmount();
 
-    expect(singleton.subscribers).to.have.lengthOf(1);
-    expect(singleton.subscribers[0]).to.eqls(spyContextListener);
+    expect(mockSitecoreContext.subscribers).to.have.lengthOf(1);
+    expect(mockSitecoreContext.subscribers[0]).to.eqls(spyContextListener);
+  });
+
+  it('should get sitecore context value', () => {
+    const wrappedComponent = shallow<SitecoreContext>(
+      <SitecoreContext componentFactory={mockComponentFactory} contextFactory={mockSitecoreContext}>
+        <NestedComponentWithContext />
+      </SitecoreContext>
+    );
+
+    const value = wrappedComponent.instance().getSitecoreContextValue();
+
+    expect(mockSitecoreContext instanceof SitecoreContextFactory).equal(true);
+    expect(value instanceof SitecoreContextFactory).equal(false);
+
+    expect(value.context).equal(mockSitecoreContext.context);
+  });
+
+  describe('SitecoreContextFactory', () => {
+    it('should get sitecore context', () => {
+      const c = new SitecoreContextFactory();
+
+      expect(c.getSitecoreContext()).deep.equal({ pageEditing: false });
+
+      c.context = {
+        pageEditing: false,
+        text: 'xxx',
+        count: 10
+      };
+
+      expect(c.getSitecoreContext()).deep.equal({
+        pageEditing: false,
+        text: 'xxx',
+        count: 10
+      });
+    });
+
+    it('should set sitecore context', () => {
+      const c = new SitecoreContextFactory();
+      
+      const fn = spy();
+
+      c.subscribeToContext(fn);
+
+      expect(c.getSitecoreContext()).deep.equal({ pageEditing: false });
+
+      c.setSitecoreContext({
+        text: 'xxx',
+        value: 10
+      });
+
+      expect(c.getSitecoreContext()).deep.equal({
+        text: 'xxx',
+        value: 10
+      });
+
+      expect(fn).to.be.called.with({
+        text: 'xxx',
+        value: 10
+      });
+    });
+
+    it('should subscribe to context', () => {
+      const c = new SitecoreContextFactory();
+
+      const fn = spy();
+
+      c.subscribeToContext(fn);
+
+      expect(c.subscribers.length).equal(1);
+
+      c.subscribers[0]();
+
+      expect(fn).to.be.called();
+    });
+
+    it('should unsubscribe from context', () => {
+      const c = new SitecoreContextFactory();
+
+      const fn = spy();
+
+      c.subscribeToContext(fn);
+
+      expect(c.subscribers.length).equal(1);
+
+      c.unsubscribeFromContext(fn);
+
+      expect(c.subscribers.length).equal(0);
+    });
   });
 });
