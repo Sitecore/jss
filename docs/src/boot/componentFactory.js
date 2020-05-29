a183363e6b47532288ca846546b5c149d20a43d2
Anastasiya Flynn <7372349+anastasiya29@users.noreply.github.com>
2020-05-29 18:51:37 -0400

    New components need to be added to componentFactory manually

--- docs/src/boot/componentFactory.js
+++ docs/src/boot/componentFactory.js
@@ -1,16 +1,21 @@
-import Article from '../app/components/Article';
-import FeatureJumbo from '../app/components/FeatureJumbo';
-import FeatureJumboShort from '../app/components/FeatureJumboShort';
-import Hero from '../app/components/Hero';
-import Logo from '../app/components/Logo';
-import SideNav from '../app/components/Navigation/SideNav';
-import TileHeadline from '../app/components/TileHeadline';
-import Tile from '../app/components/Tile';
-import ArticleContainer from '../app/containers/ArticleContainer';
-import TwocolContainer from '../app/containers/TwocolContainer';
-import PageContainer from '../app/containers/PageContainer';
-import TileContainer from '../app/containers/TileContainer';
-import TileWithButton from '../app/components/TileWithButton';
+import Article from 'components/Article';
+import FeatureJumbo from 'components/FeatureJumbo';
+import FeatureJumboShort from 'components/FeatureJumboShort';
+import Hero from 'components/Hero';
+import Logo from 'components/Logo';
+import MainNav from 'components/Navigation/MainNav';
+import SecondaryNav from 'components/Navigation/SecondaryNav';
+import SideNav from 'components/Navigation/SideNav';
+import TileHeadline from 'components/TileHeadline';
+import Tile from 'components/Tile';
+import TileWithButton from 'components/TileWithButton';
+
+import ArticleContainer from 'containers/ArticleContainer';
+import TileContainer from 'containers/TileContainer';
+import TwocolContainer from 'containers/TwocolContainer';
+import PageContainer from 'containers/PageContainer';
+import Row from 'containers/Row';
+
 
 const components = new Map();
 components.set('Article', Article);
@@ -18,15 +23,18 @@ components.set('FeatureJumbo', FeatureJumbo);
 components.set('FeatureJumboShort', FeatureJumboShort);
 components.set('Hero', Hero);
 components.set('Logo', Logo);
+components.set('MainNav', MainNav);
+components.set('SecondaryNav', SecondaryNav);
 components.set('SideNav', SideNav);
 components.set('TileHeadline', TileHeadline);
 components.set('Tile', Tile);
 components.set('TileWithButton', TileWithButton);
 
-components.set('PageContainer', PageContainer);
 components.set('ArticleContainer', ArticleContainer);
-components.set('TwocolContainer', TwocolContainer);
 components.set('TileContainer', TileContainer);
+components.set('TwocolContainer', TwocolContainer);
+components.set('PageContainer', PageContainer);
+components.set('Row', Row);
 
 const componentFactory = (componentName) => components.get(componentName);
 
