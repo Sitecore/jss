fbf7bd2656b2f48ef605171f85de420d8d9f6efa
Thomas Tyack <ttyack@hotmail.com>
2020-06-16 15:28:38 +1000

    YML Linter during builds

---
+++ samples/react/scripts/yaml-lint.js
@@ -0,0 +1,37 @@
+const path = require('path');
+var fs = require('fs');
+const yamlLint = require('yaml-lint');
+
+console.log("Started to validate the YML Files");
+
+const getAllFiles = function(dirPath, arrayOfFiles) {
+    files = fs.readdirSync(dirPath);
+
+    arrayOfFiles = arrayOfFiles || [];
+
+    files.forEach(function(file) {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+        } else {
+            arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+        }
+    })
+    return arrayOfFiles;
+}
+
+const allDataFiles = getAllFiles('./data/');
+for(var i=0; i<allDataFiles.length; i++) {
+    var fileName = allDataFiles[i];
+    if(fileName.indexOf('\\scripts') > -1)
+        fileName = fileName.replace('\\scripts', '');
+
+    if(fileName.indexOf('.yml') > -1) {
+        console.log('validating YML: ' + fileName);
+        yamlLint.lintFile(fileName).then(() => {
+            console.log('Valid YAML file. > ' + fileName);
+        }).catch((error) => {
+            console.error('Invalid YAML file > ' + fileName + '\n\r', error);
+            process.exit(1); // Exit with a failure
+        });
+    }
+}
