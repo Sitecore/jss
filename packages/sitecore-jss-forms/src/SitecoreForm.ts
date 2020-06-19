05facc31bedf9aebcad90cbe0b66b5ea759a3534
sc-illiakovalenko <55081915+sc-illiakovalenko@users.noreply.github.com>
2020-06-19 14:06:05 +0300

    [JSS][Forms] Assign Sitecore.Context.Item in Forms submit actions on a JSS Forms submit (#386)

--- packages/sitecore-jss-forms/src/SitecoreForm.ts
+++ packages/sitecore-jss-forms/src/SitecoreForm.ts
@@ -5,6 +5,7 @@ import { HtmlFormField } from './HtmlFormField';
 /** The model returned from the Sitecore Forms JSON API for a form schema */
 export interface SitecoreForm {
   htmlPrefix: string;
+  contextItemId: string;
   formSessionId: HtmlFormField;
   formItemId: HtmlFormField;
   pageItemId: HtmlFormField;
