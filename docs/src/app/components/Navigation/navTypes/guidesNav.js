ef26a5fefd5a0f11eae41488c5feb6279f528832
Anastasiya Flynn <7372349+anastasiya29@users.noreply.github.com>
2020-07-01 02:52:21 -0400

    Update 'Guides' side nav

--- docs/src/app/components/Navigation/navTypes/guidesNav.js
+++ docs/src/app/components/Navigation/navTypes/guidesNav.js
@@ -3,9 +3,31 @@ const guidesNav = {
   displayName: 'Guides & Recommended Practices',
   children: [
     {
+      url: 'sitecore-for-feds',
+      displayName: 'Sitecore for Front-End Devs',
+      children: [
+        {
+          url: 'js-vs-jss',
+          displayName: 'JS vs JSS'
+        },
+        {
+          url: 'terminology',
+          displayName: 'Terminology'
+        },
+        {
+          url: 'common-pitfalls',
+          displayName: 'Common Pitfalls'
+        }
+      ]
+    },
+    {
       url: 'getting-started',
       displayName: 'Getting Started',
       children: [
+        {
+          url: 'migrating',
+          displayName: 'Migrating to JSS'
+        },
         {
           url: 'solution-setup',
           displayName: 'Solution Setup'
@@ -21,20 +43,66 @@ const guidesNav = {
       ]
     },
     {
+      url: 'devex',
+      displayName: 'DevEx',
+      children: [
+        {
+          url: 'developer-workflows',
+          displayName: 'Developer Workflows'
+        },
+        {
+          url: 'tools',
+          displayName: 'Tools'
+        },
+        {
+          url: 'front-end-debugging',
+          displayName: 'Front-End Debugging'
+        },
+        {
+          url: 'back-end-debugging',
+          displayName: 'Back-End Debugging'
+        },
+      ]
+    },
+    {
       url: 'code-patterns',
       displayName: 'Code Patterns',
       children: [
+        {
+          url: 'validation',
+          displayName: 'Validation'
+        },
+        {
+          url: 'media-handling',
+          displayName: 'Media Handling'
+        },
         {
           url: 'ssr',
           displayName: 'SSR'
         },
         {
-          url: 'content-resolvers',
-          displayName: 'Content Resolvers'
+          url: 'extend-layout-service',
+          displayName: 'Extend Layout Service'
         },
         {
           url: 'custom-error-pages',
           displayName: 'Custom Error Pages'
+        },
+        {
+          url: 'security',
+          displayName: 'Security'
+        },
+        {
+          url: 'tracking-and-personalization',
+          displayName: 'Tracking & Personalization'
+        },
+        {
+          url: 'dictionary-service',
+          displayName: 'Dictionary Service'
+        },
+        {
+          url: 'routing',
+          displayName: 'Routing'
         }
       ]
     },
@@ -49,23 +117,33 @@ const guidesNav = {
       ]
     },
     {
-      url: 'devex',
-      displayName: 'Developer Experience',
+      url: 'infrastructure',
+      displayName: 'Infrastructure',
       children: [
         {
-          url: 'front-end debugging',
-          displayName: 'Front-End Debugging'
+          url: 'deployment-topologies',
+          displayName: 'Deployment Topologies'
         },
         {
-          url: 'back-end debugging',
-          displayName: 'Back-End Debugging'
-        },
-        {
-          url: 'tools',
-          displayName: 'Tools'
+          url: 'performance',
+          displayName: 'Performance'
         }
       ]
-    }
+    },
+    {
+      url: 'graphql',
+      displayName: 'GraphQL',
+      children: [
+        {
+          url: 'extending-graphql',
+          displayName: 'Extending GraphQL'
+        },
+        {
+          url: 'graphql-security',
+          displayName: 'GraphQL Security'
+        }
+      ]
+    },
   ],
   links: []
 };
