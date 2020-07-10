3ef8f35a3dbc04f29cc0e8a4396eeee622dfe1ba
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-07-10 11:23:42 +0300

    Revert setupProxy

--- samples/react/src/setupProxy.js
+++ samples/react/src/setupProxy.js
@@ -1,24 +1,15 @@
 const proxy = require('http-proxy-middleware');
-const ip = require('ip');
 const config = require('./temp/config');
 const { isDisconnected } = require('./util');
 
 module.exports = (app) => {
-  // when disconnected we proxy to the local faux layout service host,
-  // see scripts/disconnected-mode-proxy.js
-  const proxyURL = `http://localhost:${process.env.PROXY_PORT || 3042}/`;
-  const ipProxyURL = `http://${ip.address()}:${process.env.PROXY_PORT || 3042}/`;
-
   if (isDisconnected()) {
-    const proxyOptions = {
-      target: proxyURL,
-      router: {
-        [config.ipAddress]: ipProxyURL
-      }
-    }
+    // when disconnected we proxy to the local faux layout service host,
+    // see scripts/disconnected-mode-proxy.js
+    const proxyUrl = `http://localhost:${process.env.PROXY_PORT || 3042}/`;
 
-    app.use(proxy('/sitecore', proxyOptions));
-    app.use(proxy('/data/media', proxyOptions));
+    app.use(proxy('/sitecore', { target: proxyUrl }));
+    app.use(proxy('/data/media', { target: proxyUrl }));
   } else {
     // when in connected mode we want to proxy Sitecore paths
     // off to Sitecore
