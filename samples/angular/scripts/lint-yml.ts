51224b8d67921835eac834e48f4dd2485c8836dc
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-07-15 13:23:29 +0300

    [Angular sample] add yml-lint

---
+++ samples/angular/scripts/lint-yml.ts
@@ -0,0 +1,30 @@
+const path = require('path');
+const glob = require('glob');
+const yamlLint = require('yaml-lint');
+
+const DATA_DIR_PATH = path.resolve(__dirname, '..', './data/');
+
+const files: string[] = glob.sync('**/*.yml', { cwd: DATA_DIR_PATH });
+
+console.log('Linting .yml files...');
+
+let isValid = true;
+
+const promises = files.map(filename => {
+  filename = path.resolve(DATA_DIR_PATH, filename);
+
+  return yamlLint.lintFile(filename).catch(e => {
+    console.error(filename);
+    console.log('\x1b[31m', 'error', '\x1b[0m', e.message);
+    isValid = false;
+  });
+});
+
+Promise.all(promises).then(() => {
+  if (isValid) {
+    console.log('All files pass linting.');
+    return;
+  }
+
+  process.exit(1);
+});
