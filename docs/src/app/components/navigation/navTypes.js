a449dc4fbac904a4f8d02ffb67d81f3150418c79
Anastasiya Flynn <7372349+anastasiya29@users.noreply.github.com>
2020-08-21 00:08:36 -0400

    add 'navigation' folder with correct casing

---
+++ docs/src/app/components/navigation/navTypes.js
@@ -0,0 +1,10 @@
+'use strict';
+import docsNav from './navTypes/docsNav';
+import connectedGuideNav from './navTypes/connectedGuideNav';
+import guidesNav from './navTypes/guidesNav';
+
+export default {
+  docs: docsNav,
+  guides: guidesNav,
+  jssConnected: connectedGuideNav
+};
