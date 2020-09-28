a933c3c228a41dac6be8f7ad5fdf3e7376f7fa11
sc-illiakovalenko <55081915+sc-illiakovalenko@users.noreply.github.com>
2020-09-29 00:02:29 +0300

    [JSS 12][React sample] Race condition, under load React app may render HTML from a different route (#454)

--- samples/react/src/components/ContentBlock/index.js
+++ samples/react/src/components/ContentBlock/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { RichText, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { Text, RichText } from '@sitecore-jss/sitecore-jss-react';
 
 /**
  * A simple Content Block component, with a heading and rich text block.
  * This is the most basic building block of a content site, and the most basic
  * JSS component that's useful.
  */
-const ContentBlock = ({ fields, sitecoreContext }) => (
+const ContentBlock = ({ fields }) => (
   <React.Fragment>
-    <h2 className="display-5">WHAT is the airspeed velocity of an unladen swallow?</h2>
-    <h2 className="display-5">African or European?</h2>
-    { sitecoreContext.pageEditing ? <marquee>I am an element that will only display in Experience Editor</marquee> : null }
+    <Text tag="h2" className="display-4" field={fields.heading} />
 
-
-    <div style={{marginTop: 50}}>
-      <RichText className="contentDescription" field={fields.content} />
-    </div>
+    <RichText className="contentDescription" field={fields.content} />
   </React.Fragment>
 );
 
-export default withSitecoreContext()(ContentBlock);
+export default ContentBlock;
