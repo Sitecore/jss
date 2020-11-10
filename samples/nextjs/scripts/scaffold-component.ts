cb4f36cba4eccef70579a1d14c1d8d8d43faea73
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-11-10 15:53:37 +0200

    Edit scaffold-component script

--- samples/nextjs/scripts/scaffold-component.ts
+++ samples/nextjs/scripts/scaffold-component.ts
@@ -77,8 +77,8 @@ function scaffoldComponent() {
 
 interface IProps {
   fields: {
-    heading: Field<string>
-  }
+    heading: Field<string>;
+  };
 }
 
 const ${exportVarName}: React.FC<IProps> = (props) => (
@@ -115,13 +115,11 @@ import { CommonFieldTypes, SitecoreIcon, Manifest } from '@sitecore-jss/sitecore
  * This function is invoked by convention (*.sitecore.ts) when 'jss manifest' is run.
  * @param {Manifest} manifest Manifest instance to add components to
  */
-export default function(manifest: Manifest) {
+export default function (manifest: Manifest): void {
   manifest.addComponent({
     name: '${componentName}',
     icon: SitecoreIcon.DocumentTag,
-    fields: [
-      { name: 'heading', type: CommonFieldTypes.SingleLineText },
-    ],
+    fields: [{ name: 'heading', type: CommonFieldTypes.SingleLineText }],
     /*
     If the component implementation uses <Placeholder> or withPlaceholder to expose a placeholder,
     register it here, or components added to that placeholder will not be returned by Sitecore:
