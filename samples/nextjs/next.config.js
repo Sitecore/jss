f1b4417e00836480278441d6a6c786da5ba4648a
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2020-11-17 07:48:13 -0600

    [NextJs] i18n updates (#493)
    * Source Next.js i18n defaultLocale from package config
    
    * Fallback to using package config language for page props (allows sample to run when Next.js i18n is disabled)
    
    * Add locale variants of configured rewrites
    
    * Upgraded next-localization (w/ rosetta) library as dictionary solution of choice (atm). Simplified usage examples. Updated comments in _app.tsx.
    
    * Remove export scripts and build logic for now (since it doesn't work with next.js v10 i18n and image optimization), to be addressed in separate task

--- samples/nextjs/next.config.js
+++ samples/nextjs/next.config.js
@@ -1,20 +1,25 @@
 const jssConfig = require('./src/temp/config');
+const packageConfig = require('./package.json').config;
 
 const disconnectedServerUrl = `http://localhost:${process.env.DISCONNECTED_SERVER_PORT || 3042}/`;
 const disconnected = process.env.JSS_MODE === 'disconnected';
 
 module.exports = (phase) => {
-  
+
   const env = {
     // Expose current Next.js phase as an environment variable
     // See available phases here: https://github.com/vercel/next.js/blob/canary/packages/next/next-server/lib/constants.ts#L1-L4
-    NEXT_PHASE: phase
-  }
+    NEXT_PHASE: phase,
+  };
 
   const i18n = {
+    // These are all the locales you want to support in your application.
+    // These should generally match (or at least be a subset of) those in Sitecore.
     locales: ['en', 'da-DK'],
-    defaultLocale: 'en'
-  }
+    // This is the locale that will be used when visiting a non-locale
+    // prefixed path e.g. `/styleguide`.
+    defaultLocale: packageConfig.language,
+  };
 
   async function rewrites() {
     if (disconnected) {
@@ -24,12 +29,20 @@ module.exports = (phase) => {
           source: '/sitecore/:path*',
           destination: `${disconnectedServerUrl}/sitecore/:path*`,
         },
+        {
+          source: '/:locale/sitecore/:path*',
+          destination: `${disconnectedServerUrl}/sitecore/:path*`,
+        },
         // media items
         {
           source: '/data/media/:path*',
           destination: `${disconnectedServerUrl}/data/media/:path*`,
         },
-      ]
+        {
+          source: '/:locale/data/media/:path*',
+          destination: `${disconnectedServerUrl}/data/media/:path*`,
+        },
+      ];
     } else {
       // When in connected mode we want to proxy Sitecore paths off to Sitecore
       return [
@@ -37,9 +50,17 @@ module.exports = (phase) => {
           source: '/sitecore/:path*',
           destination: `${jssConfig.sitecoreApiHost}/sitecore/:path*`,
         },
+        {
+          source: '/:locale/sitecore/:path*',
+          destination: `${jssConfig.sitecoreApiHost}/sitecore/:path*`,
+        },
         // media items
         {
           source: '/-/:path*',
+          destination: `${jssConfig.sitecoreApiHost}/-/:path*`,
+        },
+        {
+          source: '/:locale/-/:path*',
           destination: `${jssConfig.sitecoreApiHost}/-/:path*`,
         },
         // visitor identification
@@ -47,13 +68,17 @@ module.exports = (phase) => {
           source: '/layouts/:path*',
           destination: `${jssConfig.sitecoreApiHost}/layouts/:path*`,
         },
-      ]
+        {
+          source: '/:locale/layouts/:path*',
+          destination: `${jssConfig.sitecoreApiHost}/layouts/:path*`,
+        },
+      ];
     }
-  }
+  };
 
   return {
     env,
     i18n,
-    rewrites
+    rewrites,
   };
 };
