289fd4f9a1a02b9bfebfcecf4020c05a22d02ae8
Adam Brauer <adab@sitecore.net>
2020-11-30 09:20:55 -0600

    Fixed linting errors

--- packages/sitecore-jss-nextjs/src/sharedTypes/component-module.ts
+++ packages/sitecore-jss-nextjs/src/sharedTypes/component-module.ts
@@ -1,4 +1,4 @@
-import {GetServerSideComponentProps, GetStaticComponentProps} from "./component-props";
+import { GetServerSideComponentProps, GetStaticComponentProps } from './component-props';
 
 type Module = {
   default: React.Component;
