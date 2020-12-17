04da0ed716bc7b4a267adb63e9745534d531ca52
Adam Brauer <adab@sitecore.net>
2020-12-17 09:55:28 -0600

    Doc/comments update

--- samples/nextjs/scripts/editing-server.ts
+++ samples/nextjs/scripts/editing-server.ts
@@ -4,16 +4,19 @@
   By default, this will spin up at localhost:3000, but can be configured with the
   `EditingServerOptions` parameter.
 
-  This should be set as the Http rendering engine in your Sitecore configuration
-  (already complete in the included sample \sitecore\config\JssNextWeb.config).
-  Be sure to update the `serverSideRenderingEngineEndpointUrl` accordingly as your
-  Editing Server configuration / endpoint changes.
-  See https://jss.sitecore.com/docs/fundamentals/services/view-engine
+  Your Sitecore CM instance will need to be able to access this endpoint. If your
+  public endpoint is different, you will need to set the `EDITING_HOST_PUBLIC_URL`
+  environment variable (see .env). This is set to http://localhost:3000 by default.
 
-  For local development, you may wish to utilize ngrok to proxy requests, which can
-  be helpful for certain firewall configurations or testing remote instances
+  For local development, for example, you may wish to utilize ngrok to proxy requests,
+  which can be helpful for certain firewall configurations or testing remote instances
   via the 'sc_httprenderengineurl' query string paramter.
   See https://ngrok.com
+
+  This endpoint should match the HTTP rendering engine in your Sitecore configuration
+  (see \sitecore\config\JssNextWeb.config). Be sure to update the `serverSideRenderingEngineEndpointUrl`
+  accordingly as your Editing Server configuration / public endpoint changes.
+  See https://jss.sitecore.com/docs/fundamentals/services/view-engine
 */
 
 import { startEditingServer } from '@sitecore-jss/sitecore-jss-nextjs-editing-host';
