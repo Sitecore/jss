0f5e9116aecac1d4d4132719cae4844c5946c683
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2020-12-31 11:36:49 +0200

    Add jsdoc

--- samples/nextjs/src/lib/component-props.ts
+++ samples/nextjs/src/lib/component-props.ts
@@ -7,6 +7,9 @@ import {
   RouteData,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 
+/**
+ * Styleguide sitecore context value shape
+ */
 export type StyleguideSitecoreContextValue = LayoutServiceContext & {
   itemId?: string;
   route: RouteData;
