3d04ff1a57ba933972556c75df055b77743e3c06
sc-illiakovalenko <illia.kovalenko@sitecore.com>
2021-01-04 13:18:01 +0200

    Add changes

--- packages/sitecore-jss/src/dictionary-service.ts
+++ packages/sitecore-jss/src/dictionary-service.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import mcache from 'memory-cache';
 import { AxiosDataFetcher } from './data-fetcher';
 import { DictionaryPhrases, DictionaryServiceData } from './dataModels';
 import { HttpJsonFetcher } from './httpClientInterface';
@@ -7,21 +8,66 @@ export type DictionaryServiceConfig = {
   apiHost: string;
   apiKey: string;
   jssAppName: string;
+  /**
+   * Custom data fetcher
+   * @see HttpJsonFetcher<T>
+   */
   dataFetcher?: HttpJsonFetcher<DictionaryServiceData>;
+  /**
+   * Enable/disable caching mechanism
+   * @default false
+   */
+  cacheEnabled?: boolean;
+  /**
+   * Cache timeout (ms)
+   * @default 10000
+   */
+  cacheTimeout?: number;
 };
 
 export class DictionaryService {
   constructor(private dictionaryServiceConfig: DictionaryServiceConfig) {}
 
+  /**
+   * Fetch dictionary data
+   * @param {string} language
+   */
   async fetchDictionaryData(language: string): Promise<DictionaryPhrases> {
-    const { apiHost, jssAppName, apiKey, dataFetcher } = this.dictionaryServiceConfig;
+    const {
+      dataFetcher,
+      cacheTimeout = 10000,
+      cacheEnabled = false,
+    } = this.dictionaryServiceConfig;
 
     const fetcher = dataFetcher || this.getDefaultFetcher();
 
-    const dictionaryServiceUrl = `${apiHost}/sitecore/api/jss/dictionary/${jssAppName}/${language}?sc_apikey=${apiKey}`;
+    const dictionaryServiceUrl = this.getUrl(language);
+
+    if (cacheEnabled) {
+      const cachedBody = mcache.get(dictionaryServiceUrl);
+
+      if (cachedBody) {
+        return cachedBody;
+      }
+    }
+
     const response = await fetcher(dictionaryServiceUrl);
 
+    if (cacheEnabled) {
+      mcache.put(dictionaryServiceUrl, response.data.phrases, cacheTimeout);
+    }
+
     return response.data.phrases;
+  }
+
+  /**
+   * Generate dictionary service url
+   * @param {string} language
+   */
+  private getUrl(language: string) {
+    const { apiHost, jssAppName, apiKey } = this.dictionaryServiceConfig;
+
+    return `${apiHost}/sitecore/api/jss/dictionary/${jssAppName}/${language}?sc_apikey=${apiKey}`;
   }
 
   /**
--- samples/nextjs/src/lib/dictionary-service.ts
+++ samples/nextjs/src/lib/dictionary-service.ts
@@ -1,20 +1,10 @@
-import { DictionaryServiceData, DictionaryPhrases } from '@sitecore-jss/sitecore-jss-nextjs';
+import { DictionaryService } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
-import { dataFetcher } from './data-fetcher';
 
-export class DictionaryService {
-  constructor(private apiHost: string, private apiKey: string, private jssAppName: string) {}
+const dictionaryService = new DictionaryService({
+  apiHost: config.sitecoreApiHost,
+  apiKey: config.sitecoreApiKey,
+  jssAppName: config.jssAppName,
+});
 
-  async fetchDictionaryData(language: string): Promise<DictionaryPhrases> {
-    const dictionaryServiceUrl = `${this.apiHost}/sitecore/api/jss/dictionary/${this.jssAppName}/${language}?sc_apikey=${this.apiKey}`;
-    const response = await dataFetcher<DictionaryServiceData>(dictionaryServiceUrl);
-    return response.data.phrases;
-  }
-}
-
-const dictionaryService = new DictionaryService(
-  config.sitecoreApiHost,
-  config.sitecoreApiKey,
-  config.jssAppName
-);
 export { dictionaryService };
