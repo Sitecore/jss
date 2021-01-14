7b8213d77aab28ee4f7559963ead550b77ce9dc7
Adam Brauer <adab@sitecore.net>
2021-01-14 16:29:49 -0600

    Doc/comment updates, removed security token

--- samples/nextjs/src/pages/api/editing/data/[key].ts
+++ samples/nextjs/src/pages/api/editing/data/[key].ts
@@ -1,6 +1,14 @@
 import { EditingDataMiddleware } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
 
-// Bump body size limit (1mb by default) for EE payload
+/**
+ * This Next.js API route is used to handle Sitecore Experience Editor data storage and retrieval by key
+ * (between the Sitecore Experience Editor POST and Next.js page render requests) via the `EditingDataService`.
+ *
+ * The `EditingDataMiddleware` expects this dynamic route name to be '[key]' by default, but can
+ * be configured to use something else with the `dynamicRouteKey` option.
+ */
+
+// Bump body size limit (1mb by default) for Experience Editor payload
 // See https://nextjs.org/docs/api-routes/api-middlewares#custom-config
 export const config = {
   api: {
@@ -11,6 +19,6 @@ export const config = {
 };
 
 // Wire up the EditingDataMiddleware handler
-const handler = new EditingDataMiddleware().getHandler();
+const handler = new EditingDataMiddleware({}).getHandler();
 
 export default handler;
