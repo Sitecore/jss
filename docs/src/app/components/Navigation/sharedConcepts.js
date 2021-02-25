3762f305dbc147850aab8535b42a3ff8aa079f0f
Anastasiya Flynn <flynn.github@gmail.com>
2021-02-25 03:38:35 -0500

    Docs article - Headless Concepts; update nav with new pages

--- docs/src/app/components/Navigation/sharedConcepts.js
+++ docs/src/app/components/Navigation/sharedConcepts.js
@@ -4,14 +4,14 @@ export default {
   url: 'introduction',
   displayName: 'Introduction',
   children: [
-    //{
-    //  url: 'why-jss',
-    //  displayName: 'Why JSS',
-    //},
-    //{
-    //  url: 'headless-concepts',
-    //  displayName: 'Headless Concepts',
-    //},
+    {
+      url: 'why-jss',
+      displayName: 'Why JSS',
+    },
+    {
+      url: 'headless-concepts',
+      displayName: 'Headless Concepts',
+    },
     {
       url: 'content-authoring-concepts',
       displayName: 'Content Authoring'
