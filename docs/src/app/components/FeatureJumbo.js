adf9d7fdc988583c8f748343f2b9fc4e46a73092
Anastasiya Flynn <flynn.github@gmail.com>
2021-02-25 00:40:48 -0500

    branding and marketing updates

--- docs/src/app/components/FeatureJumbo.js
+++ docs/src/app/components/FeatureJumbo.js
@@ -5,64 +5,23 @@ import RouteLinkedRichText from './RouteLinkedRichText';
 import { Link } from 'react-router-dom';
 
 const FeatureJumbo = ({ fields }) => (
-  <section className="jumbo-container bg-light">
-    <div className="jumbo-image-circle-container">
-      <div className="jumbo-image-circle">
-        <div className="jumbo-image-container image-background">
-          <Image field={fields.image} />
-        </div>
+  <div class="jumbotron jumbotron-fluid" style={{ maxHeight: '400px' }}>
+    <div class="bgPattern"></div>
+    <div class="container">
+      <div>
+        <Text field={fields.subtitle} tag="h2" className="subtitle mb-2" />
+        <Text field={fields.title} tag="h1" className="title mb-3" />
+        <RouteLinkedRichText field={fields.text} tag="p" className="lead" />
+        <Link to="/docs/nextjs/getting-started-nextjs/workflow-options" className="btn btn-primary btn-lg">
+          Get Started
+          </Link>
       </div>
     </div>
-    <div className="circleContainer">
-      <div className="circleContainer-outer-top">
-        <div className="circleContainer-inner">
-          <div className="circleContainer-inner-primary">
-            <div className="circ circ_right circle-fg-right-lg bg-success" />
-          </div>
-          <div className="circleContainer-inner-secondary">
-            <div className="circ circ_right circle-fg-right-lg-sub bg-primary" />
-            <div className="circ circ_right circle-fg-right-sm bg-primary" />
-          </div>
-        </div>
-      </div>
-      <div className="circleContainer-outer-bottom">
-        <div className="circleContainer-inner">
-          <div className="circleContainer-inner-primary">
-            <div className="circle circle-fg-left-md bg-success" />
-          </div>
-          <div className="circleContainer-inner-secondary">
-            <div className="circle circle-fg-left-md-sub bg-primary" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="shapes-wrapper">
-      <div className="outer-shape container">
-        <div className="inner-shape row">
-          <div className="content-wrapper col-12 col-md-7 min-height">
-            <div className="content-wrapper-inner">
-              <Text field={fields.subtitle} tag="h3" className="subtitle mb-2" />
-              <Text field={fields.title} tag="h1" className="title mb-3" />
-              <RouteLinkedRichText field={fields.text} tag="p" className="copy mb-5 p-0" />
-              <Link to="/docs/nextjs/getting-started-nextjs/workflow-options" className="btn btn-primary btn-lg">
-                Get Started
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  </div>
 );
 
 FeatureJumbo.propTypes = {
   fields: PropTypes.shape({
-    image: PropTypes.shape({
-      value: PropTypes.shape({
-        src: PropTypes.string,
-      }),
-      editable: PropTypes.string,
-    }),
     title: PropTypes.shape({
       value: PropTypes.string,
       editable: PropTypes.string,
