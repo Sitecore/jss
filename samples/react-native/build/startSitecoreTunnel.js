c775418f3272a8821e6feaf64e11faf2bf352ced
Adam Brauer <adab@sitecore.net>
2021-03-17 13:14:54 -0500

    Fixed tunnel for use of secure (https) Sitecore endpoints. Updated to latest ngrok.

--- samples/react-native/build/startSitecoreTunnel.js
+++ samples/react-native/build/startSitecoreTunnel.js
@@ -11,7 +11,7 @@ const getProtocol = (host) => {
   return null;
 };
 
-const startSitecoreTunnel = (sitecoreHost, port = 80) => {
+const startSitecoreTunnel = (sitecoreHost) => {
   const ngrok = require('ngrok'); // eslint-disable-line
 
   if (!sitecoreHost) {
@@ -20,20 +20,21 @@ const startSitecoreTunnel = (sitecoreHost, port = 80) => {
     );
   }
 
-  const protocol = getProtocol(sitecoreHost);
+  let protocol = getProtocol(sitecoreHost);
   if (!protocol) {
     console.warn(
       `No protocol found on host: ${sitecoreHost}. The tunnel will use 'http' by default.`
     );
+    protocol = 'http';
   }
 
   // be sure to strip the scheme/protocol from the host url, otherwise ngrok will make requests like 'http://http://jssbasicapp'.
   const hostWithoutProtocol = sitecoreHost.replace(`${protocol}://`, '');
-  const rewriteHost = `${hostWithoutProtocol}:${port}`;
+  const rewriteHost = `${hostWithoutProtocol}:${protocol === 'http' ? 80 : 443}`;
 
   return ngrok
     .connect({
-      proto: protocol,
+      proto: 'http',
       host_header: 'rewrite',
       addr: rewriteHost,
     })
