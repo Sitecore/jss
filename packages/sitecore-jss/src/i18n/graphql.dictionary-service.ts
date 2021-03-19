4ec71aa32a7e8154a6858f4727a30ed91a1e06c3
Anastasiya Flynn <flynn.github@gmail.com>
2021-03-19 15:48:30 -0400

    GraphQL dictionary service now fetches the root item ID, so it does not have to be provided explicitly

--- packages/sitecore-jss/src/i18n/graphql.dictionary-service.ts
+++ packages/sitecore-jss/src/i18n/graphql.dictionary-service.ts
@@ -44,6 +44,7 @@ export interface GraphQLDictionaryServiceConfig extends CacheOptions {
   endpoint: string;
   rootItemId?: string;
   pageSize?: number;
+  appName: string;
 }
 
 /**
@@ -121,20 +122,39 @@ export class GraphQLDictionaryService extends DictionaryServiceBase {
    * }
    */
   async fetchDictionaryData(language: string): Promise<DictionaryPhrases> {
-    const cacheKey = this.options.endpoint + language + this.options.rootItemId;
+    const cacheKey = this.options.appName + language;
     const cachedValue = this.getCachedValue(cacheKey);
     if (cachedValue) {
       return cachedValue;
     }
 
     const dataFetcher = new GraphQLRequestClient(this.options.endpoint);
+    if (!this.options.rootItemId) {
+      this.options.rootItemId = await getAppRoot(dataFetcher, this.options.appName, language);
+    }
+
+    const results = await this.getDictionaryPhrases(dataFetcher, language);
+    return this.cacheValue(cacheKey, results);
+  }
+
+  /**
+   * Gets dictionary phrases
+   * @param dataFetcher (GraphQLRequestClient)
+   * @param language (string)
+   * @returns dictionary phrases
+   */
+  async getDictionaryPhrases(
+    dataFetcher: GraphQLRequestClient,
+    language: string
+  ): Promise<DictionaryPhrases> {
     const results: DictionaryPhrases = {};
     let hasNext = true;
     let after = '';
 
     while (hasNext) {
       const fetchResponse = await dataFetcher.request<DictionaryQueryResult>(query, {
-        rootItemId: this.options.rootItemId,
+        // `search` query only works with lowercase GUIDs
+        rootItemId: this.options.rootItemId?.toLowerCase(),
         language,
         dictionaryEntryTemplateId: SitecoreTemplateId.DictionaryEntry,
         pageSize: this.options.pageSize,
@@ -149,6 +169,58 @@ export class GraphQLDictionaryService extends DictionaryServiceBase {
       after = fetchResponse.search.pageInfo.endCursor;
     }
 
-    return this.cacheValue(cacheKey, results);
+    return results;
   }
+}
+
+// TODO: Move to shared area and reuse for sitemap service (Anastasiya, March 2021)
+const appRootQuery = `
+query GetAppRoot($jssAppTemplateId: String, $appName: String!, $language: String)
+{
+  layout(site: $appName, routePath: "/", language: $language) {
+    homePage: item {
+      appRoot: ancestors(includeTemplateIDs: [$jssAppTemplateId]) {
+        id
+      }
+    }
+  }
+}
+`;
+
+/**
+ * A reply from the GraphQL Sitecore Dictionary Service
+ */
+type AppRootQueryResult = {
+  layout: {
+    homePage: {
+      appRoot: {
+        id: string;
+      }[];
+    };
+  };
+};
+
+/**
+ * Gets ID of the JSS app root for the provided GraphQL endpoint.
+ * @param dataFetcher {GraphQLRequestClient} the GraphQL data fetcher.
+ * @param appName {string} the name of the JSS app.
+ * @param language {string} the item language version.
+ * @returns the ID of the JSS app root item
+ */
+async function getAppRoot(
+  dataFetcher: GraphQLRequestClient,
+  appName: string,
+  language: string
+): Promise<string> {
+  const fetchResponse = await dataFetcher.request<AppRootQueryResult>(appRootQuery, {
+    jssAppTemplateId: SitecoreTemplateId.JssApp,
+    appName,
+    language,
+  });
+
+  if (!fetchResponse?.layout?.homePage?.appRoot?.length) {
+    throw new Error('Error fetching JSS app root item');
+  }
+
+  return fetchResponse.layout.homePage.appRoot[0].id;
 }
