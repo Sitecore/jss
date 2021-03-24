1b41f82a951e003b733058782a78a5fb20fc4f9a
Adam Brauer <adab@sitecore.net>
2021-03-24 12:51:02 -0500

    Added 'sc_apikey' header authentication for GraphQL layout and dictionary services

--- packages/sitecore-jss/src/graphql-request-client.test.ts
+++ packages/sitecore-jss/src/graphql-request-client.test.ts
@@ -3,11 +3,9 @@ import nock from 'nock';
 import { GraphQLRequestClient } from './graphql-request-client';
 
 describe('GraphQLRequestClient', () => {
-  const ENDPOINT = 'http://jssnextweb/graphql';
+  afterEach(nock.cleanAll);
 
-  const graphQLClient = new GraphQLRequestClient(ENDPOINT);
-
-  const mockGraphQLRequest = () => {
+  it('should execute graphql request', async () => {
     nock('http://jssnextweb')
       .post('/graphql')
       .reply(200, {
@@ -15,15 +13,28 @@ describe('GraphQLRequestClient', () => {
           result: 'Hello world...',
         },
       });
-  };
 
-  afterEach(nock.cleanAll);
-
-  it('should execute graphql request', async () => {
-    mockGraphQLRequest();
-
+    const graphQLClient = new GraphQLRequestClient('http://jssnextweb/graphql');
     const data = await graphQLClient.request('test');
 
     expect(data).to.deep.equal({ result: 'Hello world...' });
+  });
+
+  it('should send sc_apikey header', async () => {
+    const apiKey = 'cjhNRWNVOHRFTklwUjhYa0RSTnBhSStIam1mNE1KN1pyeW13c3FnRVExTT18bXRzdC1kLTAxOQ==';
+    nock('http://jssnextweb', {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
+      .post('/graphql')
+      .reply(200, {
+        data: {
+          result: 'Hello world...',
+        },
+      });
+
+    const graphQLClient = new GraphQLRequestClient('http://jssnextweb/graphql', apiKey);
+    await graphQLClient.request('test');
   });
 });
