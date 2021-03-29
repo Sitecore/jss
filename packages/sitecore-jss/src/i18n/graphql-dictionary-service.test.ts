037873e8bdfe11d7ea17121493e5ad66f484dfc8
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2021-03-29 15:05:10 -0500

    [NextJs] Sitecore Experience Edge Authentication (#636)
    * Added 'sc_apikey' header authentication for GraphQL layout and dictionary services
    
    * Added sc_apikey header / param to GraphQL sitemap service. Started sample app updates.
    
    * Fixed siteRootQuery error: Variable "$language" of type "String" used in position expecting type "String!"
    
    * Generate environment variables for computed config values as well
    
    * Added GRAPH_QL_ENDPOINT to .env file and Vercel deployment doc
    
    * PR feedback: use endpoint const, get rid of "calculated config" verbiage
    
    * Removed prettier comment
    
    * PR feedback: removed "Edge" usage for XM references
    
    Co-authored-by: Adam Brauer <adab@sitecore.net>

--- packages/sitecore-jss/src/i18n/graphql-dictionary-service.test.ts
+++ packages/sitecore-jss/src/i18n/graphql-dictionary-service.test.ts
@@ -8,42 +8,73 @@ import dictionaryQueryResponse from './mockDictionaryQueryResponse.json';
 describe('GraphQLDictionaryService', () => {
   const endpoint = 'http://site';
   const siteName = 'site-name';
+  const apiKey = '0FBFF61E-267A-43E3-9252-B77E71CEE4BA';
 
   afterEach(() => {
     nock.cleanAll();
   });
 
   it('should fetch app root', async () => {
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /GetSiteRoot/gi)
       .reply(200, siteRootQueryResponse);
 
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /DictionarySearch/gi)
       .reply(200, dictionaryQueryResponse);
 
-    const service = new GraphQLDictionaryService({ endpoint, siteName, cacheEnabled: false });
+    const service = new GraphQLDictionaryService({
+      endpoint,
+      apiKey,
+      siteName,
+      cacheEnabled: false,
+    });
     await service.fetchDictionaryData('en');
     expect(service.options.rootItemId).to.equal('GUIDGUIDGUID');
   });
 
   it('should fetch dictionary phrases', async () => {
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /GetSiteRoot/gi)
       .reply(200, siteRootQueryResponse);
 
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /DictionarySearch/gi)
       .reply(200, dictionaryQueryResponse);
 
-    const service = new GraphQLDictionaryService({ endpoint, siteName, cacheEnabled: false });
+    const service = new GraphQLDictionaryService({
+      endpoint,
+      apiKey,
+      siteName,
+      cacheEnabled: false,
+    });
     const result = await service.fetchDictionaryData('en');
     expect(result.foo).to.equal('foo');
     expect(result.bar).to.equal('bar');
   });
 
   it('should throw error if no app root found', async () => {
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /GetSiteRoot/gi)
       .reply(200, {
         data: {
@@ -55,11 +86,20 @@ describe('GraphQLDictionaryService', () => {
         },
       });
 
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /DictionarySearch/gi)
       .reply(200, dictionaryQueryResponse);
 
-    const service = new GraphQLDictionaryService({ endpoint, siteName, cacheEnabled: false });
+    const service = new GraphQLDictionaryService({
+      endpoint,
+      apiKey,
+      siteName,
+      cacheEnabled: false,
+    });
     await service.fetchDictionaryData('en').catch((error) => {
       expect(error.message).to.equal('Error fetching Sitecore site root item');
     });
@@ -67,16 +107,24 @@ describe('GraphQLDictionaryService', () => {
 
   // TODO: there is a known issue with mcache
   it('should use cache', async () => {
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /GetSiteRoot/gi)
       .reply(200, siteRootQueryResponse);
 
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /DictionarySearch/gi)
       .times(2)
       .reply(200, dictionaryQueryResponse);
 
-    const service = new GraphQLDictionaryService({ endpoint, siteName });
+    const service = new GraphQLDictionaryService({ endpoint, apiKey, siteName });
 
     // call fetch twice, and use nock to see if more than 1 request was actually made
     await service.fetchDictionaryData('en');
@@ -87,12 +135,17 @@ describe('GraphQLDictionaryService', () => {
 
   it('should use a custom rootItemId, if provided', async () => {
     const customRootId = 'cats';
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', (body) => body.variables.rootItemId === customRootId)
       .reply(200, dictionaryQueryResponse);
 
     const service = new GraphQLDictionaryService({
       endpoint,
+      apiKey,
       siteName,
       cacheEnabled: false,
       rootItemId: customRootId,
@@ -103,16 +156,25 @@ describe('GraphQLDictionaryService', () => {
 
   it('should use a custom pageSize, if provided', async () => {
     const customPageSize = 2;
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', /GetSiteRoot/gi)
       .reply(200, siteRootQueryResponse);
 
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', (body) => body.variables.pageSize === customPageSize)
       .reply(200, dictionaryQueryResponse);
 
     const service = new GraphQLDictionaryService({
       endpoint,
+      apiKey,
       siteName,
       cacheEnabled: false,
       pageSize: customPageSize,
@@ -123,12 +185,17 @@ describe('GraphQLDictionaryService', () => {
 
   it('should use lowercase GUIDs in GraphQL search', async () => {
     const customRootId = '{FOO-BAR-123}';
-    nock(endpoint)
+    nock(endpoint, {
+      reqheaders: {
+        sc_apikey: apiKey,
+      },
+    })
       .post('/', (body) => body.variables.rootItemId === '{foo-bar-123}')
       .reply(200, dictionaryQueryResponse);
 
     const service = new GraphQLDictionaryService({
       endpoint,
+      apiKey,
       siteName,
       cacheEnabled: false,
       rootItemId: customRootId,
