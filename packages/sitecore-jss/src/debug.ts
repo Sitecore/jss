5566fa04b81c5b37bda7c4b9844fb93ad2dec014
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2021-04-09 14:00:53 -0500

    Added DEBUG_MULTILINE to allow switching from %o (pretty-print single line) to %O (pretty-print multiple line) formatting

--- packages/sitecore-jss/src/debug.ts
+++ packages/sitecore-jss/src/debug.ts
@@ -1,8 +1,21 @@
 import debug from 'debug';
+import { isServer } from './util';
 
 const rootNamespace = 'sitecore-jss';
 
 export type Debugger = debug.Debugger;
+
+// On server/node side, allow switching from the built-in
+// `%o` (pretty-print single line) and `%O` (pretty-print multiple line)
+// with a `DEBUG_MULTILINE` environment variable.
+if (
+  isServer() &&
+  process?.env?.DEBUG_MULTILINE === 'true' &&
+  debug.formatters.o &&
+  debug.formatters.O
+) {
+  debug.formatters.o = debug.formatters.O;
+}
 
 /**
  * Default Sitecore JSS 'debug' module debuggers. Uses namespace prefix 'sitecore-jss:'.
