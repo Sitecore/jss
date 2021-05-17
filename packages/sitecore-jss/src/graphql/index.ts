05ae81638e0737bcc4508873d3739656ef1530f1
Anastasiya Flynn <flynn.github@gmail.com>
2021-05-17 12:39:31 -0400

    Feature/467450 - Edge GraphQL query & services updates (#692)
    Functional changes:
    * Update introspection, and connected/disconnected GraphQL components in Next sample to account for latest Edge scheme. The change is that `language` is required in all queries. This fixes the broken build.
    
    Non-functional devEx improvements:
    * Add CacheClient and GraphQLClient interfaces to allow implementations to override which 3rd party libs are used by services.
    * Pull shared functionality related to using `search` query (ie handling pagination) into new SearchQueryService; update GraphQLDictionaryService and SitemapService to use this service.
    * Make it easier to customize our services by changing access level of methods from private to protected
    
    Misc changes:
    * [sitecore-jss] Move app root query functionality from /utils to /graphql
    * Fix typo in docs

---
+++ packages/sitecore-jss/src/graphql/index.ts
@@ -0,0 +1,7 @@
+export { getAppRootId, AppRootQueryResult } from './app-root-query';
+export {
+  SearchQueryResult,
+  SearchQueryVariables,
+  SearchServiceConfig,
+  SearchQueryService,
+} from './search-service';
