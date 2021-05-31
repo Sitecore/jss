a7d2caa5d7080e447b3e993dbcfdba704074880f
Serhii Zubatkin <serhii.zubatkin@sitecore.com>
2021-05-31 11:00:50 +0300

    Add localization to Loading component

--- samples/nextjs/src/components/PersonalizationLoadingComponent.tsx
+++ samples/nextjs/src/components/PersonalizationLoadingComponent.tsx
@@ -1,15 +1,17 @@
 import { usePersonalization, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
 import { layoutPersonalizationService } from 'lib/layout-personalization-service-factory';
+import { useI18n } from 'next-localization';
 
 const PersonalizationLoadingComponent = (props: {
   rendering: ComponentRendering;
 }): JSX.Element | null => {
+  const { t } = useI18n();
   const { personalizedComponent, isLoading } = usePersonalization({
     uid: props.rendering.uid as string,
     layoutPersonalizationService,
   });
 
-  return isLoading ? <div>Loading</div> : personalizedComponent;
+  return isLoading ? <div>{t('Loading')}</div> : personalizedComponent;
 };
 
 export default PersonalizationLoadingComponent;
