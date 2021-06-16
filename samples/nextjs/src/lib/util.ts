8a6f5b60cd92a925e278a393d6bba65f4459eb8f
illiakovalenko <zlatoborodyi@gmail.com>
2021-06-16 23:48:23 +0300

    Extend description

--- samples/nextjs/src/lib/util.ts
+++ samples/nextjs/src/lib/util.ts
@@ -1,7 +1,8 @@
 /**
  * Get the publicUrl.
  * This is used primarily to enable compatibility with the Sitecore Experience Editor.
- * VERCEL_URL is provided in case if we are in Preview deployment
+ * VERCEL_URL is provided by Vercel in case if we are in Preview deployment (deployment based on the custom branch),
+ * preview deployment has unique url, we don't know exact url.
  */
 export const getPublicUrl = (): string => {
   return process.env.VERCEL_URL
