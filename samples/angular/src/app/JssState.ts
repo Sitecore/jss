045c3acc2eeb20f0c789abfc627e96bce46c820e
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2021-07-14 16:54:18 -0500

    fix 'jss-layout.service' import in angular sample

--- samples/angular/src/app/JssState.ts
+++ samples/angular/src/app/JssState.ts
@@ -2,7 +2,7 @@ import {
   RouteData,
   LayoutServiceContextData,
 } from '@sitecore-jss/sitecore-jss-angular';
-import { LayoutServiceError } from './layout/jss-layout-loader.service';
+import { LayoutServiceError } from './layout/jss-layout.service';
 
 export class JssState {
   language: string;
