ee052bada077f4c3c1df509f4da1207e5c0c7a78
Coby Sher <63015754+CobyPear@users.noreply.github.com>
2021-08-04 17:19:44 -0500

    Refactor react services to use factory pattern

---
+++ samples/react/src/lib/layout-service-factory.graphql.js
@@ -0,0 +1,14 @@
+import { GraphQLLayoutService } from '@sitecore-jss/sitecore-jss-react';
+import config from 'temp/config';
+
+export class LayoutServiceFactory {
+  create() {
+    return new GraphQLLayoutService({
+      endpoint: config.graphQLEndpoint,
+      apiKey: config.sitecoreApiKey,
+      siteName: config.jssAppName,
+    });
+  }
+}
+
+export const layoutServiceFactory = new LayoutServiceFactory();
---
+++ samples/react/src/lib/layout-service-factory.js
@@ -0,0 +1,14 @@
+import { RestLayoutService } from '@sitecore-jss/sitecore-jss-react';
+import config from '../temp/config';
+
+export class LayoutServiceFactory {
+  create() {
+    return new RestLayoutService({
+      apiHost: config.sitecoreApiHost,
+      apiKey: config.sitecoreApiKey,
+      siteName: config.jssAppName,
+    });
+  }
+}
+
+export const layoutServiceFactory = new LayoutServiceFactory();
