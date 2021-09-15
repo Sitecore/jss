f3bade3fdf07cf91280d77e9c28197c89350f121
Addy Pathania <aditya.pathania@sitecore.com>
2021-09-15 13:41:20 -0400

    add prefix name to vue components and templates

--- samples/vue/sitecore/definitions/components/Styleguide-Multilingual.sitecore.js
+++ samples/vue/sitecore/definitions/components/Styleguide-Multilingual.sitecore.js
@@ -9,6 +9,7 @@ import { CommonFieldTypes, SitecoreIcon, Manifest } from '@sitecore-jss/sitecore
 export default function(manifest) {
   manifest.addComponent({
     name: 'Styleguide-Multilingual',
+    templateName: 'JssVueWeb-Styleguide-Multilingual',
     icon: SitecoreIcon.FlagGeneric,
     fields: [
       {
@@ -19,6 +20,6 @@ export default function(manifest) {
     ],
     // inherit fields from another template (../templates/Styleguide-Explanatory-Component)
     // inheritance adds fields defined on the base template(s) implicitly to this component
-    inherits: ['styleguide-explanatory-component-template'],
+    inherits: ['JssVueWeb-styleguide-explanatory-component-template'],
   });
 }
