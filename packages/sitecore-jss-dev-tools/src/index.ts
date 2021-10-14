e899f9862c08ff41518a0ea9cd9112e416b42d6d
Addy Pathania <aditya.pathania@sitecore.com>
2021-10-14 17:31:38 -0400

    fixed nextjs sample build issue

--- packages/sitecore-jss-dev-tools/src/index.ts
+++ packages/sitecore-jss-dev-tools/src/index.ts
@@ -33,6 +33,39 @@ export {
 export { ScJssConfig, JssConfiguration, resolveScJssConfig } from './resolve-scjssconfig';
 export { strip } from './templating/strip';
 
-export * from './manifest';
+export {
+  generateToFile,
+  generateToVariable,
+  SitecoreIcon,
+  Manifest,
+  ManifestInstance,
+  CreateManifestInstanceArgs,
+  CommonFieldTypes,
+  FieldStorage,
+  FieldDefinition,
+  RenderingParameterDefinition,
+  PlaceholderDefinition,
+  ComponentDefinition,
+  DictionaryDefinition,
+  ImageFieldValue,
+  LinkFieldValue,
+  ContentFieldValue,
+  ItemDefinition,
+  ItemReference,
+  isItemDefinition,
+  RouteDefinition,
+  ComponentInstanceDefinition,
+  GeneratePipelineArgs,
+  GenerateContentItemArgs,
+  GeneratePlaceholdersPipelineArgs,
+  GenerateRouteItemPipelineArgs,
+  addComponent,
+  addTemplate,
+  addPlaceholder,
+  addRouteType,
+  addRoute,
+  addContent,
+  addDictionary,
+} from './manifest';
 export * from './pipelines';
 export * from './update';
