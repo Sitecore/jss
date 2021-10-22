66403c864d9cf6f2b704ac266618044374f5d6c3
illiakovalenko <zlatoborodyi@gmail.com>
2021-10-22 14:48:36 +0300

    Fix linting issue

--- packages/sitecore-jss-angular/src/components/hidden-rendering.component.spec.ts
+++ packages/sitecore-jss-angular/src/components/hidden-rendering.component.spec.ts
@@ -1,7 +1,4 @@
-import {
-  Component,
-  DebugElement,
-} from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { JssModule } from '../lib.module';
@@ -12,26 +9,23 @@ import { JssModule } from '../lib.module';
     <sc-hidden-rendering></sc-hidden-rendering>
   `,
 })
-class TestHiddenRenderingComponent {
-}
+class TestHiddenRenderingComponent {}
 
 describe('<sc-hidden-rendering />', () => {
   let fixture: ComponentFixture<TestHiddenRenderingComponent>;
   let de: DebugElement;
   let comp: TestHiddenRenderingComponent;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestHiddenRenderingComponent,
-      ],
-      imports: [
-        JssModule.withComponents([
-          { name: 'TestComponent', type: TestHiddenRenderingComponent },
-        ]),
-      ],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TestHiddenRenderingComponent],
+        imports: [
+          JssModule.withComponents([{ name: 'TestComponent', type: TestHiddenRenderingComponent }]),
+        ],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TestHiddenRenderingComponent);
@@ -48,6 +42,8 @@ describe('<sc-hidden-rendering />', () => {
   it('should show section', () => {
     const el = de.nativeElement.getElementsByTagName('sc-hidden-rendering')[0];
     expect(el).toBeDefined();
-    expect(el.getAttribute('style')).toEqual('display: block; height: 100px; background-image: linear-gradient(45deg, #ffffff 25%, #dcdcdc 25%, #dcdcdc 50%, #ffffff 50%, #ffffff 75%, #dcdcdc 75%, #dcdcdc 100%); backgroundSize: 3px 3px;');
+    expect(el.getAttribute('style')).toEqual(
+      'display: block; height: 100px; background-image: linear-gradient(45deg, #ffffff 25%, #dcdcdc 25%, #dcdcdc 50%, #ffffff 50%, #ffffff 75%, #dcdcdc 75%, #dcdcdc 100%); backgroundSize: 3px 3px;'
+    );
   });
 });
