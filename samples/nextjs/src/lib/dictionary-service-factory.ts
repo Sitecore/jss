aaba53d0d6c0e1b2f47f973ff9dded77d1eeb8a2
CobyPear <coby.sher@sitecore.com>
2021-11-10 09:54:42 -0600

    Changes from comments

--- samples/nextjs/src/lib/dictionary-service-factory.ts
+++ samples/nextjs/src/lib/dictionary-service-factory.ts
@@ -5,40 +5,26 @@ import {
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
 
-interface DictionaryServiceFactory {
-  create(): DictionaryService;
-}
-let dictionaryServiceFactory: DictionaryServiceFactory;
-
-if (process.env.FETCH_WITH === 'rest') {
-  class RestDictionaryServiceFactory {
-    create(): DictionaryService {
-      return new RestDictionaryService({
-        apiHost: config.sitecoreApiHost,
-        apiKey: config.sitecoreApiKey,
-        siteName: config.jssAppName,
-      });
-    }
+export class DictionaryServiceFactory {
+  create(): DictionaryService {
+    return process.env.FETCH_WITH === 'graphql'
+      ? new GraphQLDictionaryService({
+          endpoint: config.graphQLEndpoint,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+          /*
+      The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+      app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+      otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+      rootItemId: '{GUID}'
+      */
+        })
+      : new RestDictionaryService({
+          apiHost: config.sitecoreApiHost,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+        });
   }
-
-  dictionaryServiceFactory = new RestDictionaryServiceFactory();
-} else if (process.env.FETCH_WITH === 'graphql') {
-  class GraphQLDictionaryServiceFactory {
-    create(): DictionaryService {
-      return new GraphQLDictionaryService({
-        endpoint: config.graphQLEndpoint,
-        apiKey: config.sitecoreApiKey,
-        siteName: config.jssAppName,
-        /*
-        The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
-        app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
-        otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
-        rootItemId: '{GUID}'
-        */
-      });
-    }
-  }
-  dictionaryServiceFactory = new GraphQLDictionaryServiceFactory();
 }
 
-export { dictionaryServiceFactory };
+export const dictionaryServiceFactory = new DictionaryServiceFactory();
--- samples/nextjs/src/lib/layout-service-factory.ts
+++ samples/nextjs/src/lib/layout-service-factory.ts
@@ -5,40 +5,21 @@ import {
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
 
-interface LayoutServiceFactory {
-  create(): LayoutService;
-}
-let layoutServiceFactory: LayoutServiceFactory;
-
-if (process.env.FETCH_WITH === 'rest') {
-  class RestLayoutServiceFactory {
-    create(): LayoutService {
-      return new RestLayoutService({
-        apiHost: config.sitecoreApiHost,
-        apiKey: config.sitecoreApiKey,
-        siteName: config.jssAppName,
-      });
-    }
+export class LayoutServiceFactory {
+  create(): LayoutService {
+    return process.env.FETCH_WITH === 'graphql'
+      ? new GraphQLLayoutService({
+          endpoint: config.graphQLEndpoint,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+        })
+      : new RestLayoutService({
+          apiHost: config.sitecoreApiHost,
+          apiKey: config.sitecoreApiKey,
+          siteName: config.jssAppName,
+          configurationName: 'default',
+        });
   }
-
-  layoutServiceFactory = new RestLayoutServiceFactory();
-} else if (process.env.FETCH_WITH === 'graphql') {
-  class GraphQLLayoutServiceFactory {
-    create(): LayoutService {
-      return new GraphQLLayoutService({
-        endpoint: config.graphQLEndpoint,
-        apiKey: config.sitecoreApiKey,
-        siteName: config.jssAppName,
-        /*
-        The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
-        app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
-        otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
-        rootItemId: '{GUID}'
-        */
-      });
-    }
-  }
-  layoutServiceFactory = new GraphQLLayoutServiceFactory();
 }
 
-export { layoutServiceFactory };
+export const layoutServiceFactory = new LayoutServiceFactory();
