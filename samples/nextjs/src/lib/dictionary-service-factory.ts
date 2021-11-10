283f7754446df4a26f4fa832a0440e76ed08ec93
CobyPear <coby.sher@sitecore.com>
2021-11-10 11:21:49 -0600

    Fix comment indentation

--- samples/nextjs/src/lib/dictionary-service-factory.ts
+++ samples/nextjs/src/lib/dictionary-service-factory.ts
@@ -13,11 +13,11 @@ export class DictionaryServiceFactory {
           apiKey: config.sitecoreApiKey,
           siteName: config.jssAppName,
           /*
-      The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
-      app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
-      otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
-      rootItemId: '{GUID}'
-      */
+            The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+            app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+            otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+            rootItemId: '{GUID}'
+          */
         })
       : new RestDictionaryService({
           apiHost: config.sitecoreApiHost,
