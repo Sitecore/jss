9db0d7e4a0ce1b62466eb491f266dad158be09dc
Addy Pathania <aditya.pathania@sitecore.com>
2021-11-17 09:24:28 -0500

    added build templates

--- packages/create-sitecore-jss/scripts/build-templates.ts
+++ packages/create-sitecore-jss/scripts/build-templates.ts
@@ -1,8 +1,15 @@
-// import path from 'path';
-// import fs from 'fs';
+import path from 'path';
+import fs from 'fs-extra';
 
-// TODO: copy src/templates to dist/templates
+const templatesFolder = path.resolve(__dirname, './../src/templates');
+const distFolder = path.resolve(__dirname, './../dist');
 
-// const buildTemplates = () => {
-//   const distFolder = path.resolve(__dirname, 'dist');
-// }
+// Copy templates to dist
+fs.copy(templatesFolder, distFolder, function(err: Error) {
+  if (err) {
+    console.log('An error occurred while copying the folder.');
+    return console.error(err);
+  }
+  console.log(distFolder);
+  console.log('Copy completed!');
+});
