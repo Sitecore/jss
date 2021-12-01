db02682409443db105818c3a4b23b3cf37e6cb5b
illiakovalenko <zlatoborodyi@gmail.com>
2021-12-01 13:38:45 +0200

    Cover cmd by UT

--- packages/create-sitecore-jss/src/common/utils/cmd.ts
+++ packages/create-sitecore-jss/src/common/utils/cmd.ts
@@ -27,24 +27,19 @@ export const spawnFunc = (
 ) => {
   const result = spawn.sync(command, args, Object.assign({ stdio: 'inherit' }, options));
 
-  if (result.signal) {
-    if (result.signal === 'SIGKILL') {
-      console.log(
-        'The operation failed because the process exited too early. ' +
-          'This probably means the system ran out of memory or someone called ' +
-          '`kill -9` on the process.'
-      );
-    } else if (result.signal === 'SIGTERM') {
-      console.log(
-        'The operation failed because the process exited too early. ' +
-          'Someone might have called `kill` or `killall`, or the system could ' +
-          'be shutting down.'
-      );
-    }
-    process.exit(1);
+  if (result.signal === 'SIGKILL') {
+    console.log(
+      'The operation failed because the process exited too early. ' +
+        'This probably means the system ran out of memory or someone called ' +
+        '`kill -9` on the process.'
+    );
+  } else if (result.signal === 'SIGTERM') {
+    console.log(
+      'The operation failed because the process exited too early. ' +
+        'Someone might have called `kill` or `killall`, or the system could ' +
+        'be shutting down.'
+    );
   }
 
-  if (result.status && result.status > 0) {
-    process.exit(result.status);
-  }
+  process.exit(result.status && result.status > 0 ? result.status : 1);
 };
