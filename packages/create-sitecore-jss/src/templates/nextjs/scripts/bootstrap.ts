06131fff50be1892b11e7d13a4fd49866856b450
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2021-12-03 11:16:37 -0600

    apply next.config plugin changes

--- packages/create-sitecore-jss/src/templates/nextjs/scripts/bootstrap.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/scripts/bootstrap.ts
@@ -1,4 +1,7 @@
 import { generateConfig } from './generate-config';
+// #START_EMPTY
+import { constants } from '@sitecore-jss/sitecore-jss-nextjs';
+// #END_EMPTY
 /*
   BOOTSTRAPPING
   The bootstrap process runs before build, and generates JS that needs to be
@@ -6,12 +9,23 @@ import { generateConfig } from './generate-config';
   and the global config module.
 */
 
+// #START_EMPTY
+const disconnected = process.env.JSS_MODE === constants.JSS_MODE.DISCONNECTED;
+// #END_EMPTY
 /*
   CONFIG GENERATION
   Generates the /src/temp/config.js file which contains runtime configuration
   that the app can import and use.
 */
+// #START_EMPTY
+const port = process.env.PORT || 3000;
+// #END_EMPTY
 const configOverride: { [key: string]: string } = {};
+// #START_EMPTY
+if (disconnected) {
+  configOverride.sitecoreApiHost = `http://localhost:${port}`;
+}
+// #END_EMPTY
 
 generateConfig(configOverride);
 
@@ -19,7 +33,8 @@ generateConfig(configOverride);
   COMPONENT FACTORY GENERATION
 */
 import './generate-component-factory';
+
 /*
    PLUGINS GENERATION
 */
-import './generate-plugin';
+import './generate-plugins';
