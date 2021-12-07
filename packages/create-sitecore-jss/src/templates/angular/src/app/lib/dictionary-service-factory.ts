a1818d01f72d67ab4ec5ad737fbaa08e442735e8
CobyPear <coby.sher@sitecore.com>
2021-12-07 14:56:41 -0600

    Refactor service factories with ejs

--- packages/create-sitecore-jss/src/templates/angular/src/app/lib/dictionary-service-factory.ts
+++ packages/create-sitecore-jss/src/templates/angular/src/app/lib/dictionary-service-factory.ts
@@ -1,15 +1,35 @@
-import { DictionaryService, RestDictionaryService } from '@sitecore-jss/sitecore-jss-angular';
+import { 
+  DictionaryService, 
+<% if (fetchWith.toUpperCase() === 'GRAPHQL') { -%>
+  GraphQLDictionaryService
+<% } else if (fetchWith.toUpperCase() === 'REST') { -%>
+  RestDictionaryService
+<% } -%>  
+} from '@sitecore-jss/sitecore-jss-angular';
 import { environment as env } from '../../environments/environment';
 
 export class DictionaryServiceFactory {
   create(): DictionaryService {
-    return new RestDictionaryService({
-      apiHost: env.sitecoreApiHost,
-      apiKey: env.sitecoreApiKey,
-      siteName: env.jssAppName,
-    });
+  <% if (fetchWith.toUpperCase() === 'GRAPHQL') { -%>
+  return new GraphQLDictionaryService({
+    endpoint: env.graphQLEndpoint,
+    apiKey: env.sitecoreApiKey,
+    siteName: env.jssAppName,
+    /*
+    The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+    app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+    otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+    rootItemId: '{GUID}'
+    */
+  });
+  <% } else if (fetchWith.toUpperCase() === 'REST') { -%>
+  return new RestDictionaryService({
+    apiHost: env.sitecoreApiHost,
+    apiKey: env.sitecoreApiKey,
+    siteName: env.jssAppName,
+  });
+  <% } -%>
   }
 }
 
 export const dictionaryServiceFactory = new DictionaryServiceFactory();
-// TODO: make this ejs if/else for rest/graphql
--- packages/create-sitecore-jss/src/templates/angular/src/app/lib/layout-service-factory.ts
+++ packages/create-sitecore-jss/src/templates/angular/src/app/lib/layout-service-factory.ts
@@ -1,16 +1,30 @@
-import { LayoutService, RestLayoutService } from '@sitecore-jss/sitecore-jss-angular';
+import { 
+  LayoutService,
+<% if (fetchWith.toUpperCase() === 'GRAPHQL') { -%>
+  GraphQLLayoutService
+<% } else if (fetchWith.toUpperCase() === 'REST') { -%>
+  RestLayoutService
+<% } -%> 
+} from '@sitecore-jss/sitecore-jss-angular';
 import { environment } from '../../environments/environment';
 
 export class LayoutServiceFactory {
   create(): LayoutService {
+    <% if (fetchWith.toUpperCase() === 'GRAPHQL') { -%>
+    return new GraphQLLayoutService({
+      endpoint: environment.graphQLEndpoint,
+      apiKey: environment.sitecoreApiKey,
+      siteName: environment.jssAppName,
+    });
+    <% } else if (fetchWith.toUpperCase() === 'REST') { -%>
     return new RestLayoutService({
       apiHost: environment.sitecoreApiHost,
       apiKey: environment.sitecoreApiKey,
       siteName: environment.jssAppName,
       configurationName: 'default',
     });
+    <% } -%>
   }
 }
 
 export const layoutServiceFactory = new LayoutServiceFactory();
-// TODO: make this ejs if/else for rest/graphql
