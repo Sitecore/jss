9043065d138625b156052484f879a3a907d685a9
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2021-12-09 15:07:15 -0600

    changes after merge (implement new Initializer requirements)

--- packages/create-sitecore-jss/src/initializers/node-headless-ssr-proxy/index.ts
+++ packages/create-sitecore-jss/src/initializers/node-headless-ssr-proxy/index.ts
@@ -3,7 +3,11 @@ import { Initializer } from '../../common/Initializer';
 import { transform } from '../../common/steps/index';
 import { BaseArgs } from '../../common/args/base';
 
-export class NodeHeadlessSsrProxyInitializer implements Initializer {
+export default class NodeHeadlessSsrProxyInitializer implements Initializer {
+  get isBase(): boolean {
+    return true;
+  }
+
   async init(args: BaseArgs) {
     const templatePath = path.resolve(__dirname, '../../templates/node-headless-ssr-proxy');
     await transform(templatePath, args);
