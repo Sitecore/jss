7c3a27435a0d37d05c7d1f71f74af6054d55df6f
Addy Pathania <aditya.pathania@sitecore.com>
2021-12-10 06:44:53 -0500

    added prompts and args

---
+++ packages/create-sitecore-jss/src/initializers/react/args.ts
@@ -0,0 +1,4 @@
+﻿import { StyleguideArgs } from '../../common/args/styleguide';
+import { ReactAnswer } from './prompts';
+
+export interface ReactArgs extends StyleguideArgs, Partial<ReactAnswer> {}
--- packages/create-sitecore-jss/src/initializers/react/index.ts
+++ packages/create-sitecore-jss/src/initializers/react/index.ts
@@ -4,29 +4,41 @@ import { prompt } from 'inquirer';
 import { Initializer } from '../../common/Initializer';
 import { transform } from '../../common/steps';
 import { FetchWith } from '../../common/prompts/base';
-import { ClientAppArgs } from '../../common/args/base';
-import { ClientAppAnswer, clientAppPrompts } from '../../common/prompts/base';
+import { prompts, ReactAnswer } from './prompts';
+import { ReactArgs } from './args';
 
 export class ReactInitializer implements Initializer {
-  async init(args: ClientAppArgs) {
+  get isBase() {
+    return true;
+  }
+
+  async init(args: ReactArgs) {
     const defaults = args.yes
       ? {
           appName: 'sitecore-jss-react',
           fetchWith: FetchWith.GraphQL,
           hostName: 'https://cm.jss.localhost',
           appPrefix: true,
+          language: '',
         }
       : {};
 
-    const answers = await prompt<ClientAppAnswer>(clientAppPrompts, { ...defaults, ...args });
+    const answers = await prompt<ReactAnswer>(prompts, { ...defaults, ...args });
 
-    const templatePath = path.resolve(__dirname, '../../templates/react');
-    await transform(templatePath, { ...args, ...answers });
+    const mergedArgs = {
+      ...args,
+      ...answers,
+    };
+    const templatePath = path.resolve(__dirname, '../../templates/angular');
+    await transform(templatePath, mergedArgs, {
+      filter: (filePath) => {
+        return !!mergedArgs.language || !filePath.endsWith('{{language}}.yml');
+      },
+    });
 
     const response = {
       nextSteps: [`* Connect to Sitecore with ${chalk.green('jss setup')} (optional)`],
       appName: answers.appName,
-      yes: args.yes,
     };
 
     return response;
