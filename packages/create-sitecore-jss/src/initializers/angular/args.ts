fda498b9f8c11ac7fdc942eaa66564589e1e2e95
CobyPear <coby.sher@sitecore.com>
2021-12-14 08:55:09 -0600

    Update AngularArgs interface, remove canary version from template pkg.json

--- packages/create-sitecore-jss/src/initializers/angular/args.ts
+++ packages/create-sitecore-jss/src/initializers/angular/args.ts
@@ -1,4 +1,5 @@
+import { ClientAppArgs } from '../../common/args/base';
 import { StyleguideArgs } from '../../common/args/styleguide';
 import { AngularAnswer } from './prompts';
 
-export interface AngularArgs extends StyleguideArgs, Partial<AngularAnswer> {}
+export interface AngularArgs extends ClientAppArgs, StyleguideArgs, Partial<AngularAnswer> {}
