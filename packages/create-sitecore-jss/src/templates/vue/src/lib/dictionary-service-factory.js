b3026e65d920881d9f5527fd7917e28151a5b0e3
Addy Pathania <aditya.pathania@sitecore.com>
2021-12-15 11:55:03 -0500

    added ejs confitional to graphql and rest factory

--- packages/create-sitecore-jss/src/templates/vue/src/lib/dictionary-service-factory.js
+++ packages/create-sitecore-jss/src/templates/vue/src/lib/dictionary-service-factory.js
@@ -1,13 +1,38 @@
-import { RestDictionaryService } from '@sitecore-jss/sitecore-jss-vue';
+import {
+  DictionaryService, 
+<% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+  -%>
+    GraphQLDictionaryService
+    <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
+        RestDictionaryService
+        <% } -%>  
+} from '@sitecore-jss/sitecore-jss-angular';
 import config from '../temp/config';
 
 export class DictionaryServiceFactory {
   create() {
-    return new RestDictionaryService({
-      apiHost: config.sitecoreApiHost,
-      apiKey: config.sitecoreApiKey,
-      siteName: config.jssAppName,
-    });
+  <% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+      -%>
+  return new GraphQLDictionaryService({
+        endpoint: config.graphQLEndpoint,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+        /*
+        The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+        app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+        otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+        rootItemId: '{GUID}'
+        */
+      });
+  <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
+  return new RestDictionaryService({
+        apiHost: config.sitecoreApiHost,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+      });
+  <% } -%>
   }
 }
 
--- packages/create-sitecore-jss/src/templates/vue/src/lib/layout-service-factory.js
+++ packages/create-sitecore-jss/src/templates/vue/src/lib/layout-service-factory.js
@@ -1,14 +1,33 @@
-import { RestLayoutService } from '@sitecore-jss/sitecore-jss-vue';
+import {
+  LayoutService,
+<% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+  -%>
+    GraphQLLayoutService
+    <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
+      RestLayoutService
+      <% } -%> 
+} from '@sitecore-jss/sitecore-jss-angular';
 import config from '../temp/config';
 
 export class LayoutServiceFactory {
   create() {
+    <% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+      -%>
+    return new GraphQLLayoutService({
+        endpoint: config.graphQLEndpoint,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+      });
+    <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
     return new RestLayoutService({
-      apiHost: config.sitecoreApiHost,
-      apiKey: config.sitecoreApiKey,
-      siteName: config.jssAppName,
-      configurationName: 'default',
-    });
+        apiHost: config.sitecoreApiHost,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+        configurationName: 'default',
+      });
+    <% } -%>
   }
 }
 
