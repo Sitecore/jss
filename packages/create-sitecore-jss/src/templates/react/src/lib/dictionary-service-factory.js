44effd2219c62a66a65fa6498c886ef49da2b8df
Addy Pathania <aditya.pathania@sitecore.com>
2021-12-15 09:57:33 -0500

    added ejs templating for graphql and rest

--- packages/create-sitecore-jss/src/templates/react/src/lib/dictionary-service-factory.js
+++ packages/create-sitecore-jss/src/templates/react/src/lib/dictionary-service-factory.js
@@ -1,14 +1,40 @@
-import { RestDictionaryService } from '@sitecore-jss/sitecore-jss-react';
+import {
+  DictionaryService,
+<% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+  -%>
+    GraphQLDictionaryService
+    <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
+        RestDictionaryService
+        <% }
+-%>
+} from '@sitecore-jss/sitecore-jss-react';
 import config from '../temp/config';
-
 export class DictionaryServiceFactory {
   create() {
-    return new RestDictionaryService({
-      apiHost: config.sitecoreApiHost,
-      apiKey: config.sitecoreApiKey,
-      siteName: config.jssAppName,
-    });
+  <% if (fetchWith.toUpperCase() === 'GRAPHQL') {
+      -%>
+  return new GraphQLDictionaryService({
+        endpoint: config.graphQLEndpoint,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+        /*
+        The Dictionary Service needs a root item ID in order to fetch dictionary phrases for the current
+        app. If your Sitecore instance only has 1 JSS App, you can specify the root item ID here;
+        otherwise, the service will attempt to figure out the root item for the current JSS App using GraphQL and app name.
+        rootItemId: '{GUID}'
+        */
+      });
+  <% } else if (fetchWith.toUpperCase() === 'REST') {
+      -%>
+  return new RestDictionaryService({
+        apiHost: config.sitecoreApiHost,
+        apiKey: config.sitecoreApiKey,
+        siteName: config.jssAppName,
+      });
+  <% } -%>
   }
 }
 
 export const dictionaryServiceFactory = new DictionaryServiceFactory();
+
