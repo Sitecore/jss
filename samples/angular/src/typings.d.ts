ce5dec02b855fd2a52d4526ff09a8713a7987be1
illiakovalenko <zlatoborodyi@gmail.com>
2021-12-16 11:15:50 +0200

    Remove `samples`, update contributing guide and CI

--- samples/angular/src/typings.d.ts
+++
@@ -1,6 +0,0 @@
-/* SystemJS module definition */
-/* eslint-disable no-var */
-declare var module: NodeModule;
-interface NodeModule {
-  id: string;
-}
--- samples/react/server/htmlTemplateFactory.js
+++
@@ -1,9 +0,0 @@
-export function getHtmlTemplate(state) {
-  let indexTemplate;
-  if (process.env.JSS_RENDER_ENGINE && process.env.JSS_RENDER_ENGINE === 'http') {
-    indexTemplate = require('../build/index.html');
-  } else {
-    indexTemplate = require('../build/index.html');
-  }
-  return Promise.resolve(indexTemplate);
-}
