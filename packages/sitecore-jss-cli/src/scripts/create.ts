95a5184de9ce8fe6d730ba49b490078042989109
illiakovalenko <zlatoborodyi@gmail.com>
2021-12-17 12:32:44 +0200

    Deprecation message

--- packages/sitecore-jss-cli/src/scripts/create.ts
+++ packages/sitecore-jss-cli/src/scripts/create.ts
@@ -1,33 +1,26 @@
-/* eslint-disable prettier/prettier */
-import { Argv } from 'yargs';
+const getDeprecationMessage = (argv: { [key: string]: unknown } = {}) =>
+  [
+    'jss create is not supported from JSS CLI 20.0.0',
+    // TODO: add (link to docs: https://doc.sitecore.net/<officialLink> ) when article will be created
+    'Please use npm init',
+    `Use: npm init sitecore-jss ${argv?.template || 'nextjs'}`,
+    'Please downgrade to JSS CLI 19.0.0 if you want to install older releases',
+    'Use:',
+    'npm install -g @sitecore-jss/sitecore-jss-cli@19.0.0',
+    `jss create ${argv?.name || 'nextjs-app'} ${argv?.template ||
+      'nextjs'} --branch release/19.0.0`,
+  ].join('\n');
+
+export const command = 'create <name> <template>';
+
+export const describe = false;
+
+export const builder = {};
 
 /**
- * @param {any} yargs
+ * Handle `jss create` command
+ * @param {Object} argv cli arguments
  */
-export function builder(yargs: Argv) {
-  return yargs.command(
-    'create <name> <template>',
-    getDeprecationMessage(),
-    (innerBuilder) => innerBuilder,
-    handler
-  );
-}
-
-const getDeprecationMessage = (argv: { [key: string]: unknown } = {} ) => [
-  'jss create is not supported from JSS CLI 20.0.0',
-  // TODO: add (link to docs: https://doc.sitecore.net/<officialLink> ) when article will be created
-  'Please use npm init',
-  `Use: npm init sitecore-jss ${argv?.template || 'nextjs'}`,
-  'Please downgrade to JSS CLI 19.0.0 if you want to install older releases',
-  'Use:',
-  'npm install -g @sitecore-jss/sitecore-jss-cli@19.0.0',
-  `jss create ${argv?.name || 'nextjs-app'} ${argv?.template || 'nextjs'} --branch release/19.0.0`
-].join('\n');
-
-/**
- * @param {any} argv
- */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-async function handler(argv: any) {
+export function handler(argv: { [key: string]: unknown }) {
   console.log(getDeprecationMessage(argv));
 }
