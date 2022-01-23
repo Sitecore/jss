eef8112c669bc713111044427327609bf4a6a10a
Addy Pathania <aditya.pathania@sitecore.com>
2022-01-23 13:16:19 -0500

    updated yarn.lock, removed dubplicate interface

--- packages/sitecore-jss-nextjs/src/components/NextImage.tsx
+++ packages/sitecore-jss-nextjs/src/components/NextImage.tsx
@@ -1,40 +1,27 @@
 import { mediaApi } from '@sitecore-jss/sitecore-jss/media';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { getEEMarkup, ImageField, ImageFieldValue } from '@sitecore-jss/sitecore-jss-react';
-import Image, { ImageLoader, ImageLoaderProps, ImageProps as NextImageProps } from 'next/image';
-export interface ImageProps extends Partial<NextImageProps> {
-  [attributeName: string]: unknown;
-  /** Image field data (consistent with other field types) */
-  field?: ImageField | ImageFieldValue;
+import {
+  getEEMarkup,
+  ImageProps,
+  ImageField,
+  ImageFieldValue,
+} from '@sitecore-jss/sitecore-jss-react';
+import Image, {
+  ImageLoader,
+  ImageLoaderProps,
+  ImageProps as NextImageProperties,
+} from 'next/image';
 
-  /**
-   * Can be used to explicitly disable inline editing.
-   * If true and `media.editable` has a value, then `media.editable` will be processed
-   * and rendered as component output. If false, `media.editable` value will be ignored and not rendered.
-   */
-  editable?: boolean;
-
-  /**
-   * Parameters that will be attached to Sitecore media URLs
-   */
-  imageParams?: {
-    [paramName: string]: string | number;
-  };
-  /**
-   * Custom regexp that finds media URL prefix that will be replaced by `/-/jssmedia` or `/~/jssmedia`.
-   * @example
-   * /\/([-~]{1})assets\//i
-   * /-assets/website -> /-/jssmedia/website
-   * /~assets/website -> /~/jssmedia/website
-   */
-  mediaUrlPrefix?: RegExp;
-
-  /** HTML attributes that will be appended to the rendered <img /> tag. */
-}
+type NextImageProps = Pick<
+  ImageProps,
+  'editable' | 'imageParams' | 'field' | 'mediaUrlPrefix' | 'srcSet'
+> &
+  Partial<NextImageProperties>;
 
 export const loader: ImageLoader = ({ src, width }: ImageLoaderProps): string => {
   const url = new URL(`https://cm.jss.localhost${src}`);
+
   const params = url.searchParams;
   params.set('mw', params.get('mw') || width.toString());
   params.delete('w');
@@ -42,20 +29,13 @@ export const loader: ImageLoader = ({ src, width }: ImageLoaderProps): string =>
   return url.href;
 };
 
-export const NextImage: React.SFC<ImageProps> = ({
+export const NextImage: React.SFC<NextImageProps> = ({
   editable,
   imageParams,
   field,
   mediaUrlPrefix,
   ...otherProps
 }) => {
-  // next handles srcSet, throw error if srcSet is present
-  if (otherProps.srcSet) {
-    throw new Error(
-      'srcSet not supported on Nextjs Image component. Use deviceSizes in next.config.js: https://nextjs.org/docs/api-reference/next/image#device-sizes'
-    );
-  }
-
   // next handles src and we use a custom loader,
   // throw error if these are present
   if (otherProps.src || otherProps.loader) {
@@ -77,7 +57,7 @@ export const NextImage: React.SFC<ImageProps> = ({
 
   // we likely have an experience editor value, should be a string
   if (editable && imageField.editable) {
-    return getEEMarkup(imageField, imageParams, mediaUrlPrefix, otherProps);
+    return getEEMarkup(imageField, imageParams, mediaUrlPrefix, otherProps as { src: string });
   }
 
   // some wise-guy/gal is passing in a 'raw' image object value
@@ -93,11 +73,10 @@ export const NextImage: React.SFC<ImageProps> = ({
       'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
   }
 
-  const attrs: NextImageProps = {
+  const attrs = {
     ...img,
     ...otherProps,
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    src: mediaApi.updateImageUrl(img.src!, imageParams, mediaUrlPrefix),
+    src: mediaApi.updateImageUrl(img.src as string, imageParams, mediaUrlPrefix),
   };
 
   if (attrs) {
