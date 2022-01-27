30da55d91274987df08a6a8e1724b3c8d11d5cb8
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2022-01-27 14:24:01 -0500

    [Next] New Next/Image component (#909)
    * Implement Nextjs Image nextjs package and styleguide
    
    * WIP: Implement nextjs image in nextjs package and styleguide
    
    * Refactored image loader
    
    * Refactor way in which EE markup is applied
    in react image component to be reused in next image component.
    
    * WIP: Start refactoring test for nextjs image
    also refactor nextjs-styleguide template to use new image component
    
    * added tests and completed todos
    
    * removed unwanted code and added line to end of file
    
    * addressed review comments
    
    * fixed unit tests
    
    * removed dublicate code
    
    * removed extra comments
    
    * updated yarn.lock
    
    * updated yarn.lock, removed dubplicate interface
    
    * removed changes in yarn.lock
    
    * updated yarn.lock
    
    * fixed linting error
    
    * updated deviceSizes comment in next.config
    
    * improved comments, added utility to get hostname
    
    * removed path and loader from config
    
    * added relative path to loader function
    
    * added comment for path variable
    
    * updated error message, removed extra interface
    
    * fixed undefined path variable issue
    
    * added test cases for the loader function
    
    * renamed next-setup file
    
    * updated next.config comments, refactored redundant code
    
    Co-authored-by: CobyPear <coby.sher@sitecore.com>

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/fields/Styleguide-FieldUsage-Image.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/fields/Styleguide-FieldUsage-Image.tsx
@@ -1,4 +1,4 @@
-import { Image, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import { NextImage, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import StyleguideSpecimen from 'components/styleguide/Styleguide-Specimen';
 import { ComponentProps } from 'lib/component-props';
 import { StyleguideSpecimenFields } from 'lib/component-props/styleguide';
@@ -11,6 +11,7 @@ type StyleguideFieldUsageImageProps = ComponentProps &
     };
   };
 
+  const BLUR_DATA_URL = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
 /**
  * Demonstrates usage of an Image content field within JSS.
  * Image field data is uploaded into the Sitecore Media Library.
@@ -18,18 +19,19 @@ type StyleguideFieldUsageImageProps = ComponentProps &
 const StyleguideFieldUsageImage = (props: StyleguideFieldUsageImageProps): JSX.Element => (
   <StyleguideSpecimen {...props} e2eId="styleguide-fieldusage-image">
     <p>Plain image</p>
-    <Image media={props.fields.sample1} />
+    <NextImage field={props.fields.sample1} height="51"
+      width="204" />
 
     {/*
       Advanced image usage example
       editable: controls whether image can be edited in Sitecore Experience Editor
       imageParams: parameters that are passed to Sitecore to perform server-side resizing of the image.
-        Sample rescales image to max 100x50 dimensions on the server, respecting aspect ratio
-        IMPORTANT: imageParams must be whitelisted for resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
+      Sample rescales image to max 100x50 dimensions on the server, respecting aspect ratio
+      IMPORTANT: imageParams must be whitelisted for resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
       any other attributes: pass through to img tag
     */}
     <p>Advanced image (not editable)</p>
-    <Image
+    <NextImage
       field={props.fields.sample2}
       editable={false}
       imageParams={{ mw: 100, mh: 50 }}
@@ -39,18 +41,23 @@ const StyleguideFieldUsageImage = (props: StyleguideFieldUsageImageProps): JSX.E
     />
 
     {/*
-      Srcset adaptive image usage example
-      Adaptive srcsets are supported using Sitecore server-side resizing.
-      The `srcSet` can use Sitecore image resizing parameters (i.e. w, h, mw, mh).
-      Sample create a srcset using two sizes (server resizing), 300 and 100px max widths, respecting aspect ratio.
-      IMPORTANT: srcSet params must be whitelisted for adaptive resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
+      srcSet in Nextjs Image is set inside of the next.config by setting an array of deviceSizes and imageSizes inside the images option. 
+      See here: https://nextjs.org/docs/api-reference/next/image
+      IMPORTANT: These sizes should match your Sitecore server-side allowlist. See /sitecore/config/*.config (search for 'allowedMediaParams')
+
+      placeholder='blur' blurs the image while loading with a transparent pixel provided in blurDataUrl.
+      You can also provide a color pixel for this functionality.
+      See here: https://nextjs.org/docs/api-reference/next/image#placeholder
     */}
     <p>Srcset responsive image</p>
-    <Image
+    <NextImage
       field={props.fields.sample2}
-      srcSet={[{ mw: 300 }, { mw: 100 }]}
-      sizes="(min-width: 960px) 300px, 100px"
-      className="img-fluid"
+      height="105"
+      width="200"
+      layout="responsive"
+      placeholder="blur"
+      // transparent pixel for blur placeholder
+      blurDataURL={BLUR_DATA_URL}
     />
   </StyleguideSpecimen>
 );
---
+++ packages/sitecore-jss-nextjs/src/components/NextImage.tsx
@@ -0,0 +1,125 @@
+import { mediaApi } from '@sitecore-jss/sitecore-jss/media';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {
+  getEEMarkup,
+  ImageProps,
+  ImageField,
+  ImageFieldValue,
+} from '@sitecore-jss/sitecore-jss-react';
+import Image, {
+  ImageLoader,
+  ImageLoaderProps,
+  ImageProps as NextImageProperties,
+} from 'next/image';
+
+type NextImageProps = Omit<ImageProps, 'media'> & Partial<NextImageProperties>;
+
+export type ImageConfigComplete = {
+  path: string;
+};
+
+export const loader: ImageLoader = ({ src, width }: ImageLoaderProps): string => {
+  try {
+    const { path: configPath } = (process.env.__NEXT_IMAGE_OPTS as any) as ImageConfigComplete;
+    const r = /^(?:[a-z]+:)?\/\//i;
+    const url = r.test(src) ? new URL(`${src}`) : new URL(`${configPath}${src}`);
+    const params = url.searchParams;
+    params.set('mw', params.get('mw') || width.toString());
+    params.delete('w');
+    return url.href;
+  } catch (err) {
+    throw new Error(
+      'Failed to load image. Please make sure images path is configured correctly in next.config.js'
+    );
+  }
+};
+
+export const NextImage: React.SFC<NextImageProps> = ({
+  editable,
+  imageParams,
+  field,
+  mediaUrlPrefix,
+  ...otherProps
+}) => {
+  // next handles src and we use a custom loader,
+  // throw error if these are present
+  if (otherProps.src || otherProps.loader) {
+    throw new Error(
+      'Detected conflicting props src or loader. If you wish to use these props, use next/image directly.'
+    );
+  }
+
+  const dynamicMedia = field as ImageField | ImageFieldValue;
+
+  if (
+    !field ||
+    (!dynamicMedia.editable && !dynamicMedia.value && !(dynamicMedia as ImageFieldValue).src)
+  ) {
+    return null;
+  }
+
+  const imageField = dynamicMedia as ImageField;
+
+  // we likely have an experience editor value, should be a string
+  if (editable && imageField.editable) {
+    return getEEMarkup(
+      imageField,
+      imageParams as { [paramName: string]: string | number },
+      mediaUrlPrefix as RegExp,
+      otherProps as { src: string }
+    );
+  }
+
+  // some wise-guy/gal is passing in a 'raw' image object value
+  const img: ImageFieldValue = (dynamicMedia as ImageFieldValue).src
+    ? (field as ImageFieldValue)
+    : (dynamicMedia.value as ImageFieldValue);
+  if (!img) {
+    return null;
+  }
+  // this prop is needed for non-static images - set it to 1x1 transparent pixel base64 encoded if not supplied by user.
+  if (!otherProps.blurDataURL) {
+    otherProps.blurDataURL =
+      'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
+  }
+
+  const attrs = {
+    ...img,
+    ...otherProps,
+    src: mediaApi.updateImageUrl(
+      img.src as string,
+      imageParams as { [paramName: string]: string | number },
+      mediaUrlPrefix as RegExp
+    ),
+  };
+
+  if (attrs) {
+    return <Image loader={loader} {...attrs} />;
+  }
+
+  return null; // we can't handle the truth
+};
+
+NextImage.propTypes = {
+  field: PropTypes.oneOfType([
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+    }),
+    PropTypes.shape({
+      value: PropTypes.object,
+      editable: PropTypes.string,
+    }),
+  ]),
+  editable: PropTypes.bool,
+  mediaUrlPrefix: PropTypes.instanceOf(RegExp),
+  imageParams: PropTypes.objectOf(
+    PropTypes.oneOfType([PropTypes.number.isRequired, PropTypes.string.isRequired]).isRequired
+  ),
+};
+
+NextImage.defaultProps = {
+  editable: true,
+};
+
+NextImage.displayName = 'NextImage';
