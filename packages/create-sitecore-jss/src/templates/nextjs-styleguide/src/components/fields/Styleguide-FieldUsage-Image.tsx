1ef7c7cd59e7d748dcc55517b111d2da713d887d
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2022-02-04 15:06:37 -0500

    Revert "[Next] New Next/Image component (#909)"
    This reverts commit 30da55d91274987df08a6a8e1724b3c8d11d5cb8.

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/fields/Styleguide-FieldUsage-Image.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/fields/Styleguide-FieldUsage-Image.tsx
@@ -1,4 +1,4 @@
-import { NextImage, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Image, ImageField, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import StyleguideSpecimen from 'components/styleguide/Styleguide-Specimen';
 import { ComponentProps } from 'lib/component-props';
 import { StyleguideSpecimenFields } from 'lib/component-props/styleguide';
@@ -11,7 +11,6 @@ type StyleguideFieldUsageImageProps = ComponentProps &
     };
   };
 
-  const BLUR_DATA_URL = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
 /**
  * Demonstrates usage of an Image content field within JSS.
  * Image field data is uploaded into the Sitecore Media Library.
@@ -19,19 +18,18 @@ type StyleguideFieldUsageImageProps = ComponentProps &
 const StyleguideFieldUsageImage = (props: StyleguideFieldUsageImageProps): JSX.Element => (
   <StyleguideSpecimen {...props} e2eId="styleguide-fieldusage-image">
     <p>Plain image</p>
-    <NextImage field={props.fields.sample1} height="51"
-      width="204" />
+    <Image media={props.fields.sample1} />
 
     {/*
       Advanced image usage example
       editable: controls whether image can be edited in Sitecore Experience Editor
       imageParams: parameters that are passed to Sitecore to perform server-side resizing of the image.
-      Sample rescales image to max 100x50 dimensions on the server, respecting aspect ratio
-      IMPORTANT: imageParams must be whitelisted for resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
+        Sample rescales image to max 100x50 dimensions on the server, respecting aspect ratio
+        IMPORTANT: imageParams must be whitelisted for resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
       any other attributes: pass through to img tag
     */}
     <p>Advanced image (not editable)</p>
-    <NextImage
+    <Image
       field={props.fields.sample2}
       editable={false}
       imageParams={{ mw: 100, mh: 50 }}
@@ -41,23 +39,18 @@ const StyleguideFieldUsageImage = (props: StyleguideFieldUsageImageProps): JSX.E
     />
 
     {/*
-      srcSet in Nextjs Image is set inside of the next.config by setting an array of deviceSizes and imageSizes inside the images option. 
-      See here: https://nextjs.org/docs/api-reference/next/image
-      IMPORTANT: These sizes should match your Sitecore server-side allowlist. See /sitecore/config/*.config (search for 'allowedMediaParams')
-
-      placeholder='blur' blurs the image while loading with a transparent pixel provided in blurDataUrl.
-      You can also provide a color pixel for this functionality.
-      See here: https://nextjs.org/docs/api-reference/next/image#placeholder
+      Srcset adaptive image usage example
+      Adaptive srcsets are supported using Sitecore server-side resizing.
+      The `srcSet` can use Sitecore image resizing parameters (i.e. w, h, mw, mh).
+      Sample create a srcset using two sizes (server resizing), 300 and 100px max widths, respecting aspect ratio.
+      IMPORTANT: srcSet params must be whitelisted for adaptive resizing to occur. See /sitecore/config/*.config (search for 'allowedMediaParams')
     */}
     <p>Srcset responsive image</p>
-    <NextImage
+    <Image
       field={props.fields.sample2}
-      height="105"
-      width="200"
-      layout="responsive"
-      placeholder="blur"
-      // transparent pixel for blur placeholder
-      blurDataURL={BLUR_DATA_URL}
+      srcSet={[{ mw: 300 }, { mw: 100 }]}
+      sizes="(min-width: 960px) 300px, 100px"
+      className="img-fluid"
     />
   </StyleguideSpecimen>
 );
--- packages/sitecore-jss-nextjs/src/tests/next-setup.js
+++
@@ -1,6 +0,0 @@
-﻿process.env = {
-  ...process.env,
-  __NEXT_IMAGE_OPTS: {
-    path: 'https://cm.jss.localhost',
-  },
-};
