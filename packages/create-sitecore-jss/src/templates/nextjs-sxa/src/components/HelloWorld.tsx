2ef92049bef03850d2c6ceeddba2ecd00daa1aed
Addy Pathania <aditya.pathania@sitecore.com>
2022-02-09 09:29:50 -0500

    changed placeholder component description

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/HelloWorld.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/HelloWorld.tsx
@@ -1,7 +1,5 @@
 ﻿/**
- * A simple Content Block component, with a heading and rich text block.
- * This is the most basic building block of a content site, and the most basic
- * JSS component that's useful.
+ * A simple placeholder component.
  */
 const HelloWorld = (): JSX.Element => (
   <div className="contentBlock">
