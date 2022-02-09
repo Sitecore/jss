b1d723f9f6d88907d4401ef2fe77ec9cf8e5a593
Addy Pathania <aditya.pathania@sitecore.com>
2022-02-09 09:28:03 -0500

    removed package.json, added placeholder component

---
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/HelloWorld.tsx
@@ -0,0 +1,12 @@
+﻿/**
+ * A simple Content Block component, with a heading and rich text block.
+ * This is the most basic building block of a content site, and the most basic
+ * JSS component that's useful.
+ */
+const HelloWorld = (): JSX.Element => (
+  <div className="contentBlock">
+    <p>Hello World</p>
+  </div>
+);
+
+export default HelloWorld;
