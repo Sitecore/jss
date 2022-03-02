1cf049ce20e9c926fade9e4b15b241360468df85
illiakovalenko <zlatoborodyi@gmail.com>
2022-03-02 16:46:43 +0200

    Use `data` attribute

--- packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
+++ packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
@@ -179,7 +179,7 @@ export function convertVNodesToDynamicComponents(vnodes: VNode[]) {
       },
     } as JssDynamicComponent;
 
-    if (vnode.props.name === 'code' && vnode.props.type === 'text/sitecore') {
+    if (vnode.props.data?.name === 'code' && vnode.props.data?.type === 'text/sitecore') {
       component.isxEditorComponent = true;
     }
     return component;
@@ -234,7 +234,7 @@ function createRawElement(elem: any) {
     },
   };
 
-  return h(component, { ...elem });
+  return h(component, { data: elem });
 }
 
 /**
