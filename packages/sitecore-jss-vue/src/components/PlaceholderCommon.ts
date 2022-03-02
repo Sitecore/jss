5fbd302b171f248b58e2b3b86e15a2fc8b50919c
illiakovalenko <zlatoborodyi@gmail.com>
2022-03-02 16:20:26 +0200

    [Vue] Styleguide-Layout-Reuse breaks EE

--- packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
+++ packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
@@ -179,7 +179,7 @@ export function convertVNodesToDynamicComponents(vnodes: VNode[]) {
       },
     } as JssDynamicComponent;
 
-    if (vnode.type === 'code' && vnode.props.type === 'text/sitecore') {
+    if (vnode.props.name === 'code' && vnode.props.type === 'text/sitecore') {
       component.isxEditorComponent = true;
     }
     return component;
@@ -234,7 +234,7 @@ function createRawElement(elem: any) {
     },
   };
 
-  return h(component);
+  return h(component, { ...elem });
 }
 
 /**
