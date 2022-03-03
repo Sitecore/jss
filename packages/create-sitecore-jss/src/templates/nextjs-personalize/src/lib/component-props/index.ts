123662e1bd797e91aded78ccd184259481e18b91
Addy Pathania <aditya.pathania@sitecore.com>
2022-03-03 15:18:16 -0500

    added experiences type to personalize add on

---
+++ packages/create-sitecore-jss/src/templates/nextjs-personalize/src/lib/component-props/index.ts
@@ -0,0 +1,4 @@
+﻿// NULL means Hidden by this experience
+export type ComponentRenderingWithExpiriences = ComponentRendering & {
+  experiences: { [name: string]: ComponentRenderingWithExpiriences | null };
+};
