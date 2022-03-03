2d9d346c737a313fffbc73f54eeca634e734546c
Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>
2022-03-03 12:46:36 +0200

    [Vue] Styleguide-Layout-Reuse breaks EE (#937)
    * [Vue] Styleguide-Layout-Reuse breaks EE
    
    * Use `data` attribute
    
    * Update CHANGELOG
    
    * Use `elem` instead of `data`

--- packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
+++ packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
@@ -179,7 +179,7 @@ export function convertVNodesToDynamicComponents(vnodes: VNode[]) {
       },
     } as JssDynamicComponent;
 
-    if (vnode.type === 'code' && vnode.props.type === 'text/sitecore') {
+    if (vnode.props.elem?.name === 'code' && vnode.props.elem?.type === 'text/sitecore') {
       component.isxEditorComponent = true;
     }
     return component;
@@ -234,7 +234,7 @@ function createRawElement(elem: any) {
     },
   };
 
-  return h(component);
+  return h(component, { elem });
 }
 
 /**
