8552f67dcfd2bff145da39a7cb5895a0cf4170f1
illiakovalenko <zlatoborodyi@gmail.com>
2022-03-03 12:44:34 +0200

    Use `elem` instead of `data`

--- packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
+++ packages/sitecore-jss-vue/src/components/PlaceholderCommon.ts
@@ -179,7 +179,7 @@ export function convertVNodesToDynamicComponents(vnodes: VNode[]) {
       },
     } as JssDynamicComponent;
 
-    if (vnode.props.data?.name === 'code' && vnode.props.data?.type === 'text/sitecore') {
+    if (vnode.props.elem?.name === 'code' && vnode.props.elem?.type === 'text/sitecore') {
       component.isxEditorComponent = true;
     }
     return component;
@@ -234,7 +234,7 @@ function createRawElement(elem: any) {
     },
   };
 
-  return h(component, { data: elem });
+  return h(component, { elem });
 }
 
 /**
