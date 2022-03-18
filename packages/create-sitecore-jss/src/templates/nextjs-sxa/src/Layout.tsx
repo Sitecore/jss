b876a172d346d17b3825ed0fd3ee56bebc8e63ef
Ruslan Matkovskyi <rusm@sitecore.net>
2022-03-18 17:38:01 +0200

    #520166 added Navigation component in the Layout

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/Layout.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/Layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useI18n } from 'next-localization';
 import {
   Placeholder,
   VisitorIdentification,
   getPublicUrl,
   LayoutServiceData,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-import Navigation from './components/Navigation';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
@@ -15,6 +16,42 @@ const publicUrl = getPublicUrl();
 interface LayoutProps {
   layoutData: LayoutServiceData;
 }
+
+// This is boilerplate navigation for sample purposes. Most apps should throw this away and use their own navigation implementation.
+// Most apps may also wish to use GraphQL for their navigation construction; this sample does not simply to support disconnected mode.
+const Navigation = () => {
+  const { t } = useI18n();
+
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
+      <h5 className="my-0 mr-md-auto font-weight-normal">
+        <Link href="/">
+          <a className="text-dark">
+            <img src={`${publicUrl}/sc_logo.svg`} alt="Sitecore" />
+          </a>
+        </Link>
+      </h5>
+      <nav className="my-2 my-md-0 mr-md-3">
+        <a
+          className="p-2 text-dark"
+          href="https://jss.sitecore.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('Documentation')}
+        </a>
+        {/* #START_EMPTY */}
+        <Link href="/styleguide">
+          <a className="p-2 text-dark">{t('Styleguide')}</a>
+        </Link>
+        <Link href="/graphql">
+          <a className="p-2 text-dark">{t('GraphQL')}</a>
+        </Link>
+        {/* #END_EMPTY */}
+      </nav>
+    </div>
+  );
+};
 
 const Layout = ({ layoutData }: LayoutProps): JSX.Element => {
   const { route } = layoutData.sitecore;
