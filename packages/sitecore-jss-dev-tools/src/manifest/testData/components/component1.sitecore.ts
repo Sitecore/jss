a0e1a7acf4360b63643dea2f3f2e45f168222f38
Nikolaos Lazaridis <101863865+sc-nikolaoslazaridis@users.noreply.github.com>
2022-04-29 14:58:27 +0300

    tsconfig.json updates, create shared tsconfig for the repository (#1000)
    * Created a tsconfig.json file at root level that has all shared options used in packages. Added noImplicitThis, noImplicitAny, noUnusedLocals and allowSyntheticDefaultImports to this file. Added noImplicitAny, noImplicitReturns, noImplicitThis, noUnusedLocals and  noUnusedParameters to samples that have tsconfig file. Fixed ts errors caused by the changes. Deleted unused testData folder in sitecore-jss-dev-tools/src/manifest.
    
    * add improvements
    
    * fix test
    
    Co-authored-by: illiakovalenko <zlatoborodyi@gmail.com>

--- packages/sitecore-jss-dev-tools/src/manifest/testData/components/component1.sitecore.ts
+++
@@ -1,9 +0,0 @@
-import { Manifest } from './../../../index';
-
-export default (manifest: Manifest) => {
-  manifest.addComponent({
-    name: 'Component1',
-    displayName: 'Component1',
-    placeholders: [{ name: 'page-header' }, { name: 'page-content' }],
-  });
-};
