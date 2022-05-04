97c050def7c1a77ea8eba79521386d1aaeae8563
Addy Pathania <aditya.pathania@sitecore.com>
2022-05-04 13:42:33 -0400

    added script to restore yarn.lock

---
+++ packages/create-sitecore-jss/scripts/post-install.ts
@@ -0,0 +1,11 @@
+﻿const execSync = require('child_process').execSync;
+
+// restore yarn.lock file post initializing a sample app using watch script.
+export const postInstall = () => {
+  const output = execSync('git status', { encoding: 'utf-8' });
+  if (output.includes('yarn.lock')) {
+    execSync('git restore ../../yarn.lock', { encoding: 'utf-8' });
+  }
+};
+
+postInstall();
--- packages/create-sitecore-jss/scripts/watch-templates.ts
+++ packages/create-sitecore-jss/scripts/watch-templates.ts
@@ -2,18 +2,27 @@ import chalk from 'chalk';
 import chokidar from 'chokidar';
 import path from 'path';
 import { initRunner } from '../src/init-runner';
+import { postInstall } from './post-install';
 
 chokidar
   .watch(path.join(process.cwd(), '.\\src\\templates'), { ignoreInitial: true })
   .on('ready', () => ready())
   .on('all', (event, path) => callback(event, path));
 
+/**
+ *
+ */
 async function ready() {
   console.log(chalk.green('Initializing app...'));
   await initializeApps(false);
+  postInstall();
   console.log(chalk.green('Initializing app complete. Watching for changes...'));
 }
 
+/**
+ * @param event
+ * @param path
+ */
 async function callback(event?: string, path?: string) {
   const color = event === 'add' ? chalk.green : event === 'unlink' ? chalk.red : chalk.white;
   console.table(color(`${event} ${path}`));
