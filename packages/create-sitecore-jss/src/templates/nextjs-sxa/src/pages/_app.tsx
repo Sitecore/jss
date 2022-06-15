82d287e4ff8c6265778a047d441406ccadd3246e
Ruslan Matkovskyi <100142572+matkovskyi@users.noreply.github.com>
2022-06-15 21:03:51 +0300

    #533938 added styles sass (#1058)
    * #533938 added styles sass
    
    * #533938 removed local fontawesome and added npm package fontawesome

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/_app.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/app.css';
+import 'assets/sass/main.scss';
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   const { dictionary, ...rest } = pageProps;
