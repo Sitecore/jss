da1fc90f6ed7b3940f6eeefca545c1883290726f
Ruslan Matkovskyi <rusm@sitecore.net>
2022-06-24 07:22:16 +0300

    #539379 [SXA] fixed mobile menu

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/_app.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 
+// in the main.css is the list of used css
 import 'assets/main.scss';
 import 'assets/app.css';
 
