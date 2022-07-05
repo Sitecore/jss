778e20777bc0ec1e5ce457697925f9ee5f19b328
illiakovalenko <zlatoborodyi@gmail.com>
2022-07-05 16:36:04 +0300

    [React] Resolve duplicate `react` instances issue

--- packages/create-sitecore-jss/src/initializers/react/index.ts
+++ packages/create-sitecore-jss/src/initializers/react/index.ts
@@ -1,7 +1,14 @@
 ﻿import chalk from 'chalk';
-import path from 'path';
+import path, { sep } from 'path';
 import { prompt } from 'inquirer';
-import { Initializer, transform } from '../../common';
+import {
+  Initializer,
+  isDevEnvironment,
+  openPackageJson,
+  transform,
+  writePackageJson,
+  removeFile,
+} from '../../common';
 import { prompts, ReactAnswer } from './prompts';
 import { ReactArgs } from './args';
 
@@ -20,6 +27,30 @@ export default class ReactInitializer implements Initializer {
 
     const templatePath = path.resolve(__dirname, '../../templates/react');
     await transform(templatePath, mergedArgs);
+
+    const isDev = isDevEnvironment(args.destination);
+    const pkgPath = path.resolve(`${args.destination}${sep}package.json`);
+    const pkg = openPackageJson(pkgPath);
+
+    if (isDev) {
+      Object.entries<string>(pkg.scripts).forEach(([key, value]) => {
+        // Can't rewire `eject` script
+        if (key === 'eject') return;
+
+        // It's required to start each command using `react-app-rewired`, to solve duplicate `react` issue
+        pkg.scripts[key] = value.replace('react-scripts', 'react-app-rewired');
+      });
+
+      writePackageJson(pkg, pkgPath);
+    } else {
+      // Don't need to rewire anything if we are not in the dev env
+      delete pkg.devDependencies['react-app-rewired'];
+
+      // remove webpack overrides
+      removeFile(path.resolve(`${args.destination}${sep}config-overrides.js`));
+
+      writePackageJson(pkg, pkgPath);
+    }
 
     const response = {
       nextSteps: [`* Connect to Sitecore with ${chalk.green('jss setup')} (optional)`],
