e9a9059bc1eebe08597cfcf1860e0e9c3fb1dc23
Ruslan Matkovskyi <100142572+matkovskyi@users.noreply.github.com>
2022-08-04 17:57:35 +0300

    #542500: fixed component container and style Error block (#1121)
    (cherry picked from commit 9b8060eb05ccded9180fc172945fe7fee61b0eed)

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
@@ -16,7 +16,7 @@ interface ComponentProps {
   params: ComponentParams;
 }
 
-const Container = (props: ComponentProps): JSX.Element => {
+export const Default = (props: ComponentProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
   const containerStyles = props.params && props.params.Styles ? props.params.Styles : '';
   const styles = `${props.params.GridParameters} ${containerStyles}`.trimEnd();
@@ -45,5 +45,3 @@ const Container = (props: ComponentProps): JSX.Element => {
     </div>
   );
 };
-
-export default Container;
