45df98af2a25b67f53dcacc8082e05a73a96e53b
Artem Alexeyenko <ala@sitecore.net>
2022-08-31 14:47:37 -0400

    pipeline rewrite: removed build step for now, removed --since usage

--- scripts/scaffold-samples.js
+++ scripts/scaffold-samples.js
@@ -1,20 +1,22 @@
-import chalk from 'chalk';
-import { initRunner } from '../src/init-runner';
-// const templates = require('./templates.json');
-import samplesToSacffold from './samples.json';
+const chalk = require('chalk');
+
+const { initRunner } = require('../packages/create-sitecore-jss/dist/init-runner');
+const samplesToSacffold = require('./samples.json');
 
 for (const sample of samplesToSacffold) {
   sample.args.appName = `sample-${sample.args.appName}`;
+  sample.args.destination = `./samples/${sample.args.appName}`;
+  sample.args.hostName = `${sample.args.appName}.jss.localhost`;
   console.log(chalk.green(`Initializing sample ${sample.args.appName} ...`));
   // we need to keep noInstall as true - otherwise both yarn install and lint will execute
   // we run lint separately in the azure pipeline
   let scaffoldArgs = {
-    ...sample.args,
     templates: sample.initializers,
     yes: true,
     force: true,
     silent: true,
     noInstall: true,
+    ...sample.args,
   }
   initRunner(sample.initializers, scaffoldArgs);
 }
