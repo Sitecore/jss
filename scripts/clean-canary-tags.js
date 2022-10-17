250147316f13f56b7c1d43b6497f3d3431cef2f3
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2022-10-17 16:39:46 -0500

    Update to clean-canary-tags.js script: avoid "The command line is too long" errors by batching [skip ci]

--- scripts/clean-canary-tags.js
+++ scripts/clean-canary-tags.js
@@ -10,20 +10,23 @@ const output = execSync('git tag', {
   encoding: 'utf-8',
 });
 
-const tags = output
-  .split('\n')
-  .filter((tag) => tag.includes('-canary'))
-  .join(' ');
+const tags = output.split('\n').filter((tag) => tag.includes('-canary'));
 
-if (!tags.length) {
+if (tags.length === 0) {
   console.log('No tags to remove!');
   return;
 }
 
-// remove local tags
-execSync(`git tag -d ${tags}`, {
-  stdio: 'inherit',
-});
+// Avoid "The command line is too long" errors by batching
+const batchSize = 100;
+for (let i = 0; i < tags.length; i += batchSize) {
+  const batchTags = tags.slice(i, i + batchSize);
 
-// remove remote tags
-execSync(`git push -d origin ${tags}`, { stdio: 'inherit' });
+  // remove local tags
+  execSync(`git tag -d ${batchTags.join(' ')}`, {
+    stdio: 'inherit',
+  });
+
+  // remove remote tags
+  execSync(`git push -d origin ${batchTags.join(' ')}`, { stdio: 'inherit' });
+}
