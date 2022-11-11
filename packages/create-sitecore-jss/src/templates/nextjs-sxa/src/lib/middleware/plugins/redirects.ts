4116e5fa92cd4495ffc493ed44006ad4c111b45f
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2022-11-11 11:16:44 -0500

    [Nextjs][SXA] Performance improvement for site `redirects` service (#1225)
    * added cache for redirects middleware response
    
    * updated js doc comments
    
    * addresed review comments
    
    * Update packages/create-sitecore-jss/src/templates/nextjs-sxa/src/lib/middleware/plugins/redirects.ts
    
    Co-authored-by: Adam Brauer <400763+ambrauer@users.noreply.github.com>
    
    * remove unnecessary import
    
    * address review comments
    
    * added unit test for redirects middleware
    
    * added more tests to redirects
    
    * refactored tests
    
    * combined conditionals in redirect middleware
    
    Co-authored-by: Adam Brauer <400763+ambrauer@users.noreply.github.com>

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/lib/middleware/plugins/redirects.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/lib/middleware/plugins/redirects.ts
@@ -15,6 +15,13 @@ class RedirectsPlugin implements MiddlewarePlugin {
       // These are all the locales you support in your application.
       // These should match those in your next.config.js (i18n.locales).
       locales: ['en'],
+      // This function determines if a route should be excluded from RedirectsMiddleware.
+      // Certain paths are ignored by default (e.g. files and Next.js API routes), but you may wish to exclude more.
+      // This is an important performance consideration since Next.js Edge middleware runs on every request.
+      excludeRoute: () => false,
+      // This function determines if the middleware should be turned off.
+      // By default it is disabled while in development mode.
+      disabled: () => process.env.NODE_ENV === 'development',
     });
   }
 
