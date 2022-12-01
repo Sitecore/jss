98e893dbbe5f15e2d17382a2aac2fabfbaaf6aa3
illiakovalenko <illia.kovalenko@sitecore.com>
2022-12-01 13:42:45 +0200

    Upgrade sitecore-jss-react, sitecore-jss-react-forms, react sample, required part for sitecore-jss-nextjs, nextjs sample

--- packages/create-sitecore-jss/src/templates/react/config-overrides.js
+++ packages/create-sitecore-jss/src/templates/react/config-overrides.js
@@ -4,5 +4,11 @@ module.exports = function override(config) {
   // Provide alias to don't have duplicates of `react`
   config.resolve.alias.react = path.resolve(process.cwd(), '.', 'node_modules', 'react');
 
+  // This will remove the CRA plugin that prevents to import modules from
+  // outside the `src` directory, useful for symlinks
+  config.resolve.plugins = config.resolve.plugins.filter(
+    (p) => p.constructor.name !== 'ModuleScopePlugin'
+  );
+
   return config;
 };
