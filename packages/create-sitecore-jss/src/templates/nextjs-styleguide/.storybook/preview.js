6a3b2f855125d4b9e19572643cacc280cfa90148
illiakovalenko <illia.kovalenko@sitecore.com>
2022-12-06 13:54:48 +0200

    add eol

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/.storybook/preview.js
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/.storybook/preview.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import "../src/assets/app.css"
+import '../src/assets/app.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
