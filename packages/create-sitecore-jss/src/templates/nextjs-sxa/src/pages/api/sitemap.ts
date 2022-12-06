5bedeb5c7ee3c6270d4212cd3c8e7a3f6171f264
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2022-12-06 12:19:38 -0600

    #557761: fixed sitemap handler (#1229)
    * #557761: fixed sitemap handler
    
    * #557761: refactoring and fixed comments
    
    (cherry picked from commit 16d2eeb6ecc312de91be602eceb93e316ca1ed71)

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/api/sitemap.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/api/sitemap.ts
@@ -19,24 +19,52 @@ const sitemapApi = async (
     siteName: config.jssAppName,
   });
 
-  const sitemapPath = await sitemapXmlService.getSitemap(id as string);
+  // if url has sitemap-{n}.xml type
+  if (id) {
+    const sitemapPath = await sitemapXmlService.getSitemap(id as string);
 
-  if (sitemapPath) {
-    const isAbsoluteUrl = sitemapPath.match(ABSOLUTE_URL_REGEXP);
-    const sitemapUrl = isAbsoluteUrl ? sitemapPath : `${config.sitecoreApiHost}${sitemapPath}`;
-    res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+    // if sitemap is match otherwise redirect to 404 page
+    if (sitemapPath) {
+      const isAbsoluteUrl = sitemapPath.match(ABSOLUTE_URL_REGEXP);
+      const sitemapUrl = isAbsoluteUrl ? sitemapPath : `${config.sitecoreApiHost}${sitemapPath}`;
+      res.setHeader('Content-Type', 'text/xml;charset=utf-8');
 
-    return new AxiosDataFetcher()
-      .get(sitemapUrl, {
-        responseType: 'stream',
-      })
-      .then((response: AxiosResponse) => {
-        response.data.pipe(res);
-      })
-      .catch(() => res.redirect('/404'));
+      // need to prepare stream from sitemap url
+      return new AxiosDataFetcher()
+        .get(sitemapUrl, {
+          responseType: 'stream',
+        })
+        .then((response: AxiosResponse) => {
+          response.data.pipe(res);
+        })
+        .catch(() => res.redirect('/404'));
+    }
+
+    return res.redirect('/404');
   }
 
-  res.redirect('/404');
+  // this approache if user go to /sitemap.xml - under it generate xml page with list of sitemaps
+  const sitemaps = await sitemapXmlService.fetchSitemaps();
+
+  if (!sitemaps.length) {
+    return res.redirect('/404');
+  }
+
+  const SitemapLinks = sitemaps
+    .map(
+      (item) =>
+        `<sitemap>
+            <loc>${item}</loc>
+          </sitemap>
+        `
+    )
+    .join('');
+
+  res.setHeader('Content-Type', 'text/xml;charset=utf-8');
+
+  return res.send(`
+  <sitemapindex xmlns="http://sitemaps.org/schemas/sitemap/0.9">${SitemapLinks}</sitemapindex>
+  `);
 };
 
 export default sitemapApi;
