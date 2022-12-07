ad5ecf67bf905c6ce6d40a89b981204b9f83abd3
Addy Pathania <aditya.pathania@sitecore.com>
2022-12-06 19:48:42 -0500

    resolved monorepo issue, added config for alias

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/.storybook/main.js
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,5 +10,14 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve(__dirname, '../src/components'),
+      lib: path.resolve(__dirname, '../src/lib'),
+    };
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
   },
 };
