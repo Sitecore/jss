3435c4c97ffe83b545a85190f13f5b762476de06
illiakovalenko <illia.kovalenko@sitecore.com>
2022-12-09 10:34:40 +0200

    Add Styleguide-Specimen, Styleguide-Tracking

---
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/styleguide/Styleguide-Specimen.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import StyleguideSpecimen from './Styleguide-Specimen';
+
+export default {
+  title: 'Components/styleguide/Styleguide-Specimen',
+  component: StyleguideSpecimen,
+} as ComponentMeta<typeof StyleguideSpecimen>;
+
+const Template: ComponentStory<typeof StyleguideSpecimen> = (args) => (
+  <StyleguideSpecimen {...args}>
+    <b>Custom children</b>
+  </StyleguideSpecimen>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  params: {
+    name: 'Styleguide-Specimen',
+  },
+  rendering: {
+    uid: '{00000000-0000-0000-0000-000000000000}',
+    componentName: 'Styleguide-Specimen',
+    dataSource: '{00000000-0000-0000-0000-000000000000}',
+  },
+  fields: {
+    description: {
+      value: 'Styleguide Specimen Component',
+    },
+    heading: {
+      value: 'Styleguide Specimen',
+    },
+  },
+  e2eId: 'test-e2e-id',
+};
