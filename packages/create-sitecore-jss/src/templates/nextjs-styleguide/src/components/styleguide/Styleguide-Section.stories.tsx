a98b2e6e1400b1b48d24202207afedbea8c596e7
Addy Pathania <aditya.pathania@sitecore.com>
2022-12-12 12:49:44 -0500

    added placeholder args for styleguide-section story

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/styleguide/Styleguide-Section.stories.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/components/styleguide/Styleguide-Section.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import StyleguideSection, { StyleguideSectionProps as Props } from './Styleguide-Section';
 import { StorybookArgs, withFields, withSitecoreContext } from 'storybook-utils/utils';
+import { SingleLine, MultiLine } from '../fields/Styleguide-FieldUsage-Text.stories';
+import StyleguideFieldUsageText from './../fields/Styleguide-FieldUsage-Text';
 
 export default {
   title: 'Components/styleguide/Styleguide-Section',
@@ -9,6 +11,11 @@ export default {
 } as ComponentMeta<typeof StyleguideSection>;
 
 type Args = StorybookArgs<Props>;
+
+const components = new Map();
+components.set('Styleguide-FieldUsage-Text', StyleguideFieldUsageText);
+// eslint-disable-next-line react/display-name
+const componentFactory = (name: string) => components.get(name);
 
 const Template: ComponentStory<typeof StyleguideSection> = (args) => (
   <StyleguideSection {...args} />
@@ -23,10 +30,26 @@ Default.args = withFields<Args, Props>({
     uid: '{00000000-0000-0000-0000-000000000000}',
     componentName: 'Styleguide-Section',
     dataSource: '{00000000-0000-0000-0000-000000000000}',
+    placeholders: {
+      'NextjsApp-jss-styleguide-section': [
+        {
+          uid: '1846c499-afa7-56c4-bade-e3880eac0134',
+          componentName: 'Styleguide-FieldUsage-Text',
+          dataSource: '{6E81D12B-6E44-5CD9-919A-6707B6723FEC}',
+          fields: SingleLine.args?.fields,
+        },
+        {
+          uid: '1846c499-afa7-56c4-bade-e3880eac0134',
+          componentName: 'Styleguide-FieldUsage-Text',
+          dataSource: '{6E81D12B-6E44-5CD9-919A-6707B6723FEC}',
+          fields: MultiLine.args?.fields,
+        },
+      ],
+    },
   },
   fields: {
     heading: 'Content Data',
   },
 });
 
-Default.decorators = [withSitecoreContext()];
+Default.decorators = [withSitecoreContext(componentFactory)];
