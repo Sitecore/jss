96bd727e3614f4d6d795468871822ccc00b1d29f
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2023-01-03 13:41:07 -0600

    [Next.js][Multi-site] Fetch site info during build (#1276)
    * GraphQL site info service (#1227)
    
    * GraphQL site info service
    
    * some tweaks
    
    * more tweaks: debug, query
    
    * query fix for xm cloud
    
    * move template id to contstants
    
    * [Next.js][Multi-site] Dynamic site resolver (#1224)
    
    * [Next.js][Multi-site] Dynamic site resolver
    
    * Use SiteInfo from graphql-siteinfo-service
    
    Co-authored-by: Adam Brauer <400763+ambrauer@users.noreply.github.com>
    
    * Plugin approach for JSS config generation. Added multisite plugin to set the "sites" prop.
    
    * Standardized comments in config plugins. Misc fixes.
    
    * Fixed tests and relocated headlessSiteGroupingTemplate constant (since this query is temp anyway)
    
    * Moved defaultLanguage of 'en' to defaultConfig
    
    * Simplify config plugin order. Delete multisite-sample.ts
    
    Co-authored-by: Artem Alexeyenko <ala@sitecore.net>
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>

--- packages/create-sitecore-jss/src/templates/nextjs/scripts/bootstrap.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/scripts/bootstrap.ts
@@ -1,41 +1,21 @@
-import 'dotenv/config';
-import { generateConfig } from './generate-config';
-import { constants } from '@sitecore-jss/sitecore-jss-nextjs';
-import chalk from 'chalk';
 /*
   BOOTSTRAPPING
   The bootstrap process runs before build, and generates JS that needs to be
-  included into the build - specifically, the component name to component mapping,
-  and the global config module.
+  included into the build - specifically, plugins, the global config module,
+  and the component name to component mapping.
 */
-
-const disconnected = process.env.JSS_MODE === constants.JSS_MODE.DISCONNECTED;
-/*
-  CONFIG GENERATION
-  Generates the /src/temp/config.js file which contains runtime configuration
-  that the app can import and use.
-*/
-const port = process.env.PORT || 3000;
-const configOverride: { [key: string]: string } = {};
-if (disconnected) {
-  if (process.env.FETCH_WITH === constants.FETCH_WITH.GRAPHQL) {
-    throw new Error(
-      chalk.red(
-        'GraphQL requests to Dictionary and Layout services are not supported in disconnected mode.'
-      )
-    );
-  }
-  configOverride.sitecoreApiHost = `http://localhost:${port}`;
-}
-
-generateConfig(configOverride);
-
-/*
-  COMPONENT FACTORY GENERATION
-*/
-import './generate-component-factory';
 
 /*
    PLUGINS GENERATION
 */
 import './generate-plugins';
+
+/*
+  CONFIG GENERATION
+*/
+import './generate-config';
+
+/*
+  COMPONENT FACTORY GENERATION
+*/
+import './generate-component-factory';
