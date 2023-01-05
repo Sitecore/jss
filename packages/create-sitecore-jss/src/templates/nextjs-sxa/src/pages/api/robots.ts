915a1f7466bb9e52d90a8ab9878921d87df0ddbd
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2023-01-05 15:11:28 -0600

    Adjustments to API routes based on latest SiteResolver changes

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/api/robots.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/pages/api/robots.ts
@@ -1,25 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { GraphQLRobotsService } from '@sitecore-jss/sitecore-jss-nextjs';
+import { siteResolver } from 'lib/site-resolver';
 import config from 'temp/config';
-import { GraphQLRobotsService, SiteResolver } from '@sitecore-jss/sitecore-jss-nextjs';
 
 const robotsApi = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   // Ensure response is text/html
   res.setHeader('Content-Type', 'text/html;charset=utf-8');
 
-  // Resolve site based on hostname"
+  // Resolve site based on hostname
   const hostName = req.headers['host']?.split(':')[0] || 'localhost';
-
-  // Site information fetched directly from Sitecore.
-  const sites = JSON.parse(config.sites);
-
-  // Resolve site name based on host information
-  const siteName = await SiteResolver.resolve(hostName, sites);
+  const site = siteResolver.getByHost(hostName);
 
   // create robots graphql service
   const robotsService = new GraphQLRobotsService({
     endpoint: config.graphQLEndpoint,
     apiKey: config.sitecoreApiKey,
-    siteName: siteName || config.jssAppName,
+    siteName: site.name,
   });
 
   const robotsResult = await robotsService.fetchRobots();
