9f89fdb667319d64b74f8f3a6baf4b5e4c76c736
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2023-01-05 13:30:03 -0600

    Another refactor of SiteResolver. It now must be instantiated with the array of SiteInfo passed. Also introduced a way to resolve by site name ("getByName"). The old "resolve" became "getByHost".

--- packages/sitecore-jss/src/site/site-resolver.test.ts
+++ packages/sitecore-jss/src/site/site-resolver.test.ts
@@ -3,116 +3,174 @@ import { expect } from 'chai';
 import { SiteResolver } from './site-resolver';
 
 describe('SiteResolver', () => {
-  describe('resolve', () => {
-    it('should return undefined when sites info is empty', () => {
-      const site = SiteResolver.resolve('foo.com', []);
-
-      expect(site).to.be.undefined;
+  describe('getByHost', () => {
+    it('should throw when sites info is empty', () => {
+      const resolver = new SiteResolver([]);
+      expect(() => {
+        resolver.getByHost('foo.com');
+      }).to.throw(Error, 'Could not resolve site for host foo.com');
     });
 
-    it('should return undefined when there is no appropriate site info', () => {
-      const site = SiteResolver.resolve('foo.com', [
+    it('should throw when there is no appropriate site info', () => {
+      const resolver = new SiteResolver([
         { hostName: 'bar.com', language: '', name: 'bar' },
         { hostName: 'var.com', language: '', name: 'var' },
       ]);
-
-      expect(site).to.be.undefined;
+      expect(() => {
+        resolver.getByHost('foo.com');
+      }).to.throw(Error, 'Could not resolve site for host foo.com');
     });
 
     it('should return site', () => {
-      const site = SiteResolver.resolve('foo.com', [
+      const resolver = new SiteResolver([
         { hostName: 'bar.net', language: '', name: 'bar' },
         { hostName: 'foo.com', language: '', name: 'foo' },
         { hostName: 'var.com', language: '', name: 'var' },
       ]);
+      const site = resolver.getByHost('foo.com');
 
       expect(site).to.not.be.undefined;
       expect(site?.name).to.equal('foo');
     });
 
     it('should return site when hostname includes wildcard', () => {
-      const site = SiteResolver.resolve('foo.com', [
+      const resolver = new SiteResolver([
         { hostName: 'var.com', language: '', name: 'var' },
         { hostName: 'bar.net', language: '', name: 'bar' },
         { hostName: '*.com', language: '', name: 'foo' },
         { hostName: 'foo.com', language: '', name: 'foo-en' },
       ]);
+      const site = resolver.getByHost('foo.com');
 
       expect(site).to.not.be.undefined;
       expect(site?.name).to.equal('foo');
     });
 
     it('should return site when wildcard is provided', () => {
-      const site = SiteResolver.resolve('foo.com', [
+      const resolver = new SiteResolver([
         { hostName: 'bar.net', language: '', name: 'bar' },
         { hostName: '*', language: '', name: 'wildcard' },
         { hostName: 'foo.com', language: '', name: 'foo' },
       ]);
+      const site = resolver.getByHost('foo.com');
 
       expect(site).to.not.be.undefined;
       expect(site?.name).to.equal('wildcard');
     });
 
-    describe('should return site when multi-value hostnames are provided', () => {
+    it('should be case insensitive', () => {
+      const resolver = new SiteResolver([{ hostName: 'foo.com', language: '', name: 'foo' }]);
+      const site = resolver.getByHost('Foo.com');
+
+      expect(site).to.not.be.undefined;
+      expect(site?.name).to.equal('foo');
+    });
+
+    describe('multi-value hostname', () => {
       it('hostnames include wildcard characters', () => {
-        let site = SiteResolver.resolve('test.foo.bar.com', [
+        let resolver = new SiteResolver([
           { hostName: '*.bat.com|foo.bar.com', language: '', name: 'bar' },
           { hostName: 'test.com|*.foo.*.com|foo.com', language: '', name: 'foo' },
         ]);
+        let site = resolver.getByHost('test.foo.bar.com');
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
 
-        site = SiteResolver.resolve('xfoo.bar.com.en', [
+        resolver = new SiteResolver([
           { hostName: 'foo.bar.com', language: '', name: 'bar' },
           { hostName: 'test.com|*foo.*.com*|foo.com', language: '', name: 'foo' },
         ]);
+        site = resolver.getByHost('xfoo.bar.com.en');
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
       });
 
       it('hostname contains whitespaces', () => {
-        const site = SiteResolver.resolve('foo.com', [
+        const resolver = new SiteResolver([
           { hostName: 'bar.net', language: '', name: 'bar' },
           { hostName: 'test.com; foo.net | foo.com', language: '', name: 'foo' },
           { hostName: 'var.com', language: '', name: 'var' },
         ]);
+        const site = resolver.getByHost('foo.com');
 
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
       });
 
       it('comma delimiter is used', () => {
-        const site = SiteResolver.resolve('foo.com', [
+        const resolver = new SiteResolver([
           { hostName: 'bar.net', language: '', name: 'bar' },
           { hostName: 'test.com,foo.net,foo.com', language: '', name: 'foo' },
           { hostName: 'var.com', language: '', name: 'var' },
         ]);
+        const site = resolver.getByHost('foo.com');
 
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
       });
 
       it('semicolon delimiter is used', () => {
-        const site = SiteResolver.resolve('foo.com', [
+        const resolver = new SiteResolver([
           { hostName: 'bar.net', language: '', name: 'bar' },
           { hostName: 'test.com;foo.net;foo.com', language: '', name: 'foo' },
           { hostName: 'var.com', language: '', name: 'var' },
         ]);
+        const site = resolver.getByHost('foo.com');
 
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
       });
 
       it('pipe delimiter is used', () => {
-        const site = SiteResolver.resolve('foo.com', [
+        const resolver = new SiteResolver([
           { hostName: 'bar.net', language: '', name: '' },
           { hostName: 'test.com|foo.net|foo.com', language: '', name: 'foo' },
           { hostName: 'var.com', language: '', name: 'var' },
         ]);
+        const site = resolver.getByHost('foo.com');
 
         expect(site).to.not.be.undefined;
         expect(site?.name).to.equal('foo');
       });
+    });
+  });
+
+  describe('getByName', () => {
+    it('should throw when sites info is empty', () => {
+      const resolver = new SiteResolver([]);
+      expect(() => {
+        resolver.getByName('foo');
+      }).to.throw(Error, 'Could not resolve site for name foo');
+    });
+
+    it('should throw when there is no appropriate site info', () => {
+      const resolver = new SiteResolver([
+        { hostName: 'bar.com', language: '', name: 'bar' },
+        { hostName: 'var.com', language: '', name: 'var' },
+      ]);
+      expect(() => {
+        resolver.getByName('foo');
+      }).to.throw(Error, 'Could not resolve site for name foo');
+    });
+
+    it('should return site', () => {
+      const resolver = new SiteResolver([
+        { hostName: 'bar.net', language: '', name: 'bar' },
+        { hostName: 'foo.com', language: '', name: 'foo' },
+        { hostName: 'var.com', language: '', name: 'var' },
+      ]);
+      const site = resolver.getByName('foo');
+
+      expect(site).to.not.be.undefined;
+      expect(site?.hostName).to.equal('foo.com');
+    });
+
+    it('should be case insensitive', () => {
+      const resolver = new SiteResolver([{ hostName: 'foo.com', language: '', name: 'foo' }]);
+      const site = resolver.getByName('Foo');
+
+      expect(site).to.not.be.undefined;
+      expect(site?.hostName).to.equal('foo.com');
     });
   });
 });
--- packages/sitecore-jss/src/site/site-resolver.ts
+++ packages/sitecore-jss/src/site/site-resolver.ts
@@ -4,33 +4,60 @@ import { SiteInfo } from './graphql-siteinfo-service';
 const DELIMITERS = /\||,|;/g;
 
 /**
- * Determines site based on the provided host name
+ * Resolves site based on the provided host or site name
  */
 export class SiteResolver {
   /**
+   * @param {SiteInfo[]} sites Array of sites to be used in resolution
+   */
+  constructor(readonly sites: SiteInfo[]) {}
+
+  /**
    * Resolve site by host name
    * @param {string} hostName the host name
-   * @param {SiteInfo[]} sites the list of available sites
    * @returns {SiteInfo} the resolved site
+   * @throws {Error} if a matching site is not found
    */
-  static resolve = (hostName: string, sites: SiteInfo[]): SiteInfo | undefined => {
-    const siteInfo = sites.find((info) => {
+  public getByHost = (hostName: string): SiteInfo => {
+    const siteInfo = this.sites.find((info) => {
       const hostnames = info.hostName.replace(/\s/g, '').split(DELIMITERS);
 
       return hostnames.some(
-        (hostname) => hostName === hostname || SiteResolver.matchesPattern(hostName, hostname)
+        (hostname) => hostName === hostname || this.matchesPattern(hostName, hostname)
       );
     });
+
+    if (!siteInfo) {
+      throw new Error(`Could not resolve site for host ${hostName}`);
+    }
 
     return siteInfo;
   };
 
-  private static matchesPattern(hostname: string, pattern: string): boolean {
+  /**
+   * Resolve site by site name
+   * @param {string} siteName the site name
+   * @returns {SiteInfo} the resolved site
+   * @throws {Error} if a matching site is not found
+   */
+  public getByName = (siteName: string): SiteInfo => {
+    const siteInfo = this.sites.find(
+      (info) => info.name.toLocaleLowerCase() === siteName.toLocaleLowerCase()
+    );
+
+    if (!siteInfo) {
+      throw new Error(`Could not resolve site for name ${siteName}`);
+    }
+
+    return siteInfo;
+  };
+
+  protected matchesPattern(hostname: string, pattern: string): boolean {
     // dots should be treated as chars
     // stars should be treated as wildcards
     const regExpPattern = pattern.replace(/\./g, '\\.').replace(/\*/g, '.*');
 
-    const regExp = new RegExp(`^${regExpPattern}$`, 'g');
+    const regExp = new RegExp(`^${regExpPattern}$`, 'gi');
 
     return !!hostname.match(regExp);
   }
