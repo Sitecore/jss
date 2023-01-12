a8965e959b7502eac7ffceaafe62485b0ef618ee
Addy Pathania <aditya.pathania@sitecore.com>
2023-01-12 16:22:34 -0500

    fixed sitemap fetcher

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/sitemap-fetcher/plugins/graphql-sitemap-service.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/sitemap-fetcher/plugins/graphql-sitemap-service.ts
@@ -2,24 +2,21 @@
   GraphQLSitemapService,
   StaticPath,
   constants,
-  siteInfo,
+  SiteInfo,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import config from 'temp/config';
 import { SitemapFetcherPlugin } from '..';
 import { GetStaticPathsContext } from 'next';
-import siteResolver from 'lib/site-resolver';
-
+import { siteResolver } from 'lib/site-resolver';
 class GraphqlSitemapServicePlugin implements SitemapFetcherPlugin {
   _graphqlSitemapService: GraphQLSitemapService;
-
   constructor() {
     this._graphqlSitemapService = new GraphQLSitemapService({
       endpoint: config.graphQLEndpoint,
       apiKey: config.sitecoreApiKey,
-      sites: siteResolver.sites.map((site: siteInfo) => site.name),
+      sites: siteResolver.sites.map((site: SiteInfo) => site.name),
     });
   }
-
   async exec(context?: GetStaticPathsContext): Promise<StaticPath[]> {
     if (process.env.EXPORT_MODE) {
       // Disconnected Export mode
@@ -30,5 +27,4 @@ class GraphqlSitemapServicePlugin implements SitemapFetcherPlugin {
     return this._graphqlSitemapService.fetchSSGSitemap(context?.locales || []);
   }
 }
-
 export const graphqlSitemapServicePlugin = new GraphqlSitemapServicePlugin();
