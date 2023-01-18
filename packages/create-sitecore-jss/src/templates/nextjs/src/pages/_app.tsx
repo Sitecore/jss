a6c21d7c23b23dd7c2758ad018fdf4577dce59da
Addy Pathania <aditya.pathania@sitecore.com>
2023-01-18 01:13:41 -0500

    imports assets dynamically

--- packages/create-sitecore-jss/src/templates/nextjs/src/pages/_app.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs/src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import type { AppProps } from 'next/app';
 import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
+import { useEffect } from 'react';
+import dynamic from 'next/dynamic';
 
-import 'assets/app.css';
+// import 'assets/main.scss';
+
+const DynamicStyles = dynamic(
+  ({ projectName }: {projectName: string}) => import(`../assets/projects/${projectName}/main.scss`),
+  { ssr: false }
+);
 
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
-  const { dictionary, ...rest } = pageProps;
+  const { dictionary, site, ...rest } = pageProps;
+  const projectName = site?.project;
+
+  // Solution-1 import css files dynamically based on the project name
+  useEffect(() => {
+    DynamicStyles(projectName);
+  }, [projectName]);
 
   return (
     // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
@@ -18,3 +31,27 @@ function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element
 }
 
 export default App;
+
+// function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
+//   const { dictionary, site, ...rest } = pageProps;
+//   const projectName = site?.project;
+
+// Solution-2 import css files dynamically based on the project name
+// useEffect(() => {
+//   (async () => {
+//     await import(`../assets/projects/${projectName}/main.scss`);
+//     // : await import(`assets/main.scss`);
+//   })();
+// }, [projectName]);
+
+//   return (
+//     // Use the next-localization (w/ rosetta) library to provide our translation dictionary to the app.
+//     // Note Next.js does not (currently) provide anything for translation, only i18n routing.
+//     // If your app is not multilingual, next-localization and references to it can be removed.
+//     <I18nProvider lngDict={dictionary} locale={pageProps.locale}>
+//       <Component {...rest} />
+//     </I18nProvider>
+//   );
+// }
+
+// export default App;
