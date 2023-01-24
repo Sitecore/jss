620c09c007fa80b1dc9000f37e6e675f3e73b4e2
Addy Pathania <aditya.pathania@sitecore.com>
2023-01-24 14:01:08 -0500

    fix lint errors

--- packages/create-sitecore-jss/src/templates/vue/.eslintrc.js
+++ packages/create-sitecore-jss/src/templates/vue/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/multi-word-component-names': 'off',
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
