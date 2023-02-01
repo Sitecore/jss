54efbc5f8af5bf8964eb47cf8c4e456a4d917854
Ruslan Matkovskyi <rusm@sitecore.net>
2023-02-01 12:12:59 +0100

    #hotfix-nextjs-sxa: added new line and fix container.tsx

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
@@ -28,11 +28,8 @@ const DefaultContainer = (props: ComponentProps): JSX.Element => {
   if (backgroundImage) {
     const prefix = `${sitecoreContext.pageState !== 'normal' ? '/sitecore/shell' : ''}/-/media/`;
     backgroundImage = `${backgroundImage?.match(BACKGROUND_REG_EXP)?.pop()?.replace(/-/gi, '')}`;
-
-    // TODO: here uses ${config.sitecoreApiHost} it's temporary solutions - it will be removed when be fix bug - UNABLE_TO_VERIFY_LEAF_SIGNATUE
-    // TODO: this fix https://doc.sitecore.com/xp/en/developers/hd/200/sitecore-headless-development/walkthrough--configuring-sitecore-ca-certificates-for-node-js.html doesn't help
     backgroundStyle = {
-      backgroundImage: `url('${config.sitecoreApiHost}${prefix}${backgroundImage}')`,
+      backgroundImage: `url('${prefix}${backgroundImage}')`,
     };
   }
 
