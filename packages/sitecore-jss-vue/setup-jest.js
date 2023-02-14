2ed3eb1363d3c05098ac391499d28b9ef3ac7db3
Addy Pathania <aditya.pathania@sitecore.com>
2023-02-14 18:51:24 -0500

    refactor setup file

--- packages/sitecore-jss-vue/setup-jest.js
+++ packages/sitecore-jss-vue/setup-jest.js
@@ -1,4 +1,20 @@
-﻿global.console = {
-  ...console,
-  warn: jest.fn(),
-};
+﻿const originalConsoleWarn = console.warn;
+
+beforeAll(() => {
+  // Spy on console.warn to check for specific warning message
+  jest.spyOn(console, 'warn').mockImplementation((message) => {
+    if (
+      message ===
+        '[Vue warn]: Invalid prop: type check failed for prop "componentFactory". Expected Object, got Function' ||
+      '[Vue warn]: Invalid prop: type check failed for prop "field". Expected Object, got Null'
+    ) {
+      return;
+    }
+    originalConsoleWarn(message);
+  });
+});
+
+afterAll(() => {
+  // Restore original console.warn implementation
+  jest.spyOn(console, 'warn').mockRestore();
+});
