6da3498f586f43683ad7bf897640857bec5654d5
illiakovalenko <illia.kovalenko@sitecore.com>
2023-02-21 13:32:53 +0200

    Add comment

--- packages/create-sitecore-jss/src/templates/nextjs-personalize/src/lib/pos-resolver.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-personalize/src/lib/pos-resolver.ts
@@ -1,4 +1,9 @@
 import { PosResolver } from '@sitecore-jss/sitecore-jss-nextjs/middleware';
 import { siteResolver } from './site-resolver';
 
+/*
+  The pos resolver stores sites point of sales
+*/
+
+/** PosResolver singleton */
 export const posResolver = new PosResolver({ getSite: siteResolver.getByName });
