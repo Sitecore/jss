27608ec3704077f364d6fe59d66052ea7122320c
Artem Alexeyenko <ala@sitecore.net>
2023-03-03 14:32:18 -0500

    [tentative] work around axios v1+react-scripts v5 incompatibility

--- packages/create-sitecore-jss/src/templates/react/config-overrides.js
+++ packages/create-sitecore-jss/src/templates/react/config-overrides.js
@@ -10,5 +10,24 @@ module.exports = function override(config) {
     (p) => p.constructor.name !== 'ModuleScopePlugin'
   );
 
+  const fileLoaderRule = getFileLoaderRule(config.module.rules);
+  if(!fileLoaderRule) {
+    throw new Error("File loader not found");
+  }
+  fileLoaderRule.exclude.push(/\.cjs$/);
+
   return config;
 };
+
+function getFileLoaderRule(rules) {
+  for(const rule of rules) {
+      if("oneOf" in rule) {
+          const found = getFileLoaderRule(rule.oneOf);
+          if(found) {
+              return found;
+          }
+      } else if(rule.test === undefined && rule.type === 'asset/resource') {
+          return rule;
+      }
+  }
+}
