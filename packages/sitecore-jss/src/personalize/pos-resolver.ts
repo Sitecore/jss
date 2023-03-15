326e6a6091c251a6f3b030e9de237e936f216f64
Artem Alexeyenko <ala@sitecore.net>
2023-03-15 15:31:34 -0400

    [nextjs] prefer wildcard fallback for point of sale resolution (#1380)

--- packages/sitecore-jss/src/personalize/pos-resolver.ts
+++ packages/sitecore-jss/src/personalize/pos-resolver.ts
@@ -3,7 +3,7 @@ import { SiteInfo } from '../site';
 export class PosResolver {
   static resolve = (site: SiteInfo, language: string) => {
     return site.pointOfSale
-      ? site.pointOfSale[language] || site.pointOfSale[site.language] || site.pointOfSale['*']
+      ? site.pointOfSale[language] || site.pointOfSale['*'] || site.pointOfSale[site.language]
       : '';
   };
 }
