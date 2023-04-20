fc8f5b47da5150970bb808ef5f51109ae71369c1
illiakovalenko <illia.kovalenko@sitecore.com>
2023-04-20 19:56:56 +0300

    [Next.js][Multi-project] Resolve project styles

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/page-props-factory/plugins/project.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/page-props-factory/plugins/project.ts
@@ -1,3 +1,4 @@
+import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs/utils';
 import { SitecorePageProps } from 'lib/page-props';
 import chalk from 'chalk';
 import fs from 'fs';
@@ -7,16 +8,26 @@ class ProjectPlugin implements Plugin {
   order = 1;
 
   async exec(props: SitecorePageProps) {
-    if (
-      process.env.NODE_ENV === 'development' &&
-      props.site.project &&
-      !fs.existsSync(`${process.cwd()}/src/projects/${props.site.project}`)
-    ) {
+    const isProjectImplemented =
+      props.site.project && fs.existsSync(`${process.cwd()}/src/projects/${props.site.project}`);
+
+    if (process.env.NODE_ENV === 'development' && isProjectImplemented) {
       console.log(
         chalk.yellow(
-          `Project ${props.site.project} is not added to the app. Shared layout, components are used instead`
+          `Project ${props.site.project} is not implemented. Shared layout, components are used instead`
         )
       );
+    }
+
+    if (isProjectImplemented) {
+      const stylesPath = `${process.cwd()}/public/projects/${props.site.project}/index.css`;
+
+      if (fs.existsSync(stylesPath)) {
+        props.headLinks.push({
+          rel: 'stylesheet',
+          href: `${getPublicUrl()}/projects/${props.site.project}/index.css`,
+        });
+      }
     }
 
     return props;
