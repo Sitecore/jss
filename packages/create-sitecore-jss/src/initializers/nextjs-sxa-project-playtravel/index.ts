5549e8d68d3b2cadc02dee05a8254920914deaf5
Addy Pathania <aditya.pathania@sitecore.com>
2023-04-27 15:02:01 -0400

    added play-travel add-on

---
+++ packages/create-sitecore-jss/src/initializers/nextjs-sxa-project-playtravel/index.ts
@@ -0,0 +1,40 @@
+﻿import path, { sep } from 'path';
+import {
+  Initializer,
+  openPackageJson,
+  transform,
+  DEFAULT_APPNAME,
+  ClientAppArgs,
+  missingAddonMsg,
+} from '../../common';
+
+export default class NextjsSxaProjectPlayTravel implements Initializer {
+  get isBase(): boolean {
+    return false;
+  }
+
+  async init(args: ClientAppArgs) {
+    const pkg = openPackageJson(`${args.destination}${sep}package.json`);
+
+    const mergedArgs = {
+      ...args,
+      appName: args.appName || pkg?.config?.appName || DEFAULT_APPNAME,
+      appPrefix: args.appPrefix || pkg?.config?.prefix || false,
+    };
+
+    const templatePath = path.resolve(__dirname, '../../templates/nextjs-sxa-project-playtravel');
+
+    await transform(templatePath, mergedArgs);
+
+    if (!args.templates.includes('nextjs-sxa') && !pkg.config?.templates?.includes('nextjs-sxa')) {
+      console.log(missingAddonMsg('nextjs-sxa-project-playtravel', 'nextjs-sxa'));
+    }
+
+    const response = {
+      nextSteps: [],
+      appName: mergedArgs.appName,
+    };
+
+    return response;
+  }
+}
