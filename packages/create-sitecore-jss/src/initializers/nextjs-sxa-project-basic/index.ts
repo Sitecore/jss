6a425edb55e05413501d24d0c7327e982cbaa16c
Addy Pathania <aditya.pathania@sitecore.com>
2023-04-28 17:51:33 -0400

    add basic project

---
+++ packages/create-sitecore-jss/src/initializers/nextjs-sxa-project-basic/index.ts
@@ -0,0 +1,35 @@
+﻿﻿import path, { sep } from 'path';
+import {
+  Initializer,
+  openPackageJson,
+  transform,
+  DEFAULT_APPNAME,
+  ClientAppArgs,
+} from '../../common';
+
+export default class NextjsSxaProjectBasic implements Initializer {
+  get isBase(): boolean {
+    return false;
+  }
+
+  async init(args: ClientAppArgs) {
+    const pkg = openPackageJson(`${args.destination}${sep}package.json`);
+
+    const mergedArgs = {
+      ...args,
+      appName: args.appName || pkg?.config?.appName || DEFAULT_APPNAME,
+      appPrefix: args.appPrefix || pkg?.config?.prefix || false,
+    };
+
+    const templatePath = path.resolve(__dirname, '../../templates/nextjs-sxa-project-basic');
+
+    await transform(templatePath, mergedArgs);
+
+    const response = {
+      nextSteps: [],
+      appName: mergedArgs.appName,
+    };
+
+    return response;
+  }
+}
