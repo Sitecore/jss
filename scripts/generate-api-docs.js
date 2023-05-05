261ec0862a28f3fe49698aff8477d95fce1bf270
illiakovalenko <illia.kovalenko@sitecore.com>
2023-05-05 10:49:12 +0300

    Update

--- scripts/generate-api-docs.js
+++ scripts/generate-api-docs.js
@@ -1,5 +1,9 @@
 const { execSync } = require('child_process');
 
+execSync('git config --global user.email "builds@sitecore.com"', { stdio: 'inherit' });
+execSync('git config --global user.name "Automated Build"', { stdio: 'inherit' });
+
 execSync('npx lerna run generate-docs --since=origin/dev', { stdio: 'inherit' });
 execSync('git add .', { stdio: 'inherit' });
-execSync('git commit -m "Update API docs"', { stdio: 'inherit' });
+execSync('git commit -m "Update API docs [skip CI]"', { stdio: 'inherit' });
+execSync('git push', { stdio: 'inherit' });
