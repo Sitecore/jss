9bdefda4bae08bf8aae47d0d7ffdcbd3ba8bc7b4
Addy Pathania <aditya.pathania@sitecore.com>
2023-05-09 17:55:29 -0400

    add chalk to vue

--- packages/create-sitecore-jss/src/templates/angular/scripts/install-pre-push-hook.ts
+++ packages/create-sitecore-jss/src/templates/angular/scripts/install-pre-push-hook.ts
@@ -2,14 +2,16 @@
 import * as path from 'path';
 import { promisify } from 'util';
 import { exec } from 'child_process';
-import chalk from 'chalk';
+
+const greenLog = '\x1b[32m%s\x1b[0m';
+const redLog = '\x1b[31m%o\x1b[0m';
 
 const installHooks = async () => {
   try {
     const appPath = path.join(__dirname, '..').replace(/\\/g, '/');
     const { stdout } = await promisify(exec)('git rev-parse --show-toplevel');
     const gitRootPath = stdout.trim();
-    console.log(chalk.green(`Writing data to local .git folder ${gitRootPath}...`));
+    console.log(greenLog, 'Writing data to local .git folder...');
 
     const data = `#!/bin/sh
 #
@@ -17,15 +19,16 @@ const installHooks = async () => {
 #
 # To skip this hook, use the --no-verify flag
 # when pushing.
-#
 echo "Running lint check..."
 cd ${appPath}
-npm run lint`;
+npm run lint
+#
+  `;
 
     await promisify(fs.writeFile)(`${gitRootPath}/.git/hooks/pre-push`, data, 'utf8');
-    console.log(chalk.green('Success'));
+    console.log(greenLog, 'Success!');
   } catch (error) {
-    console.log(chalk.red(`Error installing hook: ${error}`));
+    console.log(redLog, `Error installing hook: ${error}`);
   }
 };
 
