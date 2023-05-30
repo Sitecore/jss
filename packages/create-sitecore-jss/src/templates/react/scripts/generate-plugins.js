fc94084104ec49ab54bc8f259097424bfde2bbf3
illiakovalenko <illia.kovalenko@sitecore.com>
2023-05-30 15:34:39 +0300

    Add comment

--- packages/create-sitecore-jss/src/templates/react/scripts/generate-plugins.js
+++ packages/create-sitecore-jss/src/templates/react/scripts/generate-plugins.js
@@ -1,5 +1,14 @@
 ﻿const { writePlugins, ModuleType } = require('@sitecore-jss/sitecore-jss-dev-tools');
 
+/*
+  PLUGINS GENERATION
+
+  Generating the plugins is optional, and it can be maintained manually if preferred.
+
+  The default convention uses the plugin's filename (without the extension) as the first part of the component
+  name. For example, the file `/lib/page-props-factory/plugins/exampleName.ts` would map to plugin `exampleNamePlugin`.
+*/
+
 const pluginDefinitions = [
   {
     listPath: 'scripts/temp/component-builder-plugins.js',
