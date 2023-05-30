9f087a840744cd89272a3ddc9cd95a3a57113414
illiakovalenko <illia.kovalenko@sitecore.com>
2023-05-30 17:21:43 +0300

    Add comments

--- packages/sitecore-jss-react/src/ComponentBuilder.ts
+++ packages/sitecore-jss-react/src/ComponentBuilder.ts
@@ -2,7 +2,14 @@ import { ComponentType } from 'react';
 import { ComponentBuilderBase } from '@sitecore-jss/sitecore-jss';
 import { ComponentFactory } from './components/sharedTypes';
 
+/**
+ * React implementation of component builder
+ */
 export class ComponentBuilder extends ComponentBuilderBase<ComponentType> {
+  /**
+   * Creates a new instance of component factory
+   * @returns {ComponentFactory} Component factory implementation
+   */
   getComponentFactory(): ComponentFactory {
     return (componentName: string) => {
       const component = this.components.get(componentName);
--- packages/sitecore-jss/src/component-builder.ts
+++ packages/sitecore-jss/src/component-builder.ts
@@ -1,13 +1,29 @@
+/**
+ * Configuration for ComponentBuilderBase
+ */
 export type ComponentBuilderBaseConfig<ComponentType> = {
+  /**
+   * List of components to be stored
+   */
   components: Map<string, ComponentType>;
 };
 
+/**
+ * ComponentBuilder is a abstract base class for building components based on the configuration.
+ * Base implementation requires componentFactory to be implemented.
+ */
 export abstract class ComponentBuilderBase<ComponentType> {
+  /**
+   * List of components to be stored
+   */
   protected components: Map<string, ComponentType>;
 
   constructor(protected config: ComponentBuilderBaseConfig<ComponentType>) {
     this.components = new Map([...config.components]);
   }
 
-  protected abstract getComponentFactory(componentName: string): unknown;
+  /**
+   * Component factory implementation
+   */
+  protected abstract getComponentFactory(): unknown;
 }
