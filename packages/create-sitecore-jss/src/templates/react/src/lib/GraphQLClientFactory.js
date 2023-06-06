5fd343a40552a4530620d684f80fe817b589e4bb
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2023-06-06 15:27:16 -0400

    [RAV] Remove persisted query link (#1512)
    * use batch link for graphql queries
    
    * update changlog
    
    * update changelog, rmeove extra depedencies

--- packages/create-sitecore-jss/src/templates/react/src/lib/GraphQLClientFactory.js
+++ packages/create-sitecore-jss/src/templates/react/src/lib/GraphQLClientFactory.js
@@ -2,7 +2,6 @@
 
 import 'cross-fetch/polyfill';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { sha256 } from 'js-sha256';
 
 /* eslint-disable import/order */
 
@@ -28,28 +27,22 @@ import introspectionQueryResultData from '../temp/GraphQLFragmentTypes.json';
 // ...or a batched link (multiple queries within 10ms all go in one HTTP request)
 import { BatchHttpLink } from '@apollo/client/link/batch-http';
 
-// ...and an automatic persisted query link, which reduces bandwidth by using query hashes to alias content
-// the APQ link is _chained_ behind another link that performs the actual HTTP calls, so you can choose
-// APQ + batched, or APQ + http links for example.
-import { createPersistedQueryLink } from '@apollo/client/link/persisted-queries';
 import config from '../temp/config';
 
-export default function (endpoint, ssr, initialCacheState) {
+export default function(endpoint, ssr, initialCacheState) {
   /* HTTP link selection: default to batched + APQ */
-  const link = createPersistedQueryLink({ sha256 }).concat(
-    new BatchHttpLink({
-      uri: endpoint,
-      headers: {
-        connection: 'keep-alive',
-        sc_apikey: config.sitecoreApiKey,
-      },
-    })
-  );
+  const link = new BatchHttpLink({
+    uri: endpoint,
+    headers: {
+      connection: 'keep-alive',
+      sc_apikey: config.sitecoreApiKey,
+    },
+  });
 
   const possibleTypes = {};
 
-  introspectionQueryResultData.__schema.types.forEach((supertype) => {
-    possibleTypes[supertype.name] = supertype.possibleTypes.map((subtype) => subtype.name);
+  introspectionQueryResultData.__schema.types.forEach(supertype => {
+    possibleTypes[supertype.name] = supertype.possibleTypes.map(subtype => subtype.name);
   });
 
   const cache = new InMemoryCache({
