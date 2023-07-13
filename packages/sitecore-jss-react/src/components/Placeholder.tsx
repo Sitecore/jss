f25d0851f4b6912d6b2468fb583bf658c00e6600
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2023-07-13 14:44:19 -0400

    [nextjs] Fix double placeholder in Experience Editor in production mode (#1557)
    * fix double placeholder in EE
    
    * update changelog

--- packages/sitecore-jss-react/src/components/Placeholder.tsx
+++ packages/sitecore-jss-react/src/components/Placeholder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { PlaceholderCommon, PlaceholderProps } from './PlaceholderCommon';
 import { withComponentFactory } from '../enhancers/withComponentFactory';
 import { ComponentRendering, HtmlElementRendering } from '@sitecore-jss/sitecore-jss/layout';
-import { HorizonEditor, ExperienceEditor } from '@sitecore-jss/sitecore-jss/utils';
+import { HorizonEditor } from '@sitecore-jss/sitecore-jss/utils';
 
 export interface PlaceholderComponentProps extends PlaceholderProps {
   /**
@@ -90,43 +90,11 @@ class PlaceholderComponent extends PlaceholderCommon<PlaceholderComponentProps> 
       this.props.name
     );
 
+    const components = this.getComponentsForRenderingData(placeholderData);
+
     this.isEmpty = placeholderData.every((rendering: ComponentRendering | HtmlElementRendering) =>
       isRawRendering(rendering)
     );
-
-    // This is a workaround to insert Experience Editor-specific markup on the page for empty placeholders.
-    // It makes sure the markup is inserted by react and thus avoiding hydration errors when in editing mode
-    if (this.isEmpty && ExperienceEditor.isActive()) {
-      const codePos = placeholderData.findIndex(
-        (component) =>
-          (component as HtmlElementRendering).name === 'code' &&
-          (component as HtmlElementRendering).attributes.kind === 'open'
-      );
-      const plhId = (placeholderData[codePos] as HtmlElementRendering).attributes.id;
-      const emptyPlaceholderMarkup: HtmlElementRendering[] = [
-        {
-          name: 'span',
-          contents: null,
-          attributes: {
-            type: 'text/sitecore',
-            'sc-part-of': 'placeholder rendering',
-            style: 'display: none;',
-          },
-        },
-        {
-          name: 'div',
-          contents: null,
-          attributes: {
-            class: 'scEnabledChrome scEmptyPlaceholder',
-            'sc-part-of': 'placeholder',
-            'sc-placeholder-id': plhId,
-          },
-        },
-      ];
-      placeholderData.splice(codePos + 1, 0, ...emptyPlaceholderMarkup);
-    }
-
-    const components = this.getComponentsForRenderingData(placeholderData);
 
     if (this.props.renderEmpty && this.isEmpty) {
       const rendered = this.props.renderEmpty(components);
