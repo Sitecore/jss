e6c1ac0bebc2d3deed69a7ac0342e49da3ca94da
Addy Pathania <aditya.pathania@sitecore.com>
2023-08-21 14:44:56 -0400

    add plugin for cors

---
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/next-config/plugins/cors-header.js
@@ -0,0 +1,19 @@
+﻿const config = require('../../../temp/config');
+
+/**
+ * @param {import('next').NextConfig} nextConfig
+ */
+const corsHeaderPlugin = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    async headers() {
+      return [
+        {
+          source: '/_next/:path*',
+          headers: [{ key: 'Access-Control-Allow-Origin', value: config.sitecoreApiHost }],
+        },
+      ];
+    },
+  });
+};
+
+module.exports = corsHeaderPlugin;
