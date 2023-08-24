19e1654957962cfff47353170fb173811ce59df1
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2023-08-24 15:15:16 -0500

    add monorepo support for @sitecore-feaas/clientside/react (best we have for the moment)

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/next-config/plugins/monorepo.js
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/next-config/plugins/monorepo.js
@@ -1,12 +1,25 @@
+const path = require('path');
+const CWD = process.cwd();
+
 /**
  * @param {import('next').NextConfig} nextConfig
  */
 const monorepoPlugin = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
+    // Monorepo support for @sitecore-feaas/clientside/react (part 1)
+    experimental: {
+      esmExternals: false,
+    },
     webpack: (config, options) => {
       if (options.isServer) {
         config.externals = ['react', 'vertx', ...config.externals];
       }
+      // Monorepo support for @sitecore-feaas/clientside/react (part 2)
+      config.resolve.alias['@sitecore-feaas/clientside/react'] = path.resolve(
+        CWD, options.isServer ? 
+          './node_modules/@sitecore-feaas/clientside/dist/node/react.cjs' :
+          './node_modules/@sitecore-feaas/clientside/dist/browser/react.esm.js'
+      );
 
       // Overload the Webpack config if it was already overloaded
       if (typeof nextConfig.webpack === 'function') {
