310b07f5ca9242efa5f18f2973c1ab899fe296e1
Artem Alexeyenko <ala@sitecore.net>
2023-09-18 12:16:51 -0400

    WIP throttle approach

--- packages/sitecore-jss/src/layout/graphql-layout-service.ts
+++ packages/sitecore-jss/src/layout/graphql-layout-service.ts
@@ -2,6 +2,7 @@ import { LayoutServiceBase } from './layout-service';
 import { LayoutServiceData } from './models';
 import { GraphQLClient, GraphQLRequestClient } from '../graphql-request-client';
 import debug from '../debug';
+import pThrottle from 'p-throttle';
 
 export type GraphQLLayoutServiceConfig = {
   /**
@@ -47,6 +48,8 @@ export class GraphQLLayoutService extends LayoutServiceBase {
     this.graphQLClient = this.getGraphQLClient();
   }
 
+  private throttle = pThrottle({limit: 5, interval: 1000})
+
   /**
    * Fetch layout data for an item.
    * @param {string} itemPath item path to fetch layout data for.
@@ -73,6 +76,13 @@ export class GraphQLLayoutService extends LayoutServiceBase {
       }
     );
   }
+
+  throttledFetch = this.throttle(async (itemPath: string, language?: string) => {
+
+    const data = await this.fetchLayoutData(itemPath, language);
+
+    return data
+})
 
   /**
    * Gets a GraphQL client that can make requests to the API. Uses graphql-request as the default
