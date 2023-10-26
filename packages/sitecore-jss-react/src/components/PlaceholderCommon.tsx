38029def26486f1739c1f6d9f68f9e99f63e8d09
Ruslan Matkovskyi <rusm@sitecore.net>
2023-10-26 14:51:53 +0200

    #597453: [sitecore-jss-react] fixed rendering with two and more dynamic placeholders

--- packages/sitecore-jss-react/src/components/PlaceholderCommon.tsx
+++ packages/sitecore-jss-react/src/components/PlaceholderCommon.tsx
@@ -136,7 +136,8 @@ export class PlaceholderCommon<T extends PlaceholderProps> extends React.Compone
       Object.keys(rendering.placeholders).forEach((placeholder) => {
         if (
           placeholder.indexOf('{*}') !== -1 &&
-          !EXCLUDE_PLACEHOLDERS_RENDER.some((pattern) => name.search(pattern) !== -1)
+          !EXCLUDE_PLACEHOLDERS_RENDER.some((pattern) => name.search(pattern) !== -1) &&
+          name.replace(/[^a-zA-Z]*/gi, '') === placeholder.replace(/[^a-zA-Z]*/gi, '')
         ) {
           rendering.placeholders[name] = rendering.placeholders[placeholder];
           delete rendering.placeholders[placeholder];
