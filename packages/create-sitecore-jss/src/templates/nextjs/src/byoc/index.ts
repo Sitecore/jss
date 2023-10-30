fe176dda27952d250acdeab3bddeee0d4e007473
Artem Alexeyenko <ala@sitecore.net>
2023-10-30 10:17:15 -0400

    [nextjs] Update feaas clientside and adjust types in byoc bundlers (#1638)
    * [nextjs] Update feaas clientside and adjust types in byoc bundlers
    
    * reorg index file for better readability
    
    * use caret for feaas clientside dependency

--- packages/create-sitecore-jss/src/templates/nextjs/src/byoc/index.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/src/byoc/index.ts
@@ -1,11 +1,11 @@
-// This is an out-of-box bundler for External components (BYOC)
-// It enables registering components in client-only or SSR/hybrid contexts
-// It's recommended to not modify this file - please add BYOC imports in corresponding index.*.ts files instead
-
-// Import your client-only components via client-bundle. Nextjs's dynamic() call will ensure they are only rendered client-side
-import dynamic from 'next/dynamic';
 import * as FEAAS from '@sitecore-feaas/clientside/react';
+import dynamic from 'next/dynamic';
 import config from 'temp/config';
+/**
+ * This is an out-of-box bundler for External components (BYOC) (see Sitecore documentation for more details)
+ * It enables registering components in client-only or SSR/hybrid contexts
+ * It's recommended to not modify this file - please add BYOC imports in corresponding index.*.ts files instead
+ */
 
 // Set context properties to be available within BYOC components
 FEAAS.setContextProperties({
@@ -13,14 +13,15 @@ FEAAS.setContextProperties({
   sitecoreEdgeContextId: config.sitecoreEdgeContextId,
 });
 
+// Import your client-only components via client-bundle. Nextjs's dynamic() call will ensure they are only rendered client-side
 const ClientBundle = dynamic(() => import('./index.client'), {
   ssr: false,
 });
 
-// Import your SSR/hybrid components via index.hybrid.ts
+// Import your hybrid (server rendering with client hydration) components via index.hybrid.ts
 import './index.hybrid';
 
-// As long as client bundle is exported and rendered on page (as an empty element), client-only BYOC components are registered and become available
+// As long as component bundle is exported and rendered on page (as an empty element), client-only BYOC components are registered and become available
 // The rest of components will be regsitered in both server and client-side contexts when this module is imported into Layout
-export default ClientBundle;
- 
+FEAAS.enableNextClientsideComponents(dynamic, ClientBundle);
+export default FEAAS.ExternalComponentBundle;
