af76dbfd0dc99cf1745a5e99456a330293f40737
Addy Pathania <aditya.pathania@sitecore.com>
2023-11-02 09:24:05 -0400

    add revalidate submodule

---
+++ packages/sitecore-jss-nextjs/src/revalidate/revalidate-middleware.ts
@@ -0,0 +1,26 @@
+﻿import { NextApiResponse, NextApiRequest } from 'next';
+
+/**
+ * Middleware / handler for use in healthcheck Next.js API route (e.g. '/api/healthz').
+ */
+export class RevalidateMiddleware {
+  /**
+   * Gets the Next.js API route handler
+   * @returns route handler
+   */
+  public getHandler(): (req: NextApiRequest, res: NextApiResponse) => Promise<void> {
+    return this.handler;
+  }
+
+  private handler = async (_req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    try {
+      // await Promise.all(pathsToRevalidate.map((path) => res.revalidate(path)))
+      res.revalidate('/_site_my-basic-site/');
+      // console.log(`[api/revalidate] revalidated: ${paths.join(', ')}`);
+      return res.status(200).json({ revalidated: true });
+    } catch (error) {
+      console.error(`[api/revalidate] error: ${error}`);
+      return res.status(500).json({ message: 'Error revalidating' });
+    }
+  };
+}
