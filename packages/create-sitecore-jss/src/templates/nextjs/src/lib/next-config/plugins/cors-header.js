9e3d51e51a0c6cf9997a244ed8fbf4b4ae76827a
yavorsk <yavor.krastev@sitecore.com>
2023-11-03 16:39:52 +0200

    add CORS rule to enable api calls

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/next-config/plugins/cors-header.js
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/next-config/plugins/cors-header.js
@@ -9,11 +9,16 @@ const corsHeaderPlugin = (nextConfig = {}) => {
   }
   return Object.assign({}, nextConfig, {
     async headers() {
-      const extendHeaders = typeof nextConfig.headers === "function" ? await nextConfig.headers() : [];
+      const extendHeaders =
+        typeof nextConfig.headers === 'function' ? await nextConfig.headers() : [];
       return [
-        ...await extendHeaders,
+        ...(await extendHeaders),
         {
           source: '/_next/:path*',
+          headers: [{ key: 'Access-Control-Allow-Origin', value: config.sitecoreApiHost }],
+        },
+        {
+          source: '/api/:path*',
           headers: [{ key: 'Access-Control-Allow-Origin', value: config.sitecoreApiHost }],
         },
       ];
