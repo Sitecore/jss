47a5c7dc23f19b6e5760cd1382c1d4065abf61cd
illiakovalenko <illia.kovalenko@sitecore.com>
2023-11-07 18:49:46 +0200

    Updated jsdoc

--- packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/Bootstrap.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/Bootstrap.tsx
@@ -3,11 +3,15 @@ import { initContext } from 'src/lib/context';
 import { siteResolver } from 'lib/site-resolver';
 import config from 'temp/config';
 
+/**
+ * The Bootstrap component is the entry point for performing any initialization logic
+ * that needs to happen early in the application's lifecycle.
+ */
 const Bootstrap = (props: SitecorePageProps): JSX.Element | null => {
   const site = props.layoutData?.sitecore.context.site;
   const siteInfo = siteResolver.getByName(site?.name || config.siteName);
 
-  // Initialize the Context of the App
+  // Initialize the Context value for the app
   initContext({ siteName: siteInfo.name });
 
   return null;
--- packages/create-sitecore-jss/src/templates/nextjs/src/Bootstrap.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs/src/Bootstrap.tsx
@@ -1,5 +1,9 @@
 import { SitecorePageProps } from 'lib/page-props';
 
+/**
+ * The Bootstrap component is the entry point for performing any initialization logic
+ * that needs to happen early in the application's lifecycle.
+ */
 const Bootstrap = (_props: SitecorePageProps): JSX.Element | null => {
   return null;
 };
