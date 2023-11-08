f74ca444d23cd343746c24ea4d1aa8320633344b
Addy Pathania <aditya.pathania@sitecore.com>
2023-11-08 12:43:28 -0500

    add revalidate api route

---
+++ packages/create-sitecore-jss/src/templates/nextjs/src/pages/api/revalidate.ts
@@ -0,0 +1,23 @@
+﻿import { RevalidateMiddleware } from '@sitecore-jss/sitecore-jss-nextjs/revalidate';
+import { NextApiResponse, NextApiRequest } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { query } = req;
+  const secret = query['secret'];
+  const revalidateHandler = new RevalidateMiddleware().getHandler();
+
+  // check if the secret is provided, if not, allow the request to proceed without authentication
+  if (secret === undefined) {
+    console.log('Secret not provided. Proceeding without authentication.');
+    await revalidateHandler(req, res);
+  } else if (secret !== process.env.revalidatePathSecret) {
+    // secret is provided but doesn't match the environment variable, log a message and respond with an error
+    console.log('Invalid secret provided. Authentication failed.');
+    res.status(401).end('Invalid secret');
+  } else {
+    // secret is provided and matches the environment variable, proceed with authentication
+    await revalidateHandler(req, res);
+  }
+};
+
+export default handler;
--- packages/sitecore-jss-nextjs/src/revalidate/revalidate-middleware.ts
+++ packages/sitecore-jss-nextjs/src/revalidate/revalidate-middleware.ts
@@ -1,5 +1,11 @@
 ﻿import { NextApiResponse, NextApiRequest } from 'next';
 
+type Entity = {
+  identifier: string;
+  entity_definition: string;
+  operation: string;
+};
+
 /**
  * Middleware / handler for use in healthcheck Next.js API route (e.g. '/api/healthz').
  */
@@ -12,11 +18,30 @@ export class RevalidateMiddleware {
     return this.handler;
   }
 
-  private handler = async (_req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  private isEmpty(paths: string[]) {
+    return paths.length === 0;
+  }
+
+  private getPathsToRevalidate(req: NextApiRequest): string[] {
+    return req.body.updates
+      .map((x: Entity) => x.entity_definition === 'LayoutData' && x.identifier)
+      .filter(Boolean);
+  }
+
+  private handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+    const paths = this.getPathsToRevalidate(req);
+
+    if (this.isEmpty(paths)) {
+      return res.status(204).send({}); // nothing to revalidate
+    }
+
     try {
-      // await Promise.all(pathsToRevalidate.map((path) => res.revalidate(path)))
-      res.revalidate('/_site_my-basic-site/');
-      // console.log(`[api/revalidate] revalidated: ${paths.join(', ')}`);
+      if (req.body.multisite) {
+        await Promise.all(paths.map((path: string) => res.revalidate(`/_site_${path}`)));
+      } else {
+        await Promise.all(paths.map((path: string) => res.revalidate(`/${path}`)));
+      }
+      console.log(`[api/revalidate] revalidated: ${paths.join(', ')}`);
       return res.status(200).json({ revalidated: true });
     } catch (error) {
       console.error(`[api/revalidate] error: ${error}`);
