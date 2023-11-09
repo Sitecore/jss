66211a2e52127cd1c39b5dcbae45e87d36a47215
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2023-11-09 11:51:50 -0600

    Fix issue when prerendering 404/500 pages during build on XMC editing hosts

--- packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/Bootstrap.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/Bootstrap.tsx
@@ -1,6 +1,5 @@
 import { SitecorePageProps } from 'lib/page-props';
 import { context } from 'src/lib/context';
-import { siteResolver } from 'lib/site-resolver';
 import config from 'temp/config';
 
 /**
@@ -8,15 +7,12 @@ import config from 'temp/config';
  * that needs to happen early in the application's lifecycle.
  */
 const Bootstrap = (props: SitecorePageProps): JSX.Element | null => {
-  const site = props.layoutData?.sitecore.context.site;
-  const siteInfo = siteResolver.getByName(site?.name || config.siteName);
-
   /**
    * Initializes the application Context and associated Software Development Kits (SDKs).
    * This function is the entry point for setting up the application's context and any SDKs that are required for its proper functioning.
    * It prepares the resources needed to interact with various services and features within the application.
    */
-  context.init({ siteName: siteInfo.name });
+  context.init({ siteName: props.site?.name || config.siteName });
 
   return null;
 };
