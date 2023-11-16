ee02dca259327c154a10e080d17ef48d5c1ce7ae
yavorsk <yavor.krastev@sitecore.com>
2023-11-16 09:50:51 +0200

    small wording update in comment

--- packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/Navigation.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-styleguide/src/Navigation.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import { useI18n } from 'next-localization';
 import config from 'temp/config';
 
-// Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
-// If you're not supporting the Experience Editor, you can remove this.
+// Prefix public assets with a public URL to enable compatibility with Sitecore editors.
+// If you're not supporting Sitecore editors, you can remove this.
 const publicUrl = config.publicUrl;
 
 const Navigation = (): JSX.Element => {
