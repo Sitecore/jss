a4cfc9aacf00f6cc79af37edd183384eebfc889d
Adam Due Hansen <adamduehan@gmail.com>
2023-11-17 20:48:08 +0100

    Nextjs Placeholder uses the modifyComponentProps prop.

--- packages/sitecore-jss-nextjs/src/components/Placeholder.tsx
+++ packages/sitecore-jss-nextjs/src/components/Placeholder.tsx
@@ -4,21 +4,29 @@ import {
   PlaceholderComponentProps,
 } from '@sitecore-jss/sitecore-jss-react';
 import { ComponentPropsReactContext } from './ComponentPropsContext';
+import { ComponentProps } from '@sitecore-jss/sitecore-jss-react/types/components/PlaceholderCommon';
 
 export const Placeholder = (props: PlaceholderComponentProps) => {
   const componentPropsContext = useContext(ComponentPropsReactContext);
+
+  function getContextProps(initialProps: ComponentProps) {
+    if (!initialProps.rendering.uid) return initialProps;
+
+    const data = componentPropsContext[initialProps.rendering.uid] as {
+      [key: string]: unknown;
+    };
+
+    return data;
+  }
 
   return (
     <ReactPlaceholder
       {...props}
       modifyComponentProps={(initialProps) => {
-        if (!initialProps.rendering.uid) return initialProps;
-
-        const data = componentPropsContext[initialProps.rendering.uid] as {
-          [key: string]: unknown;
-        };
-
-        return { ...initialProps, ...data };
+        const finalProps = props.modifyComponentProps
+          ? props.modifyComponentProps(initialProps)
+          : initialProps;
+        return { ...finalProps, ...getContextProps(initialProps) };
       }}
     />
   );
