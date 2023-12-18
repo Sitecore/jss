876dae5041eb83c4583be732fd4f207e3fc1dccf
art-alexeyenko <undefined>
2023-12-18 15:57:49 -0500

    [packages/sitecore-jss-react] Combine fetched and datasource item props in BYOC and FEAAS (#1694)

--- packages/sitecore-jss-react/src/components/BYOCComponent.test.tsx
+++ packages/sitecore-jss-react/src/components/BYOCComponent.test.tsx
@@ -31,9 +31,102 @@ describe('BYOCComponent', () => {
     expect(fooComponent.find('#foo-content')).to.have.length(1);
   });
 
-  it('should render when props are prefetched', () => {
+  it('should use datasource fields when provided', () => {
+    const fields = {
+      prop1: {
+        value: 'value2',
+      },
+    };
+    const mockProps = {
+      params: {
+        ComponentName: 'Foo',
+      },
+      fetchedData: {},
+      fields,
+    };
+    const Foo = () => <p id="foo-content">Test</p>;
+    FEAAS.External.registerComponent(Foo, {
+      name: 'Foo',
+      properties: {
+        prop1: {
+          type: 'string',
+        },
+      },
+    });
+    const wrapper = mount(<BYOCComponent {...mockProps} />);
+    const fooComponent = wrapper.find('feaas-external');
+    expect(fooComponent).to.have.lengthOf(1);
+    expect(fooComponent.prop('prop1')).to.equal('value2');
+    expect(fooComponent.prop('data-external-id')).to.equal('Foo');
+    expect(fooComponent.find('#foo-content')).to.have.length(1);
+  });
+
+  it('should prefer ComponentProps over datasource fields', () => {
+    const fields = {
+      prop1: {
+        value: 'value2',
+      },
+    };
+    const mockProps = {
+      params: {
+        ComponentName: 'Foo',
+        ComponentProps: JSON.stringify({ prop1: 'value1' }),
+      },
+      fetchedData: {},
+      fields,
+    };
+    const Foo = () => <p id="foo-content">Test</p>;
+    FEAAS.External.registerComponent(Foo, {
+      name: 'Foo',
+      properties: {
+        prop1: {
+          type: 'string',
+        },
+      },
+    });
+    const wrapper = mount(<BYOCComponent {...mockProps} />);
+    const fooComponent = wrapper.find('feaas-external');
+    expect(fooComponent).to.have.lengthOf(1);
+    expect(fooComponent.prop('prop1')).to.equal('value1');
+    expect(fooComponent.prop('data-external-id')).to.equal('Foo');
+    expect(fooComponent.find('#foo-content')).to.have.length(1);
+  });
+
+  it('should combine ComponentProps and datasource fields', () => {
+    const fields = {
+      prop2: {
+        value: 'value2',
+      },
+    };
+    const mockProps = {
+      params: {
+        ComponentName: 'Foo',
+        ComponentProps: JSON.stringify({ prop1: 'value1' }),
+      },
+      fetchedData: {},
+      fields,
+    };
+    const Foo = () => <p id="foo-content">Test</p>;
+    FEAAS.External.registerComponent(Foo, {
+      name: 'Foo',
+      properties: {
+        prop1: {
+          type: 'string',
+        },
+      },
+    });
+    const wrapper = mount(<BYOCComponent {...mockProps} />);
+    const fooComponent = wrapper.find('feaas-external');
+    expect(fooComponent).to.have.lengthOf(1);
+    expect(fooComponent.prop('prop1')).to.equal('value1');
+    expect(fooComponent.prop('prop2')).to.equal('value2');
+    expect(fooComponent.prop('data-external-id')).to.equal('Foo');
+    expect(fooComponent.find('#foo-content')).to.have.length(1);
+  });
+
+  it('should render with static and fetched props when props are prefetched', () => {
     const fetchedData = {
-      prop1: 'prefetched_value1',
+      prop2: 'prefetched_value1',
     };
     const mockProps = {
       params: {
@@ -54,7 +147,8 @@ describe('BYOCComponent', () => {
     const wrapper = mount(<BYOCComponent {...mockProps} />);
     const fooComponent = wrapper.find('feaas-external');
     expect(fooComponent).to.have.lengthOf(1);
-    expect(fooComponent.prop('prop1')).to.equal('prefetched_value1');
+    expect(fooComponent.prop('prop1')).to.equal('value1');
+    expect(fooComponent.prop('datasources')).to.equal('{"prop2":"prefetched_value1","_":{}}');
     expect(fooComponent.prop('data-external-id')).to.equal('Foo');
     expect(fooComponent.find('#foo-content')).to.have.length(1);
   });
