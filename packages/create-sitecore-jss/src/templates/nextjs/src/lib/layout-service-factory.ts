fafc75ef19b3a6bc411ccf2e3c8e2ebe46fd951d
Addy Pathania <aditya.pathania@sitecore.com>
2024-02-09 14:32:06 -0500

    rephrase services comment

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/layout-service-factory.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/layout-service-factory.ts
@@ -21,21 +21,14 @@ export class LayoutServiceFactory {
           siteName,
           clientFactory,
           /*
-            GraphQL endpoint may reach its rate limit with the amount of Layout and Dictionary requests it receives and throw a rate limit error.
+            GraphQL endpoint may reach its rate limit with the amount of requests it receives and throw a rate limit error.
             GraphQL Dictionary and Layout Services can handle rate limit errors from server and attempt a retry on requests.
-            For this, specify the number of retries the GraphQL client will attempt.
+            For this, specify the number of 'retries' the GraphQL client will attempt.
             It will only try the request once by default.
-            retries: 'number'
-            Additionally, you have the flexibility to customize the retry strategy by passing your customized RetryStrategy
-            object using the DefaultRetryStrategy class. The DefaultRetryStrategy class, which can be imported from the @sitecore-jss-nextjs
-            package, provides two essential methods: 'shouldRetry' which returns a boolean indicating whether a retry should
-            be attempted, and 'getDelay' which calculates the delay (in milliseconds) before the subsequent retry based on
-            the encountered error and the current attempt.
-            Example:
-            retryStrategy: new DefaultRetryStrategy({
-              statusCodes: 'number[]',
-              factor: 'number' (The exponential factor to calculate backoff-time),
-            }),
+
+            Additionally, you have the flexibility to customize the retry strategy by passing a 'retryStrategy'.
+            By default it uses the `DefaultRetryStrategy` with exponential back-off factor of 2 for error codes 429,
+            502, 503, 504, 520, 521, 522, 523, and 524. You can use this class or your own implementation of `RetryStrategy`.
           */
           retries:
             (process.env.GRAPH_QL_SERVICE_RETRIES &&
