5a4825d52aa15d2f0ad9d3608fd73649271b64b4
Addy Pathania <aditya.pathania@sitecore.com>
2024-02-15 12:24:01 -0500

    export ClientError

--- packages/sitecore-jss-angular/src/public_api.ts
+++ packages/sitecore-jss-angular/src/public_api.ts
@@ -57,7 +57,13 @@ export {
   ComponentParams,
 } from '@sitecore-jss/sitecore-jss/layout';
 export { RetryStrategy, DefaultRetryStrategy } from '@sitecore-jss/sitecore-jss/graphql';
-export { constants, HttpDataFetcher, HttpResponse, enableDebug } from '@sitecore-jss/sitecore-jss';
+export {
+  constants,
+  HttpDataFetcher,
+  HttpResponse,
+  enableDebug,
+  ClientError,
+} from '@sitecore-jss/sitecore-jss';
 export {
   isServer,
   isEditorActive,
