d7bbf9cc0ebf7fb99174209928dad7900ce1af54
illiakovalenko <illia.kovalenko@sitecore.com>
2024-02-20 13:42:13 +0200

    Added jsdoc

--- packages/sitecore-jss-angular/src/services/jss-can-activate-error.ts
+++ packages/sitecore-jss-angular/src/services/jss-can-activate-error.ts
@@ -1,5 +1,8 @@
 import { UrlTree } from '@angular/router';
 
+/**
+ * Error thrown when a JssCanActivate guard fails to resolve.
+ */
 export class JssCanActivateError extends Error {
   constructor(public message: string, public redirectValue: string | string[] | UrlTree) {
     super(message);
