1f83db2c3a330ac44ab6ceded822a354e3721203
art-alexeyenko <art.alexeyenko@gmail.com>
2024-03-04 16:50:44 -0500

    adjust types in img override

--- packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/byoc/index.feaas.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/byoc/index.feaas.tsx
@@ -3,12 +3,18 @@ import * as FEAAS from '@sitecore-feaas/clientside/react';
 
 // Element implementations for Sitecore Component Builder can be overriden here
 
+// type to adjust HTMLAttributes to nextjs Image attributes
+type ImageProps = {
+  src: string;
+  alt: string;
+} & React.HTMLAttributes<HTMLImageElement>;
+
 // Register next Image to be used in Component Builder.
 // Nextjs image implementation will be used when img is rendered in component from Component Builder
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-FEAAS.setElementImplementation('img', (attributes: any) => {
-  delete attributes.children;
-  return <Image {...attributes} />;
+FEAAS.setElementImplementation('img', (attributes: ImageProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { children, ...imgAttributes } = attributes;
+  return <Image height={1920} width={1200} {...imgAttributes} />;
 });
 
 // eslint-disable-next-line import/no-anonymous-default-export
