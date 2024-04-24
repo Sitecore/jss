8ca899aed31e0843c86d68e6ed2f790369a68c0e
Addy Pathania <aditya.pathania@sitecore.com>
2024-04-24 17:33:42 -0400

    fix nextjs placeholder

--- packages/sitecore-jss-nextjs/src/components/EditingComponentPlaceholder.tsx
+++ packages/sitecore-jss-nextjs/src/components/EditingComponentPlaceholder.tsx
@@ -5,13 +5,21 @@ import {
   RouteData,
 } from '@sitecore-jss/sitecore-jss/layout';
 import { Placeholder } from './Placeholder';
+import { useSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
 
 export const EditingComponentPlaceholder = ({
   rendering,
 }: {
   rendering: RouteData;
-}): JSX.Element => (
-  <div id={EDITING_COMPONENT_ID}>
-    <Placeholder name={EDITING_COMPONENT_PLACEHOLDER} rendering={rendering} />
-  </div>
-);
+}): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  return (
+    <div id={EDITING_COMPONENT_ID}>
+      <Placeholder
+        name={EDITING_COMPONENT_PLACEHOLDER}
+        rendering={rendering}
+        sitecoreContext={sitecoreContext}
+      />
+    </div>
+  );
+};
