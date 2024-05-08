f420ecaec3119ad40e3b57c9d828bc4d8603bb34
Artem Alexeyenko <ala@sitecore.net>
2024-05-08 16:01:53 -0400

    [nextjs][RAV] avoid unexpected linebreaks when generating config

--- packages/create-sitecore-jss/src/templates/react/scripts/generate-config.js
+++ packages/create-sitecore-jss/src/templates/react/scripts/generate-config.js
@@ -51,7 +51,7 @@ const config = {};\n`;
   // Set base configuration values, allowing override with environment variables
   Object.keys(config).forEach(prop => {
     configText += `config.${prop} = process.env.REACT_APP_${constantCase(prop)} || "${
-      config[prop]
+      config[prop]?.trim()
     }";\n`;
   });
   configText += 'module.exports = config;';
