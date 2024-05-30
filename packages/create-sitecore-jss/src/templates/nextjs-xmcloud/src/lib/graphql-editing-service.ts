bca631094ed816c86839169c81eb07c46e3ab3c1
illiakovalenko <illia.kovalenko@sitecore.com>
2024-05-30 15:59:38 +0300

    Updated comment

--- packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/lib/graphql-editing-service.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-xmcloud/src/lib/graphql-editing-service.ts
@@ -2,7 +2,7 @@ import { GraphQLEditingService } from '@sitecore-jss/sitecore-jss-nextjs/editing
 import clientFactory from 'lib/graphql-client-factory';
 
 /**
- * GraphQL Editing Service instance. Used to fetch editing data in Pages preview (editing) metadata mode.
+ * GraphQL Editing Service instance. Used to fetch editing data in Pages preview (editing) Metadata mode.
  */
 export const graphQLEditingService = new GraphQLEditingService({
   clientFactory,
