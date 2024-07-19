798edbe4d67187560f274311429ce3d6a756dce8
Artem Alexeyenko <ala@sitecore.net>
2024-07-19 08:14:53 -0400

    lint
    (cherry picked from commit 9501b262420715fa4acb477780f15885b93dc68e)

--- packages/create-sitecore-jss/src/initializers/angular/prompts.ts
+++ packages/create-sitecore-jss/src/initializers/angular/prompts.ts
@@ -5,23 +5,31 @@ import {
   styleguidePrompts,
 } from '../../common';
 
-export type AngularAnswer = ClientAppAnswer & StyleguideAnswer & {
-  xmcloud: boolean
-};
+export type AngularAnswer = ClientAppAnswer &
+  StyleguideAnswer & {
+    xmcloud: boolean;
+  };
 
-export const prompts = [...clientAppPrompts, {
-  type: 'confirm',
-  name: 'xmcloud',
-  message: 'Are you building for Sitecore XM Cloud?',
-  default: false,
-  when: (answers: AngularAnswer): boolean => {
-    // don't prompt if --yes or nextjs-xmcloud template was specified
-    if (answers.yes) {
-      return false;
-    } else if (answers.templates.includes('angular-xmcloud') && !answers.templates.includes('angular-sxp')) {
-      answers.xmcloud = true;
-      return false;
-    }
-    return true;
+export const prompts = [
+  ...clientAppPrompts,
+  {
+    type: 'confirm',
+    name: 'xmcloud',
+    message: 'Are you building for Sitecore XM Cloud?',
+    default: false,
+    when: (answers: AngularAnswer): boolean => {
+      // don't prompt if --yes or nextjs-xmcloud template was specified
+      if (answers.yes) {
+        return false;
+      } else if (
+        answers.templates.includes('angular-xmcloud') &&
+        !answers.templates.includes('angular-sxp')
+      ) {
+        answers.xmcloud = true;
+        return false;
+      }
+      return true;
+    },
   },
-}, ...styleguidePrompts];
+  ...styleguidePrompts,
+];
