5face191f6e8808e60d365af323a03e6b8a86eaa
yavorsk <yavor.krastev@sitecore.com>
2024-07-23 10:20:20 +0300

    move styleguide fragment types out of sngular base template

--- packages/create-sitecore-jss/src/templates/angular/src/graphql-fragment-types.ts
+++ packages/create-sitecore-jss/src/templates/angular/src/graphql-fragment-types.ts
@@ -49,9 +49,6 @@ export default {
             name: 'UnknownItem',
           },
           {
-            name: 'StyleguideComponentParamsRenderingParameters',
-          },
-          {
             name: 'C__StandardTemplate',
           },
           {
@@ -59,95 +56,6 @@ export default {
           },
           {
             name: 'RenderEngineType',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideTracking',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideSitecoreContext',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideSection',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideRouteFields',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideMultilingual',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideLayoutTabsTab',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideLayoutTabs',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideLayoutReuse',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideItemLinkItemTemplate',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageText',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageRichText',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageNumber',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageLink',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageItemLink',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageImage',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageFile',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageDate',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageCustom',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageContentList',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideFieldUsageCheckbox',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideExplanatoryComponent',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideContentListItemTemplate',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideComponentParams',
-          },
-          {
-            name:
-              '<%- helper.getAppPrefix(appPrefix, appName, false) %>StyleguideAngularLazyLoading',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>GraphQLIntegratedDemo',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>GraphQLConnectedDemo',
-          },
-          {
-            name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>ExampleCustomRouteType',
           },
           {
             name: '<%- helper.getAppPrefix(appPrefix, appName, false) %>ContentBlock',
