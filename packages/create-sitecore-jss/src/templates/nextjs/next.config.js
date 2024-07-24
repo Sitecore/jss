14fba9fa3c1ca8cec6220d5315867b0bf4464dc2
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2024-07-24 11:10:11 -0400

    [Next.js] Image optimization remote pattern for XM Cloud CM/preview images (#1850)
    * addl next image optimization remote pattern for XMC CM/preview images
    
    * CHANGELOG update

--- packages/create-sitecore-jss/src/templates/nextjs/next.config.js
+++ packages/create-sitecore-jss/src/templates/nextjs/next.config.js
@@ -42,6 +42,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'xmc-*.**',
+        port: '',
+      },
+      {
+        protocol: 'https',
         hostname: 'feaas*.blob.core.windows.net',
         port: '',
       },
