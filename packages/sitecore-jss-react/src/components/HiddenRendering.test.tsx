15c6cadcac3b76cf6a5974cb519b31adf785c454
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2024-07-25 17:19:08 -0500

    Add 'The component is hidden' text to HiddenRendering component

--- packages/sitecore-jss-react/src/components/HiddenRendering.test.tsx
+++ packages/sitecore-jss-react/src/components/HiddenRendering.test.tsx
@@ -9,13 +9,17 @@ describe('<HiddenRendering />', () => {
     const rendered = mount(<HiddenRendering />);
     expect(rendered).to.have.length(1);
     expect(rendered.find('div').prop('style')).to.deep.equal({
-      height: '100px',
       backgroundImage:
         'linear-gradient(45deg, #ffffff 25%, #dcdcdc 25%, #dcdcdc 50%, #ffffff 50%, #ffffff 75%, #dcdcdc 75%, #dcdcdc 100%)',
       backgroundSize: '3px 3px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '30px',
+      color: '#aaa',
     });
     expect(rendered.html()).to.equal(
-      '<div style="height: 100px; background-size: 3px 3px;"></div>'
+      '<div style="background-size: 3px 3px; display: flex; justify-content: center; align-items: center; padding: 30px; color: rgb(170, 170, 170);">The component is hidden</div>'
     );
   });
 });
