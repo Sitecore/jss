c15461c05f3c6e75bd064795e0281ec3d3665486
Adam Brauer <400763+ambrauer@users.noreply.github.com>
2024-07-26 06:36:27 -0400

    [Next.js][RAV] Add 'The component is hidden' text to Hidden Rendering component (#1857)
    * Add 'The component is hidden' text to HiddenRendering component
    
    * CHANGELOG update

--- packages/sitecore-jss-angular/src/components/hidden-rendering.component.ts
+++ packages/sitecore-jss-angular/src/components/hidden-rendering.component.ts
@@ -1,10 +1,10 @@
 import { Component, HostBinding } from '@angular/core';
 @Component({
   selector: 'sc-hidden-rendering',
-  template: '',
+  template: 'The component is hidden',
 })
 export class HiddenRenderingComponent {
   @HostBinding('style') get style() {
-    return 'background-image: linear-gradient(45deg, #ffffff 25%, #dcdcdc 25%, #dcdcdc 50%, #ffffff 50%, #ffffff 75%, #dcdcdc 75%, #dcdcdc 100%); background-size: 3px 3px; display: block; height: 100px;';
+    return 'background-image: linear-gradient(45deg, #ffffff 25%, #dcdcdc 25%, #dcdcdc 50%, #ffffff 50%, #ffffff 75%, #dcdcdc 75%, #dcdcdc 100%); background-size: 3px 3px; display: flex; justify-content: center; align-items: center; padding: 30px; color: #aaa;';
   }
 }
