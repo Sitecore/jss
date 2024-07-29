188a6740f9ff47a88273496c1ba8b2e673de519d
illiakovalenko <illia.kovalenko@sitecore.com>
2024-07-30 02:07:03 +0300

    Updated comment

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/scripts/config/plugins/computed.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/scripts/config/plugins/computed.ts
@@ -14,7 +14,6 @@ class ComputedPlugin implements ConfigPlugin {
 
   async exec(config: JssConfig) {
     const proxyBuildPath = process.env[`${constantCase('proxyBuildPath')}`]?.replace(/\/$/, '');
-
     const proxyHost = process.env[`${constantCase('proxyHost')}`];
 
     const production = process.env.JSS_MODE === constants.JSS_MODE.PRODUCTION;
@@ -25,8 +24,12 @@ class ComputedPlugin implements ConfigPlugin {
     }`;
 
     if (production) {
+      // When build for production (using `jss build`)
+      // The client side requests will be proxied through the node-xmcloud-proxy and passed to the Sitecore instance
+      // The server side requests will be executed against the Sitecore instance directly
       graphQLEndpoint = `\${typeof window === 'undefined' ? '${computed}' : '${proxyHost}${config.graphQLEndpointPath}'}`;
     } else {
+      // When working in development mode (`jss start:connected`)
       graphQLEndpoint = computed;
     }
 
