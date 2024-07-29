58528f2d9034d1ef7f7f3d87beabf79b6577d233
illiakovalenko <illia.kovalenko@sitecore.com>
2024-07-30 02:09:55 +0300

    Updated comment

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/scripts/proxy-build.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/scripts/proxy-build.ts
@@ -1,6 +1,8 @@
 import { execSync } from 'child_process';
 import { environment } from '../src/environments/environment';
 
+// Executed at the end of the build process (jss build) to move the build output to the proxy build path
+
 try {
   console.log('Moving build output to proxy build path:', environment.proxyBuildPath);
 
