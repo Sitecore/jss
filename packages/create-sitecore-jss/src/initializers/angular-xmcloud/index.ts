84bf47116712ab08421306b3db93bab35690fbde
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2024-07-30 17:22:11 -0400

    add tranfotm logic to angular-xmcloud (#1866)
    Co-authored-by: Addy Pathania <aditya.pathania22@gmail.com>

--- packages/create-sitecore-jss/src/initializers/angular-xmcloud/index.ts
+++ packages/create-sitecore-jss/src/initializers/angular-xmcloud/index.ts
@@ -1,4 +1,11 @@
-import { ClientAppArgs, DEFAULT_APPNAME, Initializer } from '../../common';
+import path, { sep } from 'path';
+import {
+  ClientAppArgs,
+  DEFAULT_APPNAME,
+  Initializer,
+  transform,
+  openPackageJson,
+} from '../../common';
 import { InitializerResults } from '../../common/Initializer';
 
 export default class AngularXmCloudInitializer implements Initializer {
@@ -7,6 +14,18 @@ export default class AngularXmCloudInitializer implements Initializer {
   }
 
   async init(args: ClientAppArgs) {
+    const pkg = openPackageJson(`${args.destination}${sep}package.json`);
+
+    const mergedArgs = {
+      ...args,
+      appName: args.appName || pkg?.config?.appName || DEFAULT_APPNAME,
+      appPrefix: args.appPrefix || pkg?.config?.prefix || false,
+    };
+
+    const templatePath = path.resolve(__dirname, '../../templates/angular-xmcloud');
+
+    await transform(templatePath, mergedArgs);
+
     const response: InitializerResults = {
       nextSteps: [],
       appName: args.appName || DEFAULT_APPNAME,
