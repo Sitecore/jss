dfaca2dc0c7129610fed225c542bd6bd5c95ff0c
Addy Pathania <aditya.pathania22@gmail.com>
2024-07-29 22:29:19 -0400

    add richtext component

---
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/richtext/richtext.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component, Input, OnInit } from '@angular/core';
+import { ComponentRendering } from '@sitecore-jss/sitecore-jss-angular';
+
+@Component({
+  selector: 'app-rich-text',
+  templateUrl: './rich-text.component.html',
+})
+export class RichTextComponent implements OnInit {
+  @Input() rendering: ComponentRendering;
+  text: string;
+  id: string;
+  styles: string;
+
+  ngOnInit() {
+    this.text = this.rendering.fields.Text;
+    this.id = this.rendering.params.RenderingIdentifier;
+    this.styles = this.rendering.params?.styles?.trimEnd();
+  }
+}
