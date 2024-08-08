79c3bae28f69d3315b601727136e971280ab75c4
Artem Alexeyenko <ala@sitecore.net>
2024-08-08 14:45:15 -0400

    Fix JSS-3824: move fetchWith to SXP prompts

--- packages/create-sitecore-jss/src/common/prompts/sxp.ts
+++ packages/create-sitecore-jss/src/common/prompts/sxp.ts
@@ -1,4 +1,5 @@
 import { Answers, DistinctQuestion } from 'inquirer';
+import { DEFAULT_FETCHWITH, FetchWith } from './base';
 
 /**
  * Set of CLI answers for an SXP app
@@ -8,6 +9,11 @@ export type SxpAnswer = Answers & {
    * Application host name
    */
   hostName: string;
+  /**
+   * Determines which request method should be used.
+   * Default is @type {DEFAULT_FETCHWITH}
+   */
+  fetchWith: FetchWith;
 };
 
 /**
@@ -24,6 +30,19 @@ export const sxpPrompts: DistinctQuestion<SxpAnswer>[] = [
         answers.hostName = `${answers.appName}.dev.local`;
       }
       return !answers.hostName;
+    },
+  },
+  {
+    type: 'list',
+    name: 'fetchWith',
+    message: 'How would you like to fetch Layout and Dictionary data?',
+    choices: Object.values(FetchWith),
+    default: DEFAULT_FETCHWITH,
+    when: (answers: SxpAnswer): boolean => {
+      if (answers.yes && !answers.fetchWith) {
+        answers.fetchWith = DEFAULT_FETCHWITH;
+      }
+      return !answers.fetchWith;
     },
   },
 ];
