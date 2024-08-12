54aa9863bafb5297df5df4e8535972e27878e18e
Addy Pathania <89087450+sc-addypathania@users.noreply.github.com>
2024-08-12 18:22:43 -0400

    add fix for page not found error

--- packages/sitecore-jss-nextjs/src/editing/editing-render-middleware.ts
+++ packages/sitecore-jss-nextjs/src/editing/editing-render-middleware.ts
@@ -358,7 +358,6 @@ export class MetadataHandler {
       } as EditingMetadataPreviewData,
       // Cache the preview data for 3 seconds to ensure the page is rendered with the correct preview data not the cached one
       {
-        path: query.route,
         maxAge: 3,
       }
     );
