7fe842beb63bcb4f95deb4b4ac422d46aade52bb
yavorsk <yavor.krastev@sitecore.com>
2024-08-13 16:50:59 +0300

    use templates for image variants

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/image/image.component.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/image/image.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, TemplateRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { EditMode, ImageField } from '@sitecore-jss/sitecore-jss-angular';
 import { SxaComponent } from '../sxa.component';
 import { JssContextService } from '../../jss-context.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-image',
   templateUrl: './image.component.html',
 })
 export class ImageComponent extends SxaComponent implements OnInit, OnDestroy {
+  @ViewChild('default', { static: true }) defaultVariant: TemplateRef<any>;
+  @ViewChild('banner', { static: true }) bannerVariant: TemplateRef<any>;
+  classHeroBannerEmpty = '';
+  backgroundStyle = {};
+  modifyImageProps = {};
   isEditing = false;
   private contextSubscription: Subscription;
 
@@ -18,8 +24,32 @@ export class ImageComponent extends SxaComponent implements OnInit, OnDestroy {
   ngOnInit() {
     super.ngOnInit();
 
+    const imageField = this.rendering.fields?.Image as ImageField;
+    this.backgroundStyle = imageField?.value?.src && {
+      'background-image': `url('${imageField.value.src}')`,
+    };
+
     this.contextSubscription = this.jssContext.state.subscribe((newState) => {
       this.isEditing = newState.sitecore && newState.sitecore.context.pageEditing;
+
+      this.classHeroBannerEmpty =
+        this.isEditing && imageField?.value?.class === 'scEmptyImage' ? 'hero-banner-empty' : '';
+
+      const isMetadataMode = newState.sitecore?.context?.editMode === EditMode.Metadata;
+      this.modifyImageProps = !isMetadataMode
+        ? {
+            ...imageField,
+            editable: imageField?.editable
+              ?.replace(`width="${imageField?.value?.width}"`, 'width="100%"')
+              .replace(`height="${imageField?.value?.height}"`, 'height="100%"'),
+          }
+        : {
+            ...imageField,
+            value: {
+              ...imageField?.value,
+              style: { width: '100%', height: '100%' },
+            },
+          };
     });
   }
 
@@ -27,5 +57,11 @@ export class ImageComponent extends SxaComponent implements OnInit, OnDestroy {
     if (this.contextSubscription) {
       this.contextSubscription.unsubscribe();
     }
+  }
+
+  public get variant(): TemplateRef<any> {
+    return this.rendering.params?.FieldNames === 'Banner'
+      ? this.bannerVariant
+      : this.defaultVariant;
   }
 }
