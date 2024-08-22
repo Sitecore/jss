270560c59a8280b6433c111182bf7fa87c8b4254
yavorsk <yavor.krastev@sitecore.com>
2024-08-22 23:27:23 +0300

    simplify promo component; remove withtext variant since it is not used anymore

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/promo/promo.component.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/promo/promo.component.ts
@@ -1,22 +1,8 @@
-import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { SxaComponent } from '../sxa.component';
 
 @Component({
   selector: 'app-promo',
   templateUrl: './promo.component.html',
 })
-export class PromoComponent extends SxaComponent {
-  @ViewChild('default', { static: true }) defaultVariant: TemplateRef<any>;
-  @ViewChild('withText', { static: true }) withTextVariant: TemplateRef<any>;
-  @ViewChild('empty', { static: true }) empty: TemplateRef<any>;
-
-  public get variant(): TemplateRef<any> {
-    if (!this.rendering.fields) {
-      return this.empty;
-    }
-
-    return this.rendering.params?.FieldNames === 'WithText'
-      ? this.withTextVariant
-      : this.defaultVariant;
-  }
-}
+export class PromoComponent extends SxaComponent {}
