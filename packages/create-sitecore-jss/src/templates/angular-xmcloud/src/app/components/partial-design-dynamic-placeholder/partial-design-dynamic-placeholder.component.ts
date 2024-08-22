91a0198a216905141f2ecce4479cdabc09b35bec
Addy Pathania <aditya.pathania22@gmail.com>
2024-08-22 15:03:26 -0400

    add partialDesignDynamicPlaceholder

---
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/partial-design-dynamic-placeholder/partial-design-dynamic-placeholder.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { SxaComponent } from '../sxa.component';
+
+@Component({
+  selector: 'app-partial-design-dynamic-placeholder',
+  templateUrl: './partial-design-dynamic-placeholder.component.html',
+})
+export class PartialDesignDynamicPlaceholderComponent extends SxaComponent implements OnInit {
+  sig: string;
+  ngOnInit() {
+    super.ngOnInit();
+
+    this.sig = this.rendering.params.sig;
+  }
+}
