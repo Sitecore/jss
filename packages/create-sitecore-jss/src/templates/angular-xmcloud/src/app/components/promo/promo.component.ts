5d22f7e1b75ccc14743fb9737678a087a88934c6
Addy Pathania <aditya.pathania22@gmail.com>
2024-08-27 20:35:50 -0400

    fix sxa styles

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/promo/promo.component.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/promo/promo.component.ts
@@ -4,5 +4,22 @@ import { SxaComponent } from '../sxa.component';
 @Component({
   selector: 'app-promo',
   templateUrl: './promo.component.html',
+  styles: [
+    `
+      :host {
+        width: 50%;
+        display: inline-block;
+      }
+      :host ::ng-deep > * {
+        width: 100%;
+        height: 100%;
+      }
+      @media (max-width: 992px) {
+        :host {
+          width: 100%;
+        }
+      }
+    `,
+  ],
 })
 export class PromoComponent extends SxaComponent {}
--- packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/richtext/richtext.component.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/components/richtext/richtext.component.ts
@@ -1,16 +1,58 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { Field } from '@sitecore-jss/sitecore-jss-angular';
 import { SxaComponent } from '../sxa.component';
 
 @Component({
   selector: 'app-richtext',
   templateUrl: './richtext.component.html',
+  styles: [
+    `
+      :host(.footer-richtext) {
+        width: 17%;
+        display: inline-block;
+      }
+
+      :host(.footer-richtext) ::ng-deep > * {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+      }
+
+      :host(.footer-richtext) ::ng-deep > *:nth-child(2) {
+        order: -1;
+      }
+
+      @media (max-width: 992px) {
+        :host(.footer-richtext) {
+          width: 100%;
+        }
+      }
+    `,
+  ],
 })
 export class RichTextComponent extends SxaComponent implements OnInit {
   text?: Field<string>;
 
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+    super();
+  }
+
   ngOnInit() {
     super.ngOnInit();
     this.text = this.rendering.fields?.Text as Field<string>;
+
+    let parent = this.el.nativeElement.parentElement;
+
+    while (parent) {
+      if (parent.id === 'footer') {
+        this.renderer.addClass(this.el.nativeElement, 'footer-richtext');
+        break;
+      } else if (parent.id === 'header') {
+        this.renderer.addClass(this.el.nativeElement, 'header-richtext');
+        break;
+      }
+      parent = parent.parentElement;
+    }
   }
 }
