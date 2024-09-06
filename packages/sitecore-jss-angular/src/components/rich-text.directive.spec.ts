971602e81826c9291186872237947ab88f631c0c
Yavor Krastev <4502045+yavorsk@users.noreply.github.com>
2024-09-06 16:07:22 +0300

    [sitecore-jss] [sitecore-jss-angular] Default Placeholder Content for empty fields in editMode metadata (#1916)
    * empty editing placeholder for rich text field
    
    * minor updates
    
    * empty editing placeholder for date and text fields
    
    * date, text directive fixes
    
    * refactoring
    
    * remove consolelog
    
    * empty field placeholder for link fields
    
    * empty field editing placeholder for image field
    
    * unit tests for image, text, richtext, date
    
    * link directives unit tests
    
    * remove console.log
    
    * fix lint errors
    
    * minor naming fixes
    
    * minor fix
    
    * add Date type to possible types for GenericFieldValue base package type (in order to make it compatible with angular package types); updated isFieldValueEmpty function and unit tests for that change
    
    * update changelog
    
    * add unit test for invalid Date object
    
    * fix lint error
    
    * rename default placeholder components
    
    * additional renaming
    
    * fix lint issue
    
    * fix lint issue
    
    * add jsdoc comments
    
    * updates to changelog
    
    * reafactoring: add property defaultFieldEditingComponent to field directives and instantiate it in the constructor instead of passing it as an argument
    
    * additional tsdoc comments
    
    * fix lint issue
    
    * remove unnecessary function
    
    * base directive unit tests
    
    * fix lint issue
    
    * Update packages/sitecore-jss-angular/src/components/base-field.directive.spec.ts
    
    * Minor renaming
    
    ---------
    
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>
    Co-authored-by: illiakovalenko <illia.kovalenko@sitecore.com>

--- packages/sitecore-jss-angular/src/components/rich-text.directive.spec.ts
+++ packages/sitecore-jss-angular/src/components/rich-text.directive.spec.ts
@@ -1,4 +1,4 @@
-import { Component, DebugElement, Input } from '@angular/core';
+import { Component, DebugElement, Input, TemplateRef } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Router } from '@angular/router';
@@ -19,26 +19,53 @@ class TestComponent {
   @Input() editable = true;
 }
 
+const emptyTextFieldEditingTemplateId = 'emptyTextFieldEditingTemplate';
+const emptyTextFieldEditingTemplate =
+  '<span>[This is a *custom* empty field component for text]</span>';
+
+@Component({
+  selector: 'test-empty-template-rich-text',
+  template: `
+    <h1
+      *scRichText="
+        field;
+        editable: editable;
+        emptyFieldEditingTemplate: ${emptyTextFieldEditingTemplateId}
+      "
+    ></h1>
+    <ng-template #${emptyTextFieldEditingTemplateId}>
+      ${emptyTextFieldEditingTemplate}
+    </ng-template>
+  `,
+})
+class TestEmptyTemplateComponent {
+  @Input() field: RichTextField;
+  @Input() emptyFieldEditingTemplate: TemplateRef<unknown>;
+  @Input() editable = true;
+}
+
 describe('<div *scRichText />', () => {
   let fixture: ComponentFixture<TestComponent>;
   let de: DebugElement;
+  let deH: DebugElement;
   let comp: TestComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [RichTextDirective, TestComponent],
+      declarations: [RichTextDirective, TestComponent, TestEmptyTemplateComponent],
       imports: [RouterTestingModule.withRoutes([{ path: 'lorem', component: TestComponent }])],
     });
 
     fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
 
-    de = fixture.debugElement.query(By.css('h1'));
+    de = fixture.debugElement;
+    deH = de.query(By.css('h1'));
     comp = fixture.componentInstance;
   });
 
   it('should render nothing with missing field', () => {
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe('');
   });
 
@@ -46,7 +73,7 @@ describe('<div *scRichText />', () => {
     comp.field = {};
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe('');
   });
 
@@ -58,7 +85,7 @@ describe('<div *scRichText />', () => {
     comp.field = field;
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe('editable');
   });
 
@@ -71,7 +98,7 @@ describe('<div *scRichText />', () => {
     comp.editable = false;
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe('<a href="www.example.com">Hello World</a>');
   });
 
@@ -82,7 +109,7 @@ describe('<div *scRichText />', () => {
     comp.field = field;
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe('value');
   });
 
@@ -93,7 +120,7 @@ describe('<div *scRichText />', () => {
     comp.field = field;
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toBe(field.value);
   });
 
@@ -106,7 +133,7 @@ describe('<div *scRichText />', () => {
     const router: Router = TestBed.inject(Router);
     spyOn(router, 'navigateByUrl');
 
-    const renderedLink = de.nativeElement.querySelector('a[href]');
+    const renderedLink = deH.nativeElement.querySelector('a[href]');
     expect(renderedLink.getAttribute('href')).toBe('/lorem');
     renderedLink.click();
     fixture.detectChanges();
@@ -120,8 +147,65 @@ describe('<div *scRichText />', () => {
     comp.field = field;
     fixture.detectChanges();
 
-    const rendered = de.nativeElement.innerHTML;
+    const rendered = deH.nativeElement.innerHTML;
     expect(rendered).toContain('<input');
     expect(rendered).toContain('<span class="scChromeData">');
+  });
+
+  describe('editMode metadata', () => {
+    const testMetadata = {
+      contextItem: {
+        id: '{09A07660-6834-476C-B93B-584248D3003B}',
+        language: 'en',
+        revision: 'a0b36ce0a7db49418edf90eb9621e145',
+        version: 1,
+      },
+      fieldId: '{414061F4-FBB1-4591-BC37-BFFA67F745EB}',
+      fieldType: 'single-line',
+      rawValue: 'Test1',
+    };
+
+    it('should render default empty field component when field value is empty', () => {
+      const field = {
+        value: '',
+        metadata: testMetadata,
+      };
+      comp.field = field;
+      fixture.detectChanges();
+
+      const rendered = de.nativeElement.innerHTML;
+      expect(rendered).toContain('<span>[No text in field]</span>');
+    });
+
+    it('should render custom empty field component when provided, when field value is empty', () => {
+      fixture = TestBed.createComponent(TestEmptyTemplateComponent);
+      fixture.detectChanges();
+
+      de = fixture.debugElement;
+      comp = fixture.componentInstance;
+
+      const field = {
+        value: '',
+        metadata: testMetadata,
+      };
+      comp.field = field;
+      fixture.detectChanges();
+
+      const rendered = de.nativeElement.innerHTML;
+      expect(rendered).toContain(emptyTextFieldEditingTemplate);
+    });
+
+    it('should render nothing when field value is empty, when editing is explicitly disabled', () => {
+      const field = {
+        value: '',
+        metadata: testMetadata,
+      };
+      comp.field = field;
+      comp.editable = false;
+      fixture.detectChanges();
+
+      const rendered = deH.nativeElement.innerHTML;
+      expect(rendered).toBe('');
+    });
   });
 });
--- packages/sitecore-jss-angular/src/components/text.directive.ts
+++ packages/sitecore-jss-angular/src/components/text.directive.ts
@@ -1,27 +1,40 @@
 import {
   Directive,
-  EmbeddedViewRef,
   Input,
   OnChanges,
   SimpleChanges,
   TemplateRef,
+  Type,
   ViewContainerRef,
 } from '@angular/core';
 import { TextField } from './rendering-field';
+import { BaseFieldDirective } from './base-field.directive';
+import { DefaultEmptyFieldEditingComponent } from './default-empty-text-field-editing-placeholder.component';
 
 @Directive({
   selector: '[scText]',
 })
-export class TextDirective implements OnChanges {
+export class TextDirective extends BaseFieldDirective implements OnChanges {
   @Input('scTextEditable') editable = true;
 
   @Input('scTextEncode') encode = true;
 
   @Input('scText') field: TextField;
 
-  private viewRef: EmbeddedViewRef<unknown>;
+  /**
+   * Custom template to render in Pages in Metadata edit mode if field value is empty
+   */
+  @Input('scTextEmptyFieldEditingTemplate') emptyFieldEditingTemplate: TemplateRef<unknown>;
 
-  constructor(private viewContainer: ViewContainerRef, private templateRef: TemplateRef<unknown>) {}
+  /**
+   * Default component to render in Pages in Metadata edit mode if field value is empty and emptyFieldEditingTemplate is not provided
+   */
+  protected defaultFieldEditingComponent: Type<unknown>;
+
+  constructor(viewContainer: ViewContainerRef, private templateRef: TemplateRef<unknown>) {
+    super(viewContainer);
+    this.defaultFieldEditingComponent = DefaultEmptyFieldEditingComponent;
+  }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.field || changes.editable || changes.encode) {
@@ -35,12 +48,13 @@ export class TextDirective implements OnChanges {
   }
 
   private updateView() {
-    const field = this.field;
-    let editable = this.editable;
-
-    if (!field || (!field.editable && (field.value === undefined || field.value === ''))) {
+    if (!this.shouldRender()) {
+      super.renderEmpty();
       return;
     }
+
+    const field = this.field;
+    let editable = this.editable;
 
     // can't use editable value if we want to output unencoded
     if (!this.encode) {
