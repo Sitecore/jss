a69fed355bafc2d87be21bd1150956aaa2ced6a0
Yavor Krastev <4502045+yavorsk@users.noreply.github.com>
2024-09-12 17:08:43 +0300

    [sitecore-jss-proxy][templates/node-xmcloud-proxy] Health check endpoint and middleware (#1928)
    * the health check endpoint and middleware
    
    * changelog update
    
    * fix typo
    
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>
    
    * add healtcheck router to hide the endpoint path in proxy package
    
    * some fefactoring and typo fixes
    
    * refactoring
    
    ---------
    
    Co-authored-by: Illia Kovalenko <23364749+illiakovalenko@users.noreply.github.com>

--- packages/sitecore-jss-proxy/src/middleware/index.ts
+++ packages/sitecore-jss-proxy/src/middleware/index.ts
@@ -2,3 +2,4 @@
 // eslint-disable-next-line prettier/prettier
 export * as headlessProxy from './headless-ssr-proxy';
 export { editingRouter } from './editing';
+export { healthCheck } from './healthcheck';
