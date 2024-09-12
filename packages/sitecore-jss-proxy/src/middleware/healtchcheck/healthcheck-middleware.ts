6e1aa471a6119d090e3be4edd21852c4fe1e9c36
yavorsk <yavor.krastev@sitecore.com>
2024-09-12 15:19:01 +0300

    some fefactoring and typo fixes

--- packages/sitecore-jss-proxy/src/middleware/healtchcheck/healthcheck-middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-/**
- * Middleware to handle health check requests
- */
-export const healthCheckMiddleware = () => (_req: Request, res: Response): void => {
-  res.status(200).send('Healthy');
-};
--- packages/sitecore-jss-proxy/src/middleware/healthcheck/index.ts
+++ packages/sitecore-jss-proxy/src/middleware/healthcheck/index.ts
@@ -1,5 +1,11 @@
-import { Router } from 'express';
-import { healthCheckMiddleware } from './healthcheck-middleware';
+import { Router, Request, Response } from 'express';
+
+/**
+ * Middleware to handle health check requests
+ */
+const healthCheckMiddleware = () => (_req: Request, res: Response): void => {
+  res.status(200).send('Healthy');
+};
 
 /**
  * Creates a router for health check requests.
