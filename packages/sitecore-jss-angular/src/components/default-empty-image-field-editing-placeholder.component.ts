70a58c00717aa65a242ede9bf15bfcd4e68565b3
Addy Pathania <89087450+addy-pathania@users.noreply.github.com>
2024-10-07 05:45:42 -0400

    [sitecore-jss-angular] Fix default empty Image field styles (#1940)

--- packages/sitecore-jss-angular/src/components/default-empty-image-field-editing-placeholder.component.ts
+++ packages/sitecore-jss-angular/src/components/default-empty-image-field-editing-placeholder.component.ts
@@ -12,7 +12,16 @@ import { Component } from '@angular/core';
       class="scEmptyImage"
     />
   `,
-  styles:
-    'img { min-width:48px; min-height:48px; max-width:400px; max-height:400px; cursor:pointer }',
+  styles: [
+    `
+      :host {
+        min-width: 48px;
+        min-height: 48px;
+        max-width: 400px;
+        max-height: 400px;
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class DefaultEmptyImageFieldEditingComponent {}
