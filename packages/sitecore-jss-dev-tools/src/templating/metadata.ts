e29e823ee534bcdf3baaebde9d7019726d6bf367
yavorsk <yavor.krastev@sitecore.com>
2024-10-15 16:23:56 +0300

    fix lint errors

--- packages/sitecore-jss-dev-tools/src/templating/metadata.ts
+++ packages/sitecore-jss-dev-tools/src/templating/metadata.ts
@@ -25,7 +25,7 @@ export function getMetadata(): Metadata {
       console.log('Creating package-lock.json ...');
       execSync('npm i --package-lock-only --workspaces false --force');
     } catch (error) {
-      console.error(`Failed to create package-lock.json in project`, error);
+      console.error('Failed to create package-lock.json in project', error);
       return metadata;
     }
   }
@@ -34,7 +34,7 @@ export function getMetadata(): Metadata {
   try {
     packageLock = JSON.parse(fs.readFileSync(packageLockPath, 'utf8'));
   } catch (error) {
-    console.error(`Failed to read/parse package-lock.json`, error);
+    console.error('Failed to read/parse package-lock.json', error);
     return metadata;
   }
 
@@ -48,13 +48,19 @@ export function getMetadata(): Metadata {
   return metadata;
 }
 
+/**
+ * Traverses the provided package-lock.json to find the packages of the tracked scopes with their exact versions
+ * @param {Record<string, unknown>} packageLockJson the package-lock json to traverse
+ * @returns {Record<string, string>} an object with the packages with their exact versions
+ */
 function getPackagesFromPackageLock(
   packageLockJson: Record<string, unknown>
 ): Record<string, string> {
   const packagesResult: Record<string, string> = {};
-  const packages = packageLockJson['packages'] as Record<string, Package>;
+  const packages = packageLockJson.packages as Record<string, Package>;
 
   // traverse packages to get all tracked package names
+  // eslint-disable-next-line guard-for-in
   for (const key in packages) {
     dependencyTypes.forEach((depType) => {
       for (const dep in packages[key][depType] as Record<string, string>) {
@@ -69,6 +75,7 @@ function getPackagesFromPackageLock(
 
   // traverse packages again to get the exact versions.
   // getting packages names and versions in one go doesn't gurantee getting all package info correctly
+  // eslint-disable-next-line guard-for-in
   for (const key in packages) {
     Object.keys(packagesResult).forEach((scPackage) => {
       if (packages[key].name === scPackage || key.endsWith(scPackage)) {
