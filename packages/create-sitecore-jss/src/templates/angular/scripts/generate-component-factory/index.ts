d89346a25ffba0d06c6b869917615b0270a0bb7e
illiakovalenko <illia.kovalenko@sitecore.com>
2024-10-16 19:04:33 +0300

    Update

--- packages/create-sitecore-jss/src/templates/angular/scripts/generate-component-factory/index.ts
+++ packages/create-sitecore-jss/src/templates/angular/scripts/generate-component-factory/index.ts
@@ -1,7 +1,23 @@
-import * as fs from 'fs';
-const path = require('path');
-const chokidar = require('chokidar');
-const { componentFactoryTemplate } = require('./template');
+const plugins = require('scripts/temp/generate-component-factory-plugins');
+import { PackageDefinition } from '@sitecore-jss/sitecore-jss-dev-tools';
+
+export interface ComponentFactoryPluginConfig {
+  watch?: boolean;
+  packages: PackageDefinition[];
+  components: string[];
+}
+
+export interface ComponentFactoryPlugin {
+  /**
+   * Detect order when the plugin should be called, e.g. 0 - will be called first (can be a plugin which data is required for other plugins)
+   */
+  order: number;
+  /**
+   * A function which will be called during component factory generation
+   * @param {JssConfig} config Current (accumulated) config
+   */
+  exec(config: ComponentFactoryPluginConfig): ComponentFactoryPluginConfig;
+}
 
 /*
   COMPONENT FACTORY GENERATION
@@ -18,149 +34,12 @@ const { componentFactoryTemplate } = require('./template');
   This is used during `jss start` to pick up new or removed components at runtime.
 */
 
-export interface PackageDefinition {
-  name: string;
-  components: {
-    moduleName: string;
-    componentName: string;
-  }[];
-}
+const defaultConfig: ComponentFactoryPluginConfig = {
+  watch: process.argv.some(arg => arg === '--watch'),
+  packages: [],
+  components: [],
+};
 
-const componentFactoryPath = path.resolve('src/app/components/app-components.module.ts');
-const componentRootPath = 'src/app/components';
-
-const isWatch = process.argv.some((arg) => arg === '--watch');
-
-if (isWatch) {
-  watchComponentFactory();
-} else {
-  writeComponentFactory();
-}
-
-function watchComponentFactory() {
-  console.log(`Watching for changes to component factory sources in ${componentRootPath}...`);
-
-  chokidar
-    .watch(componentRootPath, { ignoreInitial: true, awaitWriteFinish: true })
-    .on('add', writeComponentFactory)
-    .on('unlink', writeComponentFactory);
-}
-
-function writeComponentFactory() {
-  const componentFactory = generateComponentFactory();
-
-  console.log(`Writing component factory to ${componentFactoryPath}`);
-
-  fs.writeFileSync(componentFactoryPath, componentFactory, { encoding: 'utf8' });
-}
-
-function generateComponentFactory() {
-  // By convention, we expect to find Angular components
-  // under /src/app/components/component-name/component-name.component.ts
-  // If a component-name.module.ts file exists, we will treat it as lazy loaded.
-  // If you'd like to use your own convention, encode it below.
-  // NOTE: generating the component factory module is also totally optional,
-  // and it can be maintained manually if preferred.
-
-  const imports: string[] = [];
-  /**
-   * You can specify components which you want to import from external/internal packages
-   * in format:
-   *  {
-   *    name: 'package name',
-   *    components: [
-   *      {
-   *        componentName: 'component name', // component rendering name,
-   *        moduleName: 'module name' // component name to import from the package
-   *      }
-   *    ]
-   *  }
-   */
-  const packages: PackageDefinition[] = [
-    {
-      name: '@sitecore-jss/sitecore-jss-angular',
-      components: [{ componentName: 'Form', moduleName: 'FormComponent' }],
-    },
-  ];
-  const registrations: string[] = [];
-  const lazyRegistrations: string[] = [];
-  const declarations: string[] = [];
-  const components: string[] = [];
-
-  packages.forEach((p) => {
-    const variables = p.components
-      .map((c) => {
-        registrations.push(`{ name: '${c.componentName}', type: ${c.moduleName} },`);
-        components.push(c.componentName);
-
-        return c.moduleName;
-      })
-      .join(', ');
-    imports.push(`import { ${variables} } from '${p.name}'`);
-  });
-
-  fs.readdirSync(componentRootPath).forEach((componentFolder) => {
-    // ignore ts files in component root folder
-    if (componentFolder.endsWith('.ts') || componentFolder === '.gitignore') {
-      return;
-    }
-
-    const componentFilePath = path.join(
-      componentRootPath,
-      componentFolder,
-      `${componentFolder}.component.ts`
-    );
-
-    if (!fs.existsSync(componentFilePath)) {
-      return;
-    }
-
-    const componentFileContents = fs.readFileSync(componentFilePath, 'utf8');
-
-    // ASSUMPTION: your component should export a class directly that follows Angular conventions,
-    // i.e. `export class FooComponent` - so we can detect the component's name for auto registration.
-    const componentClassMatch = /export class (.+?)Component\b/g.exec(componentFileContents);
-
-    if (componentClassMatch === null) {
-      console.debug(
-        `Component ${componentFilePath} did not seem to export a component class. It will be skipped.`
-      );
-      return;
-    }
-
-    const componentName = componentClassMatch[1];
-    const importVarName = `${componentName}Component`;
-
-    components.push(componentName);
-
-    // check for lazy loading needs
-    const moduleFilePath = path.join(
-      componentRootPath,
-      componentFolder,
-      `${componentFolder}.module.ts`
-    );
-    const isLazyLoaded = fs.existsSync(moduleFilePath);
-
-    if (isLazyLoaded) {
-      console.debug(`Registering JSS component (lazy) ${componentName}`);
-      lazyRegistrations.push(
-        `{ path: '${componentName}', loadChildren: () => import('./${componentFolder}/${componentFolder}.module').then(m => m.${componentName}Module) },`
-      );
-    } else {
-      console.debug(`Registering JSS component ${componentName}`);
-      imports.push(
-        `import { ${importVarName} } from './${componentFolder}/${componentFolder}.component';`
-      );
-      registrations.push(`{ name: '${componentName}', type: ${importVarName} },`);
-      declarations.push(`${importVarName},`);
-    }
-  });
-
-  return componentFactoryTemplate({
-    imports,
-    components,
-    registrations,
-    lazyRegistrations,
-    declarations,
-  });
-}
+(Object.values(plugins) as ComponentFactoryPlugin[])
+  .sort((p1, p2) => p1.order - p2.order)
+  .reduce((config, plugin) => plugin.exec(config), defaultConfig);
