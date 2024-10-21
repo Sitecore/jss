b813e6464bbe654841b3640ceb6bdba9e1f59787
yavorsk <yavor.krastev@sitecore.com>
2024-10-21 09:45:39 +0300

    rework metadata generation to use 'npm query'

--- packages/sitecore-jss-dev-tools/src/templating/metadata.test.ts
+++ packages/sitecore-jss-dev-tools/src/templating/metadata.test.ts
@@ -1,16 +1,12 @@
 /* eslint-disable quotes */
 /* eslint-disable no-unused-expressions */
 import { expect } from 'chai';
-import fs from 'fs';
-import path from 'path';
 import { getMetadata } from './metadata';
 import sinon, { SinonStub } from 'sinon';
 import childProcess from 'child_process';
-import packageLockNextjs from './../test-data/metadata/package-lock-nextjs.json';
-import packageLockAngular from './../test-data/metadata/package-lock-angular.json';
-import packageLockNoSitecore from './../test-data/metadata/package-lock-no-sitecore-scopes.json';
 import metadataNextjs from './../test-data/metadata/metadata-nextjs.json';
-import metadataAngular from './../test-data/metadata/metadata-angular.json';
+import npmQueryResult from './../test-data/metadata/npm-query.json';
+import npmQueryResultNoSc from './../test-data/metadata/npm-query-no-sc.json';
 
 describe('metadata', () => {
   afterEach(() => {
@@ -18,74 +14,40 @@ describe('metadata', () => {
   });
 
   describe('getMetadata', () => {
-    const packageLockPath = `.${path.sep}package-lock.json`;
-    let readdirSync: SinonStub;
-    let readFileSync: SinonStub;
-    let existsSync: SinonStub;
     let execSyncStub: SinonStub;
 
     afterEach(() => {
-      readdirSync?.restore();
-      readFileSync?.restore();
-      existsSync?.restore();
       execSyncStub?.restore();
     });
 
-    it('it should create package-lock.json if not already present and then remove it', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(false);
-      readFileSync = sinon.stub(fs, 'readFileSync');
-      readFileSync.withArgs(packageLockPath, 'utf8').returns(JSON.stringify({}));
+    it('should return tracked packages with exact versions from result of npm query', () => {
       execSyncStub = sinon.stub(childProcess, 'execSync');
-      getMetadata();
-      expect(execSyncStub.calledTwice).to.be.true;
-    });
-
-    it('should thorow if package-lock creation failed', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(false);
-      execSyncStub = sinon.stub(childProcess, 'execSync').throws('error');
-      expect(() => getMetadata()).to.throw;
-    });
-
-    it('should return tracked packages from example nextjs package-lock.json', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(true);
-      readFileSync = sinon.stub(fs, 'readFileSync');
-      readFileSync.withArgs(packageLockPath, 'utf8').returns(JSON.stringify(packageLockNextjs));
-
+      execSyncStub
+        .withArgs('npm query [name*=@sitecore] --workspaces false')
+        .returns(JSON.stringify(npmQueryResult));
       const metadata = getMetadata();
       expect(metadata).to.deep.equal(metadataNextjs);
     });
 
-    it('should return tracked packages from example angular package-lock.json', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(true);
-      readFileSync = sinon.stub(fs, 'readFileSync');
-      readFileSync.withArgs(packageLockPath, 'utf8').returns(JSON.stringify(packageLockAngular));
-
-      const metadata = getMetadata();
-      expect(metadata).to.deep.equal(metadataAngular);
+    it('should throw if result of npm query is not valid', () => {
+      execSyncStub = sinon.stub(childProcess, 'execSync');
+      execSyncStub.withArgs('npm query [name*=@sitecore] --workspaces false').returns('[{"name":}');
+      expect(() => getMetadata()).to.throw;
     });
 
-    it('should not return packages for package-lock not containng tracked packages', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(true);
-      readFileSync = sinon.stub(fs, 'readFileSync');
-      readFileSync.withArgs(packageLockPath, 'utf8').returns(JSON.stringify(packageLockNoSitecore));
+    it('should thorow if npm query command fails', () => {
+      execSyncStub = sinon.stub(childProcess, 'execSync');
+      execSyncStub.withArgs('npm query [name*=@sitecore] --workspaces false').throws();
+      expect(() => getMetadata()).to.throw;
+    });
 
+    it('should not return packages for result of npm query not containng tracked packages', () => {
+      execSyncStub = sinon.stub(childProcess, 'execSync');
+      execSyncStub
+        .withArgs('npm query [name*=@sitecore] --workspaces false')
+        .returns(JSON.stringify(npmQueryResultNoSc));
       const metadata = getMetadata();
       expect(metadata).to.deep.equal({ packages: {} });
-    });
-
-    it('should throw if package-lock json not valid', () => {
-      existsSync = sinon.stub(fs, 'existsSync');
-      existsSync.withArgs(packageLockPath).returns(true);
-      readFileSync = sinon.stub(fs, 'readFileSync');
-      readFileSync
-        .withArgs(packageLockPath, 'utf8')
-        .returns(fs.readFileSync('./../test-data/metadata/package-lock-invalid.json', 'utf8'));
-      expect(() => getMetadata()).to.throw;
     });
   });
 });
--- packages/sitecore-jss-dev-tools/src/templating/metadata.ts
+++ packages/sitecore-jss-dev-tools/src/templating/metadata.ts
@@ -1,15 +1,11 @@
 import { Metadata } from '@sitecore-jss/sitecore-jss/editing';
-import fs from 'fs';
-import path from 'path';
 import { execSync } from 'child_process';
 
 type Package = {
-  [key: string]: unknown;
   name: string;
   version: string;
 };
 
-const dependencyTypes = ['dependencies', 'devDependencies', 'peerDependencies'];
 const trackedScopes = ['@sitecore', '@sitecore-cloudsdk', '@sitecore-feaas', '@sitecore-jss'];
 
 /**
@@ -18,74 +14,32 @@ const trackedScopes = ['@sitecore', '@sitecore-cloudsdk', '@sitecore-feaas', '@s
 export function getMetadata(): Metadata {
   const metadata: Metadata = { packages: {} };
 
-  const packageLockPath = `.${path.sep}package-lock.json`;
-  const packageLockIsAvailable = fs.existsSync(packageLockPath);
-
-  if (!packageLockIsAvailable) {
-    try {
-      console.log('Creating package-lock.json ...');
-      execSync('npm i --package-lock-only --workspaces false --force');
-    } catch (error) {
-      console.error('Failed to create package-lock.json in project', error);
-      return metadata;
-    }
-  }
-
-  let packageLock = {};
+  let queryResult: Package[] = [];
   try {
-    packageLock = JSON.parse(fs.readFileSync(packageLockPath, 'utf8'));
+    queryResult = JSON.parse(execSync('npm query [name*=@sitecore] --workspaces false').toString());
   } catch (error) {
-    console.error('Failed to read/parse package-lock.json', error);
+    console.error('Failed to retrieve sitecore packages using npm query', error);
     return metadata;
   }
 
-  metadata.packages = getPackagesFromPackageLock(packageLock);
-
-  if (!packageLockIsAvailable) {
-    console.log('Deleting package-lock.json ...');
-    execSync(`del "${packageLockPath}"`);
-  }
+  metadata.packages = getPackagesFromQueryResult(queryResult);
 
   return metadata;
 }
 
 /**
- * Traverses the provided package-lock.json to find the packages of the tracked scopes with their exact versions
- * @param {Record<string, unknown>} packageLockJson the package-lock json to traverse
+ * Retrieve all packages of the tracked scopes with their exact versions
+ * @param {Package[]} scPackages list of packages
  * @returns {Record<string, string>} an object with the packages with their exact versions
  */
-function getPackagesFromPackageLock(
-  packageLockJson: Record<string, unknown>
-): Record<string, string> {
-  const packagesResult: Record<string, string> = {};
-  const packages = packageLockJson.packages as Record<string, Package>;
+function getPackagesFromQueryResult(scPackages: Package[]): Record<string, string> {
+  const packages: Record<string, string> = {};
 
-  // traverse packages to get all tracked package names
-  // eslint-disable-next-line guard-for-in
-  for (const key in packages) {
-    dependencyTypes.forEach((depType) => {
-      for (const dep in packages[key][depType] as Record<string, string>) {
-        if (trackedScopes.some((trackedScope) => dep.startsWith(trackedScope))) {
-          if (!(dep in packagesResult)) {
-            packagesResult[dep] = '';
-          }
-        }
-      }
-    });
-  }
+  scPackages.forEach((scPackage) => {
+    if (trackedScopes.some((trackedScope) => scPackage.name.startsWith(trackedScope))) {
+      packages[scPackage.name] = scPackage.version;
+    }
+  });
 
-  // traverse packages again to get the exact versions.
-  // getting packages names and versions in one go doesn't gurantee getting all package info correctly
-  // eslint-disable-next-line guard-for-in
-  for (const key in packages) {
-    Object.keys(packagesResult).forEach((scPackage) => {
-      if (packages[key].name === scPackage || key.endsWith(scPackage)) {
-        if (!packagesResult[scPackage]) {
-          packagesResult[scPackage] = packages[key].version;
-        }
-      }
-    });
-  }
-
-  return packagesResult;
+  return packages;
 }
