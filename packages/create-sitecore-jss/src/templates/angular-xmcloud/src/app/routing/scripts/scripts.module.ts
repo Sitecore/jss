27e39a81cc759235c817fc21f63faa97f8953476
Artem Alexeyenko <ala@sitecore.net>
2024-10-22 03:14:08 -0400

    [angular][xmcloud] Init CloudSDK on browser (#1952)
    * [angular][xmcloud] Init CloudSDK on browser
    
    * PR followup
    
    * fix events init
    
    * correct cloudsdk package in nextjs

--- packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/routing/scripts/scripts.module.ts
+++ packages/create-sitecore-jss/src/templates/angular-xmcloud/src/app/routing/scripts/scripts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ScriptsComponent } from './scripts.component';
 import { JssModule } from '@sitecore-jss/sitecore-jss-angular';
+import { CloudSdkInitComponent } from './cloud-sdk-init.component';
 
 @NgModule({
   exports: [ScriptsComponent],
   imports: [JssModule],
-  declarations: [ScriptsComponent],
+  declarations: [ScriptsComponent, CloudSdkInitComponent],
 })
 export class ScriptsModule {}
