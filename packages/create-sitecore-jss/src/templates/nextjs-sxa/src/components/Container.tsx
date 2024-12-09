2eaed8bafe3f2cf43410c3d37eabf8ce8f2b14e8
Ruslan Matkovskyi <ruslan.matkovskyi@sitecore.com>
2024-12-10 00:45:57 +0100

    CHANGELOG has been changed

--- packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
+++ packages/create-sitecore-jss/src/templates/nextjs-sxa/src/components/Container.tsx
@@ -4,74 +4,54 @@ import {
   Placeholder,
   useSitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
-import React from 'react';
+import config from 'temp/config';
+
+const MEDIA_URL_PATTERN = /mediaurl="([^"]*)"/i;
 
 interface ComponentProps {
   rendering: ComponentRendering & { params: ComponentParams };
   params: ComponentParams;
 }
 
-const BACKGROUND_REG_EXP = /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/gi;
-const MEDIA_URL_PATTERN = /mediaurl="([^"]*)"/i;
-const SITECORE_MEDIA_PREFIX = '/sitecore/shell/-/media/';
+const DefaultContainer = (props: ComponentProps): JSX.Element => {
+  const { sitecoreContext } = useSitecoreContext();
+  const isEditMode = sitecoreContext.pageState === 'edit';
+  const containerStyles = props.params && props.params.Styles ? props.params.Styles : '';
+  const styles = `${props.params.GridParameters} ${containerStyles}`.trimEnd();
+  const phKey = `container-${props.params.DynamicPlaceholderId}`;
+  const id = props.params.RenderingIdentifier;
+  const backgroundImage = props.params.BackgroundImage as string;
+  let backgroundStyle: { [key: string]: string } = {};
 
-const getBackgroundStyle = (
-  backgroundImage: string,
-  isEditMode: boolean
-): { [key: string]: string } => {
-  if (!backgroundImage) return {};
+  if (backgroundImage && backgroundImage.match(MEDIA_URL_PATTERN)) {
+    const mediaUrl = backgroundImage.match(MEDIA_URL_PATTERN)?.[1] || '';
 
-  if (isEditMode) {
-    const processedImage = backgroundImage
-      .match(BACKGROUND_REG_EXP)
-      ?.pop()
-      ?.replace(/-/gi, '');
-    return processedImage
-      ? { backgroundImage: `url('${SITECORE_MEDIA_PREFIX}${processedImage}')` }
-      : {};
+    backgroundStyle = {
+      backgroundImage: `url('${isEditMode ? config.sitecoreApiHost : ''}${mediaUrl}')`,
+    };
   }
 
-  const mediaUrl = backgroundImage.match(MEDIA_URL_PATTERN)?.[1] || '';
-  return mediaUrl ? { backgroundImage: `url('${mediaUrl}')` } : {};
-};
-
-const DefaultContainer: React.FC<ComponentProps> = ({ params, rendering }): JSX.Element => {
-  const { sitecoreContext } = useSitecoreContext();
-  const {
-    Styles: containerStyles = '',
-    GridParameters = '',
-    DynamicPlaceholderId,
-    RenderingIdentifier,
-    BackgroundImage,
-  } = params;
-
-  const styles = `${GridParameters} ${containerStyles}`.trimEnd();
-  const placeholderKey = `container-${DynamicPlaceholderId}`;
-  const backgroundStyle = getBackgroundStyle(
-    BackgroundImage as string,
-    sitecoreContext.pageState === 'edit'
-  );
-
   return (
-    <div className={`component container-default ${styles}`} id={RenderingIdentifier || undefined}>
+    <div className={`component container-default ${styles}`} id={id ? id : undefined}>
       <div className="component-content" style={backgroundStyle}>
         <div className="row">
-          <Placeholder name={placeholderKey} rendering={rendering} />
+          <Placeholder name={phKey} rendering={props.rendering} />
         </div>
       </div>
     </div>
   );
 };
 
-export const Default: React.FC<ComponentProps> = (props): JSX.Element => {
-  const { Styles = '' } = props.params;
-  const hasContainerStyle = Styles.split(' ').includes('container');
+export const Default = (props: ComponentProps): JSX.Element => {
+  const splitStyles = props.params?.Styles?.split(' ');
 
-  return hasContainerStyle ? (
-    <div className="container-wrapper">
-      <DefaultContainer {...props} />
-    </div>
-  ) : (
-    <DefaultContainer {...props} />
-  );
+  if (splitStyles && splitStyles.includes('container')) {
+    return (
+      <div className="container-wrapper">
+        <DefaultContainer {...props} />
+      </div>
+    );
+  }
+
+  return <DefaultContainer {...props} />;
 };
