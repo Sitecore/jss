70babd71f38dfcaddd85411e80aaded6a297fe62
Artem Alexeyenko <ala@sitecore.net>
2025-04-25 16:50:32 -0400

    Adjust plugin

--- packages/create-sitecore-jss/src/templates/nextjs-xmcloud/scripts/generate-component-builder/plugins/code-extraction.ts
+++ packages/create-sitecore-jss/src/templates/nextjs-xmcloud/scripts/generate-component-builder/plugins/code-extraction.ts
@@ -1,16 +1,16 @@
-import { ComponentBuilderPlugin } from '..';
+import { ComponentBuilderPlugin, ComponentBuilderPluginConfig } from '..';
 import { extractComponents } from '@sitecore-jss/sitecore-jss-dev-tools';
 
 /**
- * Provides Sitecore Components (FEaaS) packages configuration
+ * If consented to, sends components code for code generation learning to XMC
  */
-class CodeExtractorPlugin implements ComponentBuilderPlugin {
+class CodeExtractionPlugin implements ComponentBuilderPlugin {
   order = 9999;
 
-  exec() {
-    extractComponents();
+  exec(config: ComponentBuilderPluginConfig) {
+    extractComponents({});
+    return config;
   }
-    
 }
 
-export const feaasPlugin = new CodeExtractorPlugin();
+export const codeExtractionPlugin = new CodeExtractionPlugin();
