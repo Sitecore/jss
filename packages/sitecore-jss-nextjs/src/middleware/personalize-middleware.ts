9d818ae81bb5b0aadb4badecbff31658a777a846
yavorsk <yavor.krastev@sitecore.com>
2025-05-20 11:12:31 +0300

    update geolocation type in PersonalizeOptions to use personalize's PersonalizeGeolocation interface

--- packages/sitecore-jss-nextjs/src/middleware/personalize-middleware.ts
+++ packages/sitecore-jss-nextjs/src/middleware/personalize-middleware.ts
@@ -11,6 +11,7 @@ import { debug } from '@sitecore-jss/sitecore-jss';
 import { MiddlewareBase, MiddlewareBaseConfig, REWRITE_HEADER_NAME } from './middleware';
 import { CloudSDK } from '@sitecore-cloudsdk/core/server';
 import { personalize } from '@sitecore-cloudsdk/personalize/server';
+import { PersonalizeGeolocation } from '@sitecore-cloudsdk/personalize/dist/esm/src/lib/personalization/personalizer';
 
 export type CdpServiceConfig = {
   /**
@@ -58,16 +59,7 @@ export type PersonalizeOptions = {
   /**
    * Geolocation data used for personalization
    */
-  geo?: PersonalizeGeoData;
-};
-
-/**
- * Represents the geolocation data used for personalization
- */
-export type PersonalizeGeoData = {
-  city?: string;
-  country?: string;
-  region?: string;
+  geo?: PersonalizeGeolocation;
 };
 
 /**
