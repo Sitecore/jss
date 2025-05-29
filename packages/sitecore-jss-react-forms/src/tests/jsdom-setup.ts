c06019e903e324144c19b2e63b6af6857c4ef593
yavorsk <yavor.krastev@sitecore.com>
2025-05-29 15:13:57 +0300

    fix pipeline error

--- packages/sitecore-jss-react-forms/src/tests/jsdom-setup.ts
+++ packages/sitecore-jss-react-forms/src/tests/jsdom-setup.ts
@@ -35,9 +35,7 @@ process.env.TEST = 'true';
 
 global.window = jsDomWindow;
 global.document = jsDomWindow.document;
-global.navigator = {
-  userAgent: 'node.js',
-} as Navigator;
+global.navigator['#userAgent'] = 'node.js';
 
 global.HTMLElement = jsDomWindow.HTMLElement; // makes chai "happy" https://github.com/chaijs/chai/issues/1029
 copyProps(jsDomWindow, global);
