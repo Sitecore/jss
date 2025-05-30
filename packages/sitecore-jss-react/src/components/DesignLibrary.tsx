6992a7027cc0c1ceddff5c08d89c27964c7a7d4c
yavorsk <yavor.krastev@sitecore.com>
2025-05-30 12:28:53 +0300

    update yarn lock, fix build

--- packages/sitecore-jss-react/src/components/DesignLibrary.tsx
+++ packages/sitecore-jss-react/src/components/DesignLibrary.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { JSX, useEffect, useMemo, useState } from 'react';
 import { Placeholder } from './Placeholder';
 import {
   ComponentRendering,
