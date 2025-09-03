4f9eb9179dab2bb84ddb21a8849f7fed084b49ca
illiakovalenko <illia.kovalenko@sitecore.com>
2025-09-03 14:30:00 +0300

    [Next.js] Support component-level data fetching in 404/500 pages

--- packages/create-sitecore-jss/src/templates/nextjs/src/lib/page-props-factory/plugins/component-props.ts
+++ packages/create-sitecore-jss/src/templates/nextjs/src/lib/page-props-factory/plugins/component-props.ts
@@ -1,50 +1,17 @@
-import { ComponentPropsService, ComponentPropsError } from '@sitecore-jss/sitecore-jss-nextjs';
-import { SitecorePageProps } from 'lib/page-props';
 import { GetServerSidePropsContext, GetStaticPropsContext } from 'next';
-import { moduleFactory } from 'temp/componentBuilder';
-import { Plugin, isServerSidePropsContext } from '..';
+import { SitecorePageProps } from 'lib/page-props';
+import { fetchComponentProps } from 'lib/component-props';
+import { Plugin } from '..';
 
 class ComponentPropsPlugin implements Plugin {
-  private componentPropsService: ComponentPropsService;
-
   // Make sure to run this plugin last to ensure that the updated layout data is used
   order = 10;
-
-  constructor() {
-    this.componentPropsService = new ComponentPropsService();
-  }
 
   async exec(props: SitecorePageProps, context: GetServerSidePropsContext | GetStaticPropsContext) {
     if (!props.layoutData.sitecore.route) return props;
 
     // Retrieve component props using side-effects defined on components level
-    if (isServerSidePropsContext(context)) {
-      props.componentProps = await this.componentPropsService.fetchServerSideComponentProps({
-        layoutData: props.layoutData,
-        context,
-        moduleFactory,
-      });
-    } else {
-      props.componentProps = await this.componentPropsService.fetchStaticComponentProps({
-        layoutData: props.layoutData,
-        context,
-        moduleFactory,
-      });
-    }
-
-    const errors = Object.keys(props.componentProps)
-      .map(id => {
-        const component = props.componentProps[id] as ComponentPropsError;
-
-        return component.error
-          ? `\nUnable to get component props for ${component.componentName} (${id}): ${component.error}`
-          : '';
-      })
-      .join('');
-
-    if (errors.length) {
-      throw new Error(errors);
-    }
+    props.componentProps = await fetchComponentProps(props.layoutData, context);
 
     return props;
   }
