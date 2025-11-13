efdd14b5a381d5714e66afbab23035156aa12e8d
Artem Alexeyenko <ala@sitecore.net>
2025-11-13 13:56:38 -0500

    make lint happy

--- packages/sitecore-jss-angular/src/components/form.component.ts
+++ packages/sitecore-jss-angular/src/components/form.component.ts
@@ -44,21 +44,7 @@ export type FormRendering = {
  */
 @Component({
   selector: 'app-form',
-  template: `
-    @if (isEditing) {
-        @if (!rendering.params.FormId) {
-    <div
-      style="background: darkorange; outline: 5px solid orange; padding: 10px; color: white; max-width: 500px;"
-    >
-      <h2>{{ rendering.componentName }}</h2>
-      <p>JSS component is missing FormId rendering parameter.</p>
-    </div>
-    }
-    @if (hasError) {
-    <div class="sc-jss-placeholder-error">There was a problem loading this section</div>
-        }
-    }
-  `,
+  templateUrl: './form.component.html',
 })
 export class FormComponent implements OnInit, OnDestroy {
   /**
